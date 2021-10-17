a00e6da2ae7debe3f1447a5778fc078900f8c20f
gavin.doughtie <gavin.doughtie@gmail.com>
2021-10-17 15:40:24 -0700

    attempting to build JSON

--- pages/proposals/create.tsx
+++ pages/proposals/create.tsx
@@ -6,7 +6,8 @@ import { useRouter } from 'next/router'
 import LineAlert from 'components/LineAlert'
 import cloneDeep from 'lodash.clonedeep'
 
-import { coins, StdFee } from '@cosmjs/stargate';
+import { coins, StdFee } from '@cosmjs/stargate'
+import { buildOutgoingMessage, makeSpender } from './messagehelpers'
 
 interface FormElements extends HTMLFormControlsCollection {
   label: HTMLInputElement
@@ -21,24 +22,25 @@ interface ProposalFormElement extends HTMLFormElement {
 const contractAddress = process.env.NEXT_PUBLIC_DAO_CONTRACT_ADDRESS || ''
 
 function validateJsonMsg(json: any) {
-  if (typeof json !== 'object') {
-    return false
-  }
-  if (Array.isArray(json)) {
-    return false
-  }
-  const messages = json?.body?.messages || []
-  if (messages.length !== 1) {
-    return false
-  }
-  // const [message] = messages
-  // if (message['@type'] !== '/cosmos.bank.v1beta1.MsgSend') {
-  //   return false
-  // }
-  // if (message.from_address !== contractAddress) {
-  //   return false
-  // }
   return true
+  // if (typeof json !== 'object') {
+  //   return false
+  // }
+  // if (Array.isArray(json)) {
+  //   return false
+  // }
+  // const messages = json?.body?.messages || []
+  // if (messages.length !== 1) {
+  //   return false
+  // }
+  // // const [message] = messages
+  // // if (message['@type'] !== '/cosmos.bank.v1beta1.MsgSend') {
+  // //   return false
+  // // }
+  // // if (message.from_address !== contractAddress) {
+  // //   return false
+  // // }
+  // return true
 }
 
 const ProposalCreate: NextPage = () => {
@@ -48,14 +50,29 @@ const ProposalCreate: NextPage = () => {
   const [transactionHash, setTransactionHash] = useState('')
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
+  const [proposalMessage, setProposalMessage] = useState<any>() // message type?
+  const [messageJson, setMessageJson] = useState('')
   const [proposalID, setProposalID] = useState('')
+  let spendMsg
+
+  const spend = makeSpender(walletAddress)
+
+  const handleSpend = () => {
+    const amount = prompt('Amount?')
+    if (amount) {
+      spendMsg = spend(amount)
+      setProposalMessage(spendMsg)
+      setMessageJson(JSON.stringify([spendMsg]))
+      console.dir(spendMsg)
+    }
+  }
 
   const defaultExecuteFee: StdFee = {
     amount: coins(3000, process.env.NEXT_PUBLIC_STAKING_DENOM!),
     gas: '333333',
-  };
+  }
 
-  const handleSubmit = (event: FormEvent<ProposalFormElement>) => {
+  const handleSubmit = async (event: FormEvent<ProposalFormElement>) => {
     event.preventDefault()
     setLoading(true)
     setError('')
@@ -66,20 +83,17 @@ const ProposalCreate: NextPage = () => {
     const description = currentTarget.description.value.trim()
     const jsonStr = currentTarget.json.value.trim()
 
-    if (
-      title.length === 0 ||
-      description.length === 0
-    ) {
+    if (title.length === 0 || description.length === 0) {
       setLoading(false)
       setError('Title and Description are required.')
     }
 
     // clone json string to avoid prototype poisoning
     // https://medium.com/intrinsic-blog/javascript-prototype-poisoning-vulnerabilities-in-the-wild-7bc15347c96
-    let json;
+    let json
     const jsonClone = cloneDeep(jsonStr)
     if (jsonClone) {
-      json = JSON.parse(jsonClone);
+      json = JSON.parse(jsonClone)
       if (!validateJsonMsg(json)) {
         setLoading(false)
         setError('Error in JSON message.')
@@ -87,16 +101,24 @@ const ProposalCreate: NextPage = () => {
       }
     }
 
+    console.log(`JSON MESSAGE BEING SENT:`)
+    console.dir(json)
+
     const msg = {
       title,
       description,
-      msgs: json || []
+      msgs: json || [],
     }
 
-    signingClient
-      ?.execute(walletAddress, contractAddress, { propose: msg }, defaultExecuteFee)
-      .then((response) => {
-        setLoading(false)
+    try {
+      const response = await signingClient?.execute(
+        walletAddress,
+        contractAddress,
+        { propose: msg },
+        defaultExecuteFee
+      )
+      setLoading(false)
+      if (response) {
         setTransactionHash(response.transactionHash)
         const [{ events }] = response.logs
         const [wasm] = events.filter((e) => e.type === 'wasm')
@@ -104,19 +126,21 @@ const ProposalCreate: NextPage = () => {
           (w) => w.key === 'proposal_id'
         )
         setProposalID(value)
-      })
-      .catch((e) => {
-        setLoading(false)
-        setError(e.message)
-      })
+      }
+    } catch (e: any) {
+      setLoading(false)
+      setError(e.message)
+    }
   }
 
   const complete = transactionHash.length > 0
+  const jsonValue = spendMsg ? JSON.stringify(spendMsg) : ''
 
   return (
     <WalletLoader>
       <div className="flex flex-col w-full">
         <div className="grid bg-base-100 place-items-center">
+          <button onClick={() => handleSpend()}>Spend</button>
           <form
             className="text-left container mx-auto max-w-lg"
             onSubmit={handleSubmit}
@@ -133,12 +157,13 @@ const ProposalCreate: NextPage = () => {
               className="input input-bordered rounded box-border p-3 h-24 w-full focus:input-primary text-xl"
               name="description"
               readOnly={complete}
-            />
+            ></textarea>
             <label className="block mt-4">JSON</label>
             <textarea
               className="input input-bordered rounded box-border p-3 w-full font-mono h-80 focus:input-primary text-x"
               cols={7}
               name="json"
+              defaultValue={messageJson}
               readOnly={complete}
             />
             {!complete && (
