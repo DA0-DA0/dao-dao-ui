ffd33870788f73aebe9a0b13fb67aadf4f7b8146
Jake Hartnell <jake.hartnell@gmail.com>
2021-10-17 16:04:21 -0700

    Get spend proposals working

--- pages/proposals/messagehelpers.ts
+++ pages/proposals/messagehelpers.ts
@@ -1,42 +1,24 @@
 const DAO_ADDRESS = process.env.NEXT_PUBLIC_DAO_CONTRACT_ADDRESS || '';
 const DENOM = process.env.NEXT_PUBLIC_STAKING_DENOM || '';
 
-export function makeSpender(from_address: string) {
-  const spendBase = {
-    // '@type': '/cosmos.bank.v1beta1.MsgSend',
-    bank: 'send',
-    from_address,
-    to_address: DAO_ADDRESS,
-  };
-  const amountBase = {
-    denom: DENOM,
-  };
+export function makeSpender(to_address: string) {
   return (amount: string) => {
-    return {
-      ...spendBase,
-      amount: [
-        {
-          ...amountBase,
-          amount,
+    return [
+      {
+        bank: {
+          send: {
+            '@type': '/cosmos.bank.v1beta1.MsgSend',
+            from_address: DAO_ADDRESS,
+            to_address,
+            amount: [
+              {
+                amount,
+                denom: DENOM,
+              },
+            ],
+          },
         },
-      ],
-    };
-  };
-}
-
-export function buildOutgoingMessage(msg: any) {
-  return {
-    body: {
-      messages: [msg],
-      memo: '',
-      timeout_height: '0',
-      extension_options: [],
-      non_critical_extension_options: [],
-    },
-    auth_info: {
-      signer_infos: [],
-      fee: { amount: [], gas_limit: '200000', payer: '', granter: '' },
-    },
-    signatures: [],
+      },
+    ];
   };
 }
