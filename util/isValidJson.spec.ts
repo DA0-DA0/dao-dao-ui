ffe590ded79af20ee1e6a1c9874fc4cc606305fd
elsehow <yes@cosmopol.is>
2021-10-18 12:25:54 -0700

    unit tests for json validation

---
+++ util/isValidJson.spec.ts
@@ -0,0 +1,27 @@
+import 'jest'
+import isValidJson from './isValidJson'
+
+const goodInputs =
+  [
+    [{"bank":{"send":{"@type":"/cosmos.bank.v1beta1.MsgSend","from_address":"wasm1nc5tatafv6eyq7llkr2gv50ff9e22mnfhap4vz","to_address":"wasm19mfj3r5g5yqsjcxk5ey76mfm9ed6x8a333lxzr","amount":[{"amount":"666","denom":"ustake"}]}}}],
+    [{"bank":{"send":{"@type":"/cosmos.bank.v1beta1.MsgSend",}}}],
+  ]
+const badInputs =[
+  1, // numbers?
+  'hi', // strings?
+  isValidJson, // methods?
+  ['naughty'],
+  [{"bank": {"send": {}}}],
+  {"body":{"messages":[{"@type":"/cosmos.bank.v1beta1.MsgSend"}]}},
+  // good but a string?
+  '[{"bank":{"send":{"@type":"/cosmos.bank.v1beta1.MsgSend","from_address":"wasm1nc5tatafv6eyq7llkr2gv50ff9e22mnfhap4vz","to_address":"wasm19mfj3r5g5yqsjcxk5ey76mfm9ed6x8a333lxzr","amount":[{"amount":"666","denom":"ustake"}]}}}],'
+]
+
+describe('isValidJson', () => {
+  it('should accept good json', () => {
+    goodInputs.forEach(input => expect(isValidJson(input)).toBe(true))
+  })
+  it('should reject bad json', () => {
+    badInputs.forEach(input => expect(isValidJson(input)).toBe(false))
+  })
+})
