72a0ffb143354c3c4d508f1accce0139099e490a
gavin.doughtie <gavin.doughtie@gmail.com>
2021-10-18 15:51:49 -0700

    refactored messagehelpers a little

--- util/messagehelpers.ts
+++ util/messagehelpers.ts
@@ -1,22 +1,36 @@
+import { BankMsg } from 'types/cw3';
+
 const DAO_ADDRESS = process.env.NEXT_PUBLIC_DAO_CONTRACT_ADDRESS || '';
 const DENOM = process.env.NEXT_PUBLIC_STAKING_DENOM || '';
 
+export const TYPE_KEY = '@type';
+export const BANK_SEND_TYPE = '/cosmos.bank.v1beta1.MsgSend';
+
+export function makeBankMessage(
+  amount: string,
+  to_address: string,
+  from_address = DAO_ADDRESS,
+  denom = DENOM
+): BankMsg {
+  return {
+    send: {
+      from_address,
+      to_address,
+      amount: [
+        {
+          amount,
+          denom,
+        },
+      ],
+    },
+  };
+}
+
 export function makeSpendMessage(amount: string, to_address: string) {
+  const bank: BankMsg = makeBankMessage(amount, to_address);
   return [
     {
-      bank: {
-        send: {
-          '@type': '/cosmos.bank.v1beta1.MsgSend',
-          from_address: DAO_ADDRESS,
-          to_address,
-          amount: [
-            {
-              amount,
-              denom: DENOM,
-            },
-          ],
-        },
-      },
+      bank,
     },
   ];
 }
