6a26512dad0d0935de7b1917fb7fc1f83e042a9d
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-11-18 14:42:43 -0800

    Add multisig create proposal page & hook.

--- hooks/proposals.ts
+++ hooks/proposals.ts
@@ -2,6 +2,9 @@ import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
 import { useEffect, useState } from 'react'
 import { ProposalListResponse, ProposalResponse, Timestamp } from 'types/cw3'
 import { useSigningClient } from 'contexts/cosmwasm'
+import { memoForProposal, Proposal } from 'models/proposal/proposal'
+import { messageForProposal } from 'models/proposal/proposalSelectors'
+import { defaultExecuteFee } from 'util/fee'
 
 export function useProposals(contractAddress: string) {
   const { walletAddress, signingClient } = useSigningClient()
@@ -39,4 +42,50 @@ export function useProposals(contractAddress: string) {
     sign(signingClient)
   }, [walletAddress, signingClient, startBefore])
   return { proposals, hideLoadMore, loading, setStartBefore }
+}
+
+export function useCreateProposal(contractAddress: string) {
+  const { walletAddress, signingClient } = useSigningClient()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [proposalID, setProposalID] = useState('')
+
+  const execute = async (proposal: Proposal) => {
+    setLoading(true)
+    setError('')
+
+    const propose = messageForProposal(proposal)
+    const memo = memoForProposal(proposal)
+
+    try {
+      const response = await signingClient?.execute(
+        walletAddress,
+        contractAddress,
+        { propose },
+        defaultExecuteFee,
+        memo
+      )
+      setLoading(false)
+      if (response) {
+        // setTransactionHash(response.transactionHash)
+        const [{ events }] = response.logs
+        const [wasm] = events.filter((e) => e.type === 'wasm')
+        const [{ value }] = wasm.attributes.filter(
+          (attr) => attr.key === 'proposal_id'
+        )
+        setProposalID(value)
+        return response
+      }
+    } catch (e: any) {
+      console.error(
+        `Error submitting proposal ${JSON.stringify(proposal, undefined, 2)}`
+      )
+      console.dir(e)
+      console.error(e.message)
+      setLoading(false)
+      setError(e.message)
+    }
+  }
+
+  return { walletAddress, loading, error, proposalID, execute }
 }
