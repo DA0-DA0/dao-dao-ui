062e2b370e9c44dc598be9273bdbf328b6a6c342
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-11-17 23:53:14 -0800

    Refactor out ProposalList component.

--- pages/proposals/index.tsx
+++ pages/proposals/index.tsx
@@ -1,16 +1,11 @@
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
-import ProposalCard from 'components/ProposalCard'
 import WalletLoader from 'components/WalletLoader'
 import { useSigningClient } from 'contexts/cosmwasm'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { ProposalListResponse, ProposalResponse, Timestamp } from 'types/cw3'
-
-// TODO: review union Expiration from types/cw3
-type Expiration = {
-  at_time: Timestamp
-}
+import ProposalList from 'components/ProposalList'
 
 const contractAddress = process.env.NEXT_PUBLIC_DAO_CONTRACT_ADDRESS || ''
 
@@ -64,42 +59,17 @@ const Home: NextPage = () => {
           </button>
         </div>
       </div>
-      <div className="w-96 lg:w-6/12 max-w-full">
-        {proposals?.length === 0 && (
-          <div className="text-center">
-            No proposals found, please create a proposal.
-          </div>
-        )}
-        {proposals &&
-          proposals.map((proposal, idx) => {
-            const { title, id, status } = proposal
-            const expires = proposal.expires as Expiration
-
-            return (
-              <ProposalCard
-                key={`${id}_${idx}`}
-                title={title}
-                id={`${id}`}
-                status={status}
-                expires_at={parseInt(expires.at_time)}
-                contractAddress={contractAddress}
-              />
-            )
-          })}
-        {!hideLoadMore && (
-          <button
-            className="btn btn-primary btn-outline text-lg w-full mt-2"
-            onClick={() => {
-              const proposal = proposals && proposals[proposals.length - 1]
-              if (proposal) {
-                setStartBefore(proposal.id)
-              }
-            }}
-          >
-            Load More
-          </button>
-        )}
-      </div>
+      <ProposalList
+        proposals={proposals}
+        contractAddress={contractAddress}
+        hideLoadMore={hideLoadMore}
+        onLoadMore={() => {
+          const proposal = proposals && proposals[proposals.length - 1]
+          if (proposal) {
+            setStartBefore(proposal.id)
+          }
+        }}
+      />
       <div></div>
     </WalletLoader>
   )
