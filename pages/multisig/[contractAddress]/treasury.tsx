494b976b1d6e2eb81e6f4c5ac5fcffaad65f0000
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-11-19 19:35:48 -0800

    Create multisig treasury page.

---
+++ pages/multisig/[contractAddress]/treasury.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import WalletLoader from 'components/WalletLoader'
+import Transfers from 'components/Transfers'
+import TokenBalances from 'components/TokenBalances'
+import {
+  useNativeBalances,
+  useCw20Balances,
+  useTransactions,
+} from 'hooks/treasury'
+
+const Home: NextPage = () => {
+  const router = useRouter()
+  const contractAddress = router.query.contractAddress as string
+
+  const { nativeBalances } = useNativeBalances(contractAddress)
+  const cw20 = useCw20Balances(contractAddress)
+  const { txs } = useTransactions(contractAddress)
+
+  return (
+    <WalletLoader>
+      <div className="text-left my-8 w-full">
+        <TokenBalances
+          native={nativeBalances}
+          cw20={cw20.balances}
+          cw20info={cw20.info}
+        />
+      </div>
+      <div className="text-left w-full">
+        <Transfers txs={txs} />
+      </div>
+    </WalletLoader>
+  )
+}
+
+export default Home
