91b08d9e75a0ec906791d19a44db85cb76531a95
gavin.doughtie <gavin.doughtie@gmail.com>
2021-11-24 16:09:12 -0800

    added codegeneraged files

---
+++ types/dao-contracts/claims_response.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+import { Expiration, Uint128 } from "./shared-types";
+
+export interface ClaimsResponse {
+  claims: Claim[];
+  [k: string]: unknown;
+}
+export interface Claim {
+  amount: Uint128;
+  release_at: Expiration;
+  [k: string]: unknown;
+}
---
+++ types/dao-contracts/total_staked_at_height_response.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+import { Uint128 } from "./shared-types";
+
+export interface TotalStakedAtHeightResponse {
+  height: number;
+  total: Uint128;
+  [k: string]: unknown;
+}
