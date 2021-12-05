78b7f559cda652a2b24c28bd6645303069706b41
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-04 21:44:07 -0800

    Add basic staking hooks

--- hooks/staking.ts
+++ hooks/staking.ts
@@ -4,4 +4,122 @@ import { defaultExecuteFee } from 'util/fee'
 
 export function useStaking(contractAddress: string) {
   const { walletAddress, signingClient } = useSigningClient()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    if (walletAddress.length === 0 || !signingClient) {
+      return
+    }
+    setLoading(true)
+    signingClient
+      ?.queryContractSmart(contractAddress, {
+        staked_balance_at_height: {
+          address: walletAddress,
+        },
+      })
+      .then(console.log)
+      .catch((e) => setError(e.msg))
+    setLoading(false)
+  }, [contractAddress, signingClient, walletAddress])
+
+  const claim = async () => {
+    setError('')
+    signingClient
+      ?.execute(
+        walletAddress,
+        contractAddress,
+        {
+          claim: {},
+        },
+        defaultExecuteFee
+      )
+      .then((response) => {
+        console.log(response)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+        setError(err.message)
+      })
+  }
+
+  const delegateVotes = async (recipient: string) => {
+    setError('')
+    signingClient
+      ?.execute(
+        walletAddress,
+        contractAddress,
+        {
+          delegate_votes: {
+            recipient,
+          },
+        },
+        defaultExecuteFee
+      )
+      .then((response) => {
+        console.log(response)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+        setError(err.message)
+      })
+  }
+
+  const stake = async (amount: string) => {
+    setError('')
+    signingClient
+      ?.execute(
+        walletAddress,
+        contractAddress,
+        {
+          stake: {
+            amount,
+          },
+        },
+        defaultExecuteFee
+      )
+      .then((response) => {
+        console.log(response)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+        setError(err.message)
+      })
+  }
+
+  const unstake = async (amount: string) => {
+    setError('')
+    signingClient
+      ?.execute(
+        walletAddress,
+        contractAddress,
+        {
+          unstake: {
+            amount,
+          },
+        },
+        defaultExecuteFee
+      )
+      .then((response) => {
+        console.log(response)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+        setError(err.message)
+      })
+  }
+
+  return {
+    walletAddress,
+    loading,
+    error,
+    claim,
+    delegateVotes,
+    stake,
+    unstake,
+  }
 }
--- pages/dao/[contractAddress]/staking.tsx
+++ pages/dao/[contractAddress]/staking.tsx
@@ -2,16 +2,42 @@ import type { NextPage } from 'next'
 import WalletLoader from 'components/WalletLoader'
 import { TokenBalance } from 'components/TokenBalances'
 import { useDaoConfig } from 'hooks/dao'
+import { useStaking } from 'hooks/staking'
 import { useDaoCw20BalancesForWallet } from 'hooks/treasury'
 import { useRouter } from 'next/router'
+import LineAlert from 'components/LineAlert'
 
 const Staking: NextPage = () => {
   const router = useRouter()
   const contractAddress = router.query.contractAddress as string
 
   const balances = useDaoCw20BalancesForWallet(contractAddress)
+  const { config, gov_token } = useDaoConfig(contractAddress)
 
-  console.log(balances)
+  const {
+    loading: stakingLoading,
+    error: stakingError,
+    claim,
+    delegateVotes,
+    stake,
+    unstake,
+  } = useStaking(gov_token)
+
+  const handleStake = () => {
+    stake('1000')
+  }
+
+  const handleDelegateVotes = () => {
+    delegateVotes('juno1s4ckh9405q0a3jhkwx9wkf9hsjh66nmu769tz5')
+  }
+
+  const handleUnstake = () => {
+    unstake('1000')
+  }
+
+  const handleClaim = () => {
+    claim()
+  }
 
   return (
     <WalletLoader>
@@ -25,9 +51,23 @@ const Staking: NextPage = () => {
             />
           ))
         : null}
-      <button>Stake</button>
-      <button>Unstake</button>
-      <button>Claim</button>
+      <button className="btn btn-base" onClick={handleStake}>
+        Stake
+      </button>
+      <button className="btn btn-base" onClick={handleDelegateVotes}>
+        Delegate votes
+      </button>
+      <button className="btn btn-base" onClick={handleUnstake}>
+        Unstake
+      </button>
+      <button className="btn btn-base" onClick={handleClaim}>
+        Claim
+      </button>
+      {stakingError && (
+        <div className="mt-8">
+          <LineAlert variant="error" msg={stakingError} />
+        </div>
+      )}
     </WalletLoader>
   )
 }
