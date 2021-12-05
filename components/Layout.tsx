de085d39f6d7444ea1e3ce2d3662b1e27d893e32
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-04 17:21:11 -0800

    Start adding sidebar, revise layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,24 +1,18 @@
 import { ReactNode } from 'react'
 import Head from 'next/head'
-import Footer from './Footer'
-import Nav from './Nav'
+import SidebarLayout from 'components/Sidebar'
 
 const PUBLIC_SITE_TITLE = process.env.NEXT_PUBLIC_SITE_TITLE
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-base-100 text-base-content">
+    <div>
       <Head>
         <title>{PUBLIC_SITE_TITLE}</title>
         <link rel="icon" type="image/svg+xml" href="/daodao-dark.svg" />
         <link rel="icon" href="/yin_yang.png" />
       </Head>
-
-      <Nav />
-      <main className="flex flex-col items-center justify-center w-full flex-1 p-2 md:px-20 text-center">
-        {children}
-      </main>
-      <Footer />
+      <SidebarLayout>{children}</SidebarLayout>
     </div>
   )
 }
