ea25b008300970fa9c31e2ecb0b1dc856cd900d1
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-15 11:36:46 -0800

    Fix vercel script

--- vercel.sh
+++ vercel.sh
@@ -2,9 +2,8 @@
 
 # https://vercel.com/support/articles/how-do-i-use-the-ignored-build-step-field-on-vercel
 
-# Only build main branch
-echo $(git branch --show-current)
-if [[ $(git branch --show-current) ==  "main" ]] ; then
+# Only build main branch (on Vercel this is "master")
+if [[ $(git branch --show-current) ==  "master" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
