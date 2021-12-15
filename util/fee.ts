fec1a19b2dc30aa3c462c7921be898c6af573a3f
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-14 16:10:51 -0800

    Set default gas fee higher, closes #92 (hopefully)

--- util/fee.ts
+++ util/fee.ts
@@ -2,5 +2,5 @@ import { coins, StdFee } from '@cosmjs/stargate'
 
 export const defaultExecuteFee: StdFee = {
   amount: coins(100000, process.env.NEXT_PUBLIC_STAKING_DENOM!),
-  gas: '333333',
+  gas: '666666',
 }
