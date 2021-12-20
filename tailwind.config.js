9bb49e6d0af0172741dd47480b13f66dcc7f347e
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-19 22:30:33 -0800

    Update dependencies, move to tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    options: {
-      safelist: [/data-theme$/],
-    },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  options: {
+    safelist: [/data-theme$/],
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [require('daisyui')],
