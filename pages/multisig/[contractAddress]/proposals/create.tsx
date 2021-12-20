7e3a5419f2f7270cddbb6780b57179da53bdb3d9
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-20 00:26:30 -0800

    Navigate to proposal page after creation on multisig
    Was navigating nowhere to a pointless screen

--- pages/multisig/[contractAddress]/proposals/create.tsx
+++ pages/multisig/[contractAddress]/proposals/create.tsx
@@ -1,4 +1,3 @@
-import LineAlert from 'components/LineAlert'
 import ProposalEditor from 'components/ProposalEditor'
 import WalletLoader from 'components/WalletLoader'
 import type { NextPage } from 'next'
@@ -9,45 +8,30 @@ import { Proposal } from 'models/proposal/proposal'
 const ProposalCreate: NextPage = () => {
   const router = useRouter()
   const contractAddress = router.query.contractAddress as string
-  const ROUTE_PREFIX = `/multisig/${contractAddress}/proposals`
 
   const { walletAddress, loading, error, proposalID, execute } =
     useCreateProposal(contractAddress)
 
-  const handleProposal = (
-    proposal: Proposal,
-    contractAddress: string,
-    govTokenAddress?: string
-  ) => {
-    execute(proposal).then(() => {
-      const paramStr = `initialMessageStatus=success`
-
-      if (proposalID.length > 0)
-        router.push(`${ROUTE_PREFIX}/${proposalID}?${paramStr}`)
-    })
+  const handleProposal = (proposal: Proposal, contractAddress: string) => {
+    execute(proposal)
   }
 
-  const content = proposalID ? (
-    <div>
-      <LineAlert className="mt-2" variant="success" msg="Proposal Saved" />
-      <a
-        href={`${ROUTE_PREFIX}/${proposalID}`}
-      >{`Proposal ${proposalID} saved`}</a>
-    </div>
-  ) : (
-    <ProposalEditor
-      onProposal={handleProposal}
-      error={error}
-      loading={loading}
-      contractAddress={contractAddress}
-      recipientAddress={walletAddress}
-      multisig={true}
-    />
-  )
+  if (proposalID) {
+    router.push(`/multisig/${contractAddress}/proposals/${proposalID}`)
+  }
 
   return (
     <WalletLoader>
-      <div className="flex flex-col w-full">{content}</div>
+      <div className="flex flex-col w-full">
+        <ProposalEditor
+          onProposal={handleProposal}
+          error={error}
+          loading={loading}
+          contractAddress={contractAddress}
+          recipientAddress={walletAddress}
+          multisig={true}
+        />
+      </div>
     </WalletLoader>
   )
 }
