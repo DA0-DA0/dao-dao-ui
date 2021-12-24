538eb742770f331d293f41bf2813e69062f7b995
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-12-24 17:45:10 -0500

    Cleanup, leaving old selectors with new keys.

--- selectors/cosm.ts
+++ selectors/cosm.ts
@@ -10,26 +10,21 @@ const CHAIN_RPC_ENDPOINT = process.env.NEXT_PUBLIC_CHAIN_RPC_ENDPOINT || ''
 const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID
 
 export const stargateClient = selector({
-  key: 'StargateClient',
+  key: 'StargateClient1',
   get: async () => {
     return await StargateClient.connect(CHAIN_RPC_ENDPOINT)
   },
 })
 
 export const cosmWasmClient = selector({
-  key: 'CosmWasmClient',
+  key: 'CosmWasmClient1',
   get: async () => {
     return await CosmWasmClient.connect(CHAIN_RPC_ENDPOINT)
   },
 })
 
 export const offlineSigner = atom({
-  key: 'OfflineSigner',
-  default: null,
-})
-
-export const offlineSigner2 = atom({
-  key: 'OfflineSigner2',
+  key: 'OfflineSigner1',
   default: null,
 })
 
@@ -43,30 +38,13 @@ const getWaitKeplr = async () => {
   return (window as any).keplr.getOfflineSigner(CHAIN_ID)
 }
 
-export const currentUserIDState = atom({
-  key: 'CurrentUserID',
-  default: selector({
-    key: 'CurrentUserID/Default',
-    get: () => getWaitKeplr(),
-  }),
-})
-
-export const currentWalletAddress = selector({
-  key: 'WalletAddress3',
-  get: async ({ get }) => {
-    const client = get(currentUserIDState)
-    const [{ address }] = await client.getAccounts()
-    return address
-  },
-})
-
 export const kelprOfflineSigner = selector({
-  key: 'KeplrOfflineSigner',
+  key: 'KeplrOfflineSigner1',
   get: () => getWaitKeplr(),
 })
 
 export const cosmWasmSigningClient = selector({
-  key: 'SigningCosmWasmClient',
+  key: 'SigningCosmWasmClient1',
   get: async ({ get }) => {
     const offlineSigner = get(kelprOfflineSigner)
     return await SigningCosmWasmClient.connectWithSigner(
@@ -77,21 +55,10 @@ export const cosmWasmSigningClient = selector({
 })
 
 export const walletAddress = selector({
-  key: 'WalletAddress2',
+  key: 'WalletAddress1',
   get: async ({ get }) => {
     const client = get(kelprOfflineSigner)
     const [{ address }] = await client.getAccounts()
     return address
   },
-  set: (_asdf, newValue) => newValue,
-})
-
-export const userOfflineSigner = atom({
-  key: 'UserOfflineSigner',
-  default: null,
-})
-
-export const userWalletAddress = atom({
-  key: 'WalletAddress6',
-  default: '',
 })
