4042c6c806949d48cd5dbcd9ac01f525b2c91b2c
Zeke Medley <zekemedley@gmail.com>
2022-01-25 15:31:21 -0800

    Use currentColor to fill the message icon
    This fixes contrast issues when in dark mode

--- components/icons/Message.tsx
+++ components/icons/Message.tsx
@@ -12,7 +12,7 @@ const SvgMessage = (props: SVGProps<SVGSVGElement>) => (
   >
     <path
       d="M14.66 2.666c0-.733-.593-1.333-1.327-1.333H2.667c-.734 0-1.334.6-1.334 1.333v8c0 .734.6 1.334 1.334 1.334H12l2.667 2.666-.007-12Zm-1.327 0v8.78l-.78-.78H2.667v-8h10.666ZM4 8h8v1.333H4V8Zm0-2h8v1.333H4V6Zm0-2h8v1.333H4V4Z"
-      fill="#323232"
+      fill="currentColor"
     />
   </svg>
 )
