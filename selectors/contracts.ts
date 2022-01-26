f0a02931752bb863b918b2fe934bfd0984a10114
Eliot Baker <430383+ebaker@users.noreply.github.com>
2022-01-26 11:21:59 -0800

    Add allContractsByCodeId selector to query contracts beyond 100 limit. #280

--- selectors/contracts.ts
+++ selectors/contracts.ts
@@ -1,5 +1,12 @@
 import { cosmWasmClient } from 'selectors/cosm'
 import { selectorFamily } from 'recoil'
+import { WasmExtension } from '@cosmjs/cosmwasm-stargate'
+import { toHex } from '@cosmjs/encoding'
+import { QueryClient } from '@cosmjs/stargate'
+import {
+  QueryCodesResponse,
+  QueryContractsByCodeResponse,
+} from 'cosmjs-types/cosmwasm/wasm/v1/query'
 
 export const contractsByCodeId = selectorFamily({
   key: 'contractsByCodeId',
@@ -12,5 +19,39 @@ export const contractsByCodeId = selectorFamily({
       }
       let contracts = await client.getContracts(codeId)
       return contracts
+    },
+})
+
+export const allContractsByCodeId = selectorFamily({
+  key: 'contractsByCodeId',
+  get:
+    (codeId: number) =>
+    async ({ get }) => {
+      const client = get(cosmWasmClient)
+      if (!client) {
+        return []
+      }
+
+      const queryClient: QueryClient & WasmExtension = (
+        client as any
+      ).forceGetQueryClient()
+
+      const all = []
+
+      try {
+        let startAtKey: Uint8Array | undefined = undefined
+        do {
+          const response: QueryContractsByCodeResponse =
+            await queryClient.wasm.listContractsByCodeId(codeId, startAtKey)
+          const { contracts, pagination } = response
+
+          all.unshift(...contracts)
+          startAtKey = pagination?.nextKey
+        } while (startAtKey?.length !== 0)
+      } catch (_e: any) {
+        return get(contractsByCodeId(codeId))
+      }
+
+      return all
     },
 })
