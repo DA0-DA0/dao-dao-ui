2faf645f115b6a2835bc5c6c5cc6964e483b9192
Sagar Saija <sagarsaija@yahoo.com>
2022-02-01 13:01:05 -0800

    update to use chevron arrow icons with tailwind

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,55 +1,18 @@
 import { ReactNode } from 'react'
 import { useRecoilState } from 'recoil'
 import { sidebarExpandedAtom } from 'atoms/sidebar'
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/outline'
 
 export function Sidebar({ children }: { children: ReactNode }) {
   const [expanded, setExpanded] = useRecoilState(sidebarExpandedAtom)
 
-  const collapsedArrowClass = expanded ? (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M6 18L18 6M6 6l12 12"
-      />
-    </svg>
-  ) : (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M15 19l-7-7 7-7"
-      />
-    </svg>
-  )
+  const collapsedArrowClass = expanded ? <ChevronRightIcon className="h-5" /> : <ChevronLeftIcon className="h-5" />
 
   if (!expanded) {
     return (
       <button
         onClick={() => setExpanded((e) => !e)}
-        style={{
-          color: 'grey',
-          cursor: 'pointer',
-          fontSize: '25px',
-          lineHeight: '1',
-          position: 'fixed',
-          top: '6%',
-          right: '0.75%',
-        }}
+        className="text-secondary text-white fixed top-4 right-3 p-2 dark:bg-slate-800 rounded-lg"
       >
         {collapsedArrowClass}
       </button>
@@ -60,16 +23,7 @@ export function Sidebar({ children }: { children: ReactNode }) {
     <>
       <button
         onClick={() => setExpanded((e) => !e)}
-        className="expanded"
-        style={{
-          color: 'grey',
-          cursor: 'pointer',
-          fontSize: '25px',
-          lineHeight: '1',
-          position: 'fixed',
-          top: '2%',
-          right: '0.75%',
-        }}
+        className="text-secondary text-white fixed top-4 right-3 p-2 dark:bg-slate-800 rounded-lg"
       >
         {collapsedArrowClass}
       </button>
