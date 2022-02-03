1da52c196663875ebc7339b4b44075fc1dbca379
Zeke Medley <zekemedley@gmail.com>
2022-02-03 15:25:16 -0800

    Improve error messages for some common errors

--- util/cleanChainError.ts
+++ util/cleanChainError.ts
@@ -5,5 +5,17 @@ export function cleanChainError(error: string): string {
   // error or just the actual error. In either case the error in
   // question is just the last line of the returned string.
   const lines = error.split('\n')
-  return lines[lines.length - 1]
+  const errorLine = lines[lines.length - 1]
+
+  if (errorLine.startsWith('Account does not exist on chain')) {
+    return 'One of the accounts in this transaction does not exist on chain. Double check that they all have received tokens in the past.'
+  }
+  if (errorLine.includes('account sequence mismatch')) {
+    return 'You have a different transaction pending. Please wait a little and then try again.'
+  }
+  if (errorLine.toLowerCase().includes('unauthorized')) {
+    return 'Unauthorized. Double check that you have staked governance tokens.'
+  }
+
+  return errorLine
 }
