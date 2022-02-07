74f16a628d92508ececf7bdf0bff710299cbae77
Zeke Medley <zekemedley@gmail.com>
2022-02-07 13:55:55 -0800

    Stop namespacing favorited contracts based on the code ID
    This was a relic of the past when we were making backwards
    incompatible changes on the testnet. Whereas previously we would
    namespace based on the code ID, we now namespace based on a series of
    backwards compatible ones. Should we make a non-backwards compatible
    change we can update the `NEXT_PUBLIC_FAVORITES_NAMESPACE` to that
    code id.

--- atoms/pinned.ts
+++ atoms/pinned.ts
@@ -1,11 +1,16 @@
 import { atom, AtomEffect } from 'recoil'
 
-const CODE_ID = process.env.NEXT_PUBLIC_DAO_CONTRACT_CODE_ID as string
+// As a relic of when we were doing non-backwards compatible changes on the
+// testnet we used to namespace a user's favorited DAOs / Multisigs. That
+// namespacing used to occur based on the DAO code ID what was being used so we
+// now lock the namespace to the last DAO code ID before we got stopped removing
+// favorites when the contracts were updated.
+const NAMESPACE = process.env.NEXT_PUBLIC_FAVORITES_NAMESPACE as string
 
 const localStorageEffect: <T>(key: string) => AtomEffect<T> =
   (key) =>
   ({ setSelf, onSet, node: _ }) => {
-    const lookup_key = `${CODE_ID}_${key}`
+    const lookup_key = `${NAMESPACE}_${key}`
 
     const savedValue = localStorage.getItem(lookup_key)
     if (savedValue != null) {
