64fdf643973ffaee54c546ab44575ed8cbc00152
Zeke Medley <zekemedley@gmail.com>
2022-02-07 14:15:38 -0800

    Remove redundant localStorageEffect function duplication

--- atoms/pinned.ts
+++ atoms/pinned.ts
@@ -1,4 +1,5 @@
 import { atom, AtomEffect } from 'recoil'
+import { localStorageEffect } from 'atoms/localStorageEffect'
 
 // As a relic of when we were doing non-backwards compatible changes on the
 // testnet we used to namespace a user's favorited DAOs / Multisigs. That
@@ -7,34 +8,16 @@ import { atom, AtomEffect } from 'recoil'
 // favorites when the contracts were updated.
 const NAMESPACE = process.env.NEXT_PUBLIC_FAVORITES_NAMESPACE as string
 
-const localStorageEffect: <T>(key: string) => AtomEffect<T> =
-  (key) =>
-  ({ setSelf, onSet, node: _ }) => {
-    const lookup_key = `${NAMESPACE}_${key}`
-
-    const savedValue = localStorage.getItem(lookup_key)
-    if (savedValue != null) {
-      const json = JSON.parse(savedValue)
-      setSelf(json)
-    }
-
-    onSet((newValue: any, _: any, isReset: boolean) => {
-      if (isReset) {
-        localStorage.removeItem(lookup_key)
-      } else {
-        localStorage.setItem(lookup_key, JSON.stringify(newValue))
-      }
-    })
-  }
-
 export const pinnedDaosAtom = atom<string[]>({
   key: 'pinnedDaoAddresses',
   default: [],
-  effects_UNSTABLE: [localStorageEffect<string[]>('pinnedDaos')],
+  effects_UNSTABLE: [localStorageEffect<string[]>(`${NAMESPACE}_pinnedDaos`)],
 })
 
 export const pinnedMultisigsAtom = atom<string[]>({
   key: 'pinnedMultisigAddresses',
   default: [],
-  effects_UNSTABLE: [localStorageEffect<string[]>('pinnedMultisigs')],
+  effects_UNSTABLE: [
+    localStorageEffect<string[]>(`${NAMESPACE}_pinnedMultisigs`),
+  ],
 })
