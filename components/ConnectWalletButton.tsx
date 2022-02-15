2cb3735d5b3564d38d3e0d4bceca50d94675e233
Zeke Medley <zekemedley@gmail.com>
2022-02-14 19:33:38 -0800

    Upgrade visuals of the wallet connect button.

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -1,8 +1,13 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
 import { useRecoilValue, useRecoilState, useSetRecoilState } from 'recoil'
 
-import { CashIcon } from '@heroicons/react/outline'
+import {
+  CashIcon,
+  CheckCircleIcon,
+  LogoutIcon,
+  PaperClipIcon,
+} from '@heroicons/react/outline'
 
 import { Button } from '@components'
 
@@ -12,9 +17,51 @@ import {
   installWarningVisibleAtom,
   chainWarningVisibleAtom,
   chainDisabledAtom,
+  keplrAccountNameSelector,
+  walletChainBalanceSelector,
 } from 'selectors/cosm'
 import { connectKeplrWithoutAlerts } from 'services/keplr'
+import { NATIVE_DECIMALS, NATIVE_DENOM } from 'util/constants'
+import {
+  convertDenomToHumanReadableDenom,
+  convertMicroDenomToDenomWithDecimals,
+} from 'util/conversion'
+
+import SvgWallet from './icons/Wallet'
+
 const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID
+
+function CopyButton({ text }: { text: string }) {
+  const [copied, setCopied] = useState(false)
+  return (
+    <div className="tooltip tooltip-bottom" data-tip="Copy wallet address">
+      <button
+        type="button"
+        onClick={() => {
+          navigator.clipboard.writeText(text)
+          setTimeout(() => setCopied(false), 2000)
+          setCopied(true)
+        }}
+      >
+        {copied ? (
+          <CheckCircleIcon className="w-[18px]" />
+        ) : (
+          <PaperClipIcon className="w-[18px]" />
+        )}
+      </button>
+    </div>
+  )
+}
+
+function DisconnectButton({ onClick }: { onClick: () => void }) {
+  return (
+    <div className="tooltip tooltip-bottom" data-tip="Disconnect wallet">
+      <button type="button" onClick={onClick}>
+        <LogoutIcon className="w-[18px]" />
+      </button>
+    </div>
+  )
+}
 
 function WalletConnect() {
   const [wallet, setWallet] = useRecoilState(connectedWalletAtom)
@@ -22,6 +69,14 @@ function WalletConnect() {
   const setChainWarningVisible = useSetRecoilState(chainWarningVisibleAtom)
   const setChainDisabled = useSetRecoilState(chainDisabledAtom)
   const walletAddress = useRecoilValue(walletAddressSelector)
+  const walletName = useRecoilValue(keplrAccountNameSelector)
+  const walletBalance = useRecoilValue(walletChainBalanceSelector)
+  const walletBalanceHuman = convertMicroDenomToDenomWithDecimals(
+    walletBalance,
+    NATIVE_DECIMALS
+  )
+  const chainDenomHuman =
+    convertDenomToHumanReadableDenom(NATIVE_DENOM).toUpperCase()
 
   const handleConnect = useCallback(async () => {
     if (!wallet) {
@@ -49,21 +104,36 @@ function WalletConnect() {
     setWallet,
   ])
 
+  if (walletAddress) {
+    return (
+      <div className="transition w-full flex flex-wrap gap-4 items-center justify-between py-2 px-4 my-4 border border-secondary hover:border-accent rounded-lg group">
+        <div className="flex flex-wrap items-center justify-center gap-4">
+          <SvgWallet width="20px" height="20px" fill="currentColor" />
+          <div className="font-mono text-sm">
+            <span>{walletName}</span>
+            <br />
+            <span className="text-accent text-light">
+              {walletBalanceHuman} {chainDenomHuman}
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4 transition opacity-0 group-hover:opacity-100">
+          <CopyButton text={walletAddress} />
+          <DisconnectButton onClick={handleConnect} />
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="flex flex-grow md:flex-grow-0 mt-4">
-      {walletAddress ? (
-        <Button full onClick={handleConnect}>
-          {walletAddress}
-        </Button>
-      ) : (
-        <Button
-          full
-          onClick={handleConnect}
-          iconBefore={<CashIcon className="inline w-4 h-4" />}
-        >
-          Connect wallet
-        </Button>
-      )}
+    <div className="my-4 ">
+      <Button
+        full
+        onClick={handleConnect}
+        iconBefore={<CashIcon className="inline w-4 h-4" />}
+      >
+        Connect wallet
+      </Button>
     </div>
   )
 }
--- selectors/cosm.ts
+++ selectors/cosm.ts
@@ -7,6 +7,7 @@ import {
 import { connectKeplrWithoutAlerts } from '../services/keplr'
 import { walletTokenBalanceUpdateCountAtom } from './treasury'
 import { localStorageEffect } from '../atoms/localStorageEffect'
+import { NATIVE_DENOM } from 'util/constants'
 
 export type WalletConnection = 'keplr' | ''
 
@@ -92,6 +93,33 @@ export const cosmWasmSigningClient = selector({
   // We have to do this because of how SigningCosmWasmClient
   // will update its internal chainId
   dangerouslyAllowMutability: true,
+})
+
+export const walletChainBalanceSelector = selector<number>({
+  key: 'walletChainBalanceSelector',
+  get: async ({ get }) => {
+    const connectedWallet = get(connectedWalletAtom)
+    if (connectedWallet !== 'keplr') {
+      return 0
+    }
+    const client = get(stargateClient)
+    if (!client) {
+      return 0
+    }
+    const address = get(walletAddress)
+    const balance = await client.getBalance(address, NATIVE_DENOM)
+    return Number(balance.amount)
+  },
+})
+
+export const keplrAccountNameSelector = selector<string | undefined>({
+  key: 'keplrAccoutSelector',
+  get: async ({ get }) => {
+    // Invalidate state when the keplr instance changes.
+    get(kelprOfflineSigner)
+    const info = await window.keplr?.getKey(CHAIN_ID as string)
+    return info?.name
+  },
 })
 
 export const walletAddress = selector({
