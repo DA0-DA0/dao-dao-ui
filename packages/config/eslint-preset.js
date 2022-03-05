ebe7fbe86bea8f16dc44e1926c62cae9f86de5ab
ebaker <430383+ebaker@users.noreply.github.com>
2022-03-04 17:07:54 -0800

    Init packages (#407)

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,11 +1,48 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ['next/core-web-vitals'],
+  plugins: ['import'],
+
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
+      rootDir: ['apps/*/', 'packages/*/'],
     },
   },
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'recoil',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '{next,next/**}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+            patternOptions: { nocomment: false },
+          },
+          {
+            pattern: '@components',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', '{next,next/**}', 'recoil'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
-};
+}
