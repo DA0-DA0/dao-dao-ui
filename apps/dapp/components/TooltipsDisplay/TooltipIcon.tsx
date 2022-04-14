d9420997be37da2eee6f977581337eb576a5355a
Griko Nibras <git@griko.id>
2022-04-15 05:26:21 +0700

    chore(dapp): format and lint

--- apps/dapp/components/TooltipsDisplay/TooltipIcon.tsx
+++ apps/dapp/components/TooltipsDisplay/TooltipIcon.tsx
@@ -9,7 +9,7 @@ export const TooltipIcon = forwardRef<HTMLDivElement, TooltipIconProps>(
   function TooltipIcon(props, ref) {
     return (
       <Tooltip {...props} ref={ref}>
-        <InformationCircleIcon className="w-4 h-4 mb-1 text-gray-500 hover:text-gray-400 cursor-help" />
+        <InformationCircleIcon className="mb-1 w-4 h-4 text-gray-500 hover:text-gray-400 cursor-help" />
       </Tooltip>
     )
   }
