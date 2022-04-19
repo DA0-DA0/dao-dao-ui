fc8c3a354301c090922a4db98853ab10c520f53d
Noah Saso <noahsaso@gmail.com>
2022-04-19 12:28:31 -0700

    Added cw20-staked-balance-voting selectors & cw4-voting selectors + hooks.

--- packages/state/hooks/cw-governance.ts
+++ packages/state/hooks/cw-governance.ts
@@ -1,6 +1,38 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
-import { wrapExecuteHook } from './wrapExecuteHook'
+import { useCallback } from 'react'
+
+import { useRecoilValue } from 'recoil'
+
+import { Client as ExecuteClient } from '../clients/cw-governance'
+import {
+  executeClient,
+  ExecuteClientParams,
+} from '../recoil/selectors/cw-governance'
+
+// Get the keys that are functions.
+type FunctionKeyOf<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never
+}[keyof T]
+
+const wrapExecuteHook =
+  <T extends FunctionKeyOf<ExecuteClient>>(fn: T) =>
+  (params: ExecuteClientParams) => {
+    const client = useRecoilValue(executeClient(params))
+
+    return useCallback(
+      (...args: Parameters<ExecuteClient[T]>) => {
+        if (client)
+          return (
+            client[fn] as (
+              ...args: Parameters<ExecuteClient[T]>
+            ) => ReturnType<ExecuteClient[T]>
+          )(...args)
+        throw new Error('Client undefined.')
+      },
+      [client]
+    )
+  }
 
 export const useExecuteProposalHook = wrapExecuteHook('executeProposalHook')
 export const useUpdateConfig = wrapExecuteHook('updateConfig')
---
+++ packages/state/recoil/selectors/cw20-staked-balance-voting.ts
@@ -0,0 +1,118 @@
+import { selectorFamily } from 'recoil'
+
+import {
+  DaoResponse,
+  InfoResponse,
+  QueryClient,
+  StakingContractResponse,
+  TokenContractResponse,
+  TotalPowerAtHeightResponse,
+  VotingPowerAtHeightResponse,
+} from '../../clients/cw20-staked-balance-voting'
+import { cosmWasmClient } from './chain'
+
+type QueryClientParams = {
+  contractAddress: string
+}
+
+const queryClient = selectorFamily<QueryClient | undefined, QueryClientParams>({
+  key: 'cw20StakedBalanceVotingQueryClient',
+  get:
+    ({ contractAddress }) =>
+    ({ get }) => {
+      const client = get(cosmWasmClient)
+      if (!client) return
+
+      return new QueryClient(client, contractAddress)
+    },
+})
+
+export const stakingContractSelector = selectorFamily<
+  StakingContractResponse | undefined,
+  QueryClientParams
+>({
+  key: 'cw20StakedBalanceVotingStakingContract',
+  get:
+    (queryClientParams) =>
+    async ({ get }) => {
+      const client = get(queryClient(queryClientParams))
+      if (!client) return
+
+      return await client.stakingContract()
+    },
+})
+
+export const daoSelector = selectorFamily<
+  DaoResponse | undefined,
+  QueryClientParams
+>({
+  key: 'cw20StakedBalanceVotingDao',
+  get:
+    (queryClientParams) =>
+    async ({ get }) => {
+      const client = get(queryClient(queryClientParams))
+      if (!client) return
+
+      return await client.dao()
+    },
+})
+
+export const votingPowerAtHeightSelector = selectorFamily<
+  VotingPowerAtHeightResponse | undefined,
+  QueryClientParams & { params: Parameters<QueryClient['votingPowerAtHeight']> }
+>({
+  key: 'cw20StakedBalanceVotingVotingPowerAtHeight',
+  get:
+    ({ params, ...queryClientParams }) =>
+    async ({ get }) => {
+      const client = get(queryClient(queryClientParams))
+      if (!client) return
+
+      return await client.votingPowerAtHeight(...params)
+    },
+})
+
+export const totalPowerAtHeightSelector = selectorFamily<
+  TotalPowerAtHeightResponse | undefined,
+  QueryClientParams & { params: Parameters<QueryClient['totalPowerAtHeight']> }
+>({
+  key: 'cw20StakedBalanceVotingTotalPowerAtHeight',
+  get:
+    ({ params, ...queryClientParams }) =>
+    async ({ get }) => {
+      const client = get(queryClient(queryClientParams))
+      if (!client) return
+
+      return await client.totalPowerAtHeight(...params)
+    },
+})
+
+export const infoSelector = selectorFamily<
+  InfoResponse | undefined,
+  QueryClientParams
+>({
+  key: 'cw20StakedBalanceVotingInfo',
+  get:
+    (queryClientParams) =>
+    async ({ get }) => {
+      const client = get(queryClient(queryClientParams))
+      if (!client) return
+
+      return await client.info()
+    },
+})
+
+export const tokenContractSelector = selectorFamily<
+  TokenContractResponse | undefined,
+  QueryClientParams
+>({
+  key: 'cw20StakedBalanceVotingTokenContract',
+  get:
+    (queryClientParams) =>
+    async ({ get }) => {
+      const client = get(queryClient(queryClientParams))
+      if (!client) return
+
+      return await client.tokenContract()
+    },
+})
