7856b6a368e1a556e9c8e5b15ba02d9ca992ab95
Zeke Medley <zekemedley@gmail.com>
2022-05-05 16:51:39 -0700

    Add missing selector file.

---
+++ packages/state/recoil/selectors/clients/cw-rewards.ts
@@ -0,0 +1,18 @@
+import { selectorFamily } from 'recoil'
+import { cosmWasmClientSelector } from '../chain'
+
+export const rewardsRateSelector = selectorFamily<string, string>({
+  key: 'rewardsRateSelector',
+  get:
+    (contractAddress: string) =>
+    async ({ get }) => {
+      const client = get(cosmWasmClientSelector)
+      if (!client) return
+
+      const result = await client.queryContractSmart(contractAddress, {
+        info: {},
+      })
+
+      return result.config.reward_rate
+    },
+})
