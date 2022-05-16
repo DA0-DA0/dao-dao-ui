c95aa5f0e1c1275edd845394f34152a11fd16618
Zeke Medley <zekemedley@gmail.com>
2022-05-16 11:43:03 -0700

    Convert action menu to buttons on mobile.

--- apps/dapp/components/starred/ActionMenu.tsx
+++ apps/dapp/components/starred/ActionMenu.tsx
@@ -11,7 +11,7 @@ const ActionItem = ({
   icon: ReactNode
   text: string
 }) => (
-  <div className="py-0.5 px-2 hover:bg-secondary rounded-md link-text">
+  <div className="py-2 px-4 bg-card hover:bg-secondary rounded md:py-0.5 md:px-2 md:bg-transparent link-text">
     <Link href={href}>
       <a className="flex gap-2 items-center">
         {icon}
@@ -22,27 +22,29 @@ const ActionItem = ({
 )
 
 export const ActionMenu = () => (
-  <div className="p-4 bg-primary rounded-md border border-transparent hover:border-btn-secondary">
+  <div className="md:p-4 md:bg-card md:rounded-md md:border md:border-transparent md:hover:border-btn-secondary">
     <div className="flex flex-col gap-1 font-medium md:gap-0 text-md">
-      <ActionItem
-        href="/dao/create"
-        icon={<PlusIcon className="w-4" />}
-        text={'Create a DAO'}
-      />
-      <ActionItem
-        href="/multisig/create"
-        icon={<PlusIcon className="w-4" />}
-        text={'Create a multisig'}
-      />
+      <div className="grid flex-col grid-cols-2 gap-1 md:flex md:gap-0">
+        <ActionItem
+          href="/dao/create"
+          icon={<PlusIcon className="w-4" />}
+          text={'Create a DAO'}
+        />
+        <ActionItem
+          href="/multisig/create"
+          icon={<PlusIcon className="w-4" />}
+          text={'Create a multisig'}
+        />
+      </div>
       <ActionItem
         href="/dao/list"
         icon={<MapIcon className="w-4" />}
-        text={'Explore all DAOs'}
+        text={'Explore DAOs'}
       />
       <ActionItem
         href="/multisig/list"
         icon={<MapIcon className="w-4" />}
-        text={'Explore all multisigs'}
+        text={'Explore multisigs'}
       />
     </div>
   </div>
