2bc73831de01d4c7346c554e62d106e9fca3baba
Noah Saso <noahsaso@gmail.com>
2022-06-17 18:26:28 -0700

    Update .env.development env vars for v1.

--- apps/sda/util/constants.ts
+++ apps/sda/util/constants.ts
@@ -1,19 +1,4 @@
-export const DAO_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_DAO_CONTRACT_CODE_ID as string,
-  10
-)
-
 export const CW20_CODE_ID = parseInt(process.env.NEXT_PUBLIC_CW20_CODE_ID!, 10)
-
-export const STAKE_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_STAKE_CW20_CODE_ID as string,
-  10
-)
-
-export const C4_GROUP_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_C4_GROUP_CODE_ID as string,
-  10
-)
 
 export const SITE_TITLE = process.env.NEXT_PUBLIC_SITE_TITLE as string
 
