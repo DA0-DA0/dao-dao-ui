95beabd588b78c0e41a871cfe8c1780251964306
Noah Saso <noahsaso@gmail.com>
2022-06-20 16:35:49 -0700

    Fixed SDA static prop maker t reference.

--- apps/sda/server/makeGetStaticProps.ts
+++ apps/sda/server/makeGetStaticProps.ts
@@ -1,5 +1,9 @@
+// eslint-disable-next-line regex/invalid
+import { StringMap, TFunctionKeys, TOptions } from 'i18next'
 import type { GetStaticProps } from 'next'
 
+// eslint-disable-next-line regex/invalid
+import { _probablyDontUseThisI18n } from '@dao-dao/i18n'
 import { serverSideTranslations } from '@dao-dao/i18n/serverSideTranslations'
 import { CwCoreQueryClient } from '@dao-dao/state'
 import { InfoResponse as Cw20StakedBalanceVotingInfoResponse } from '@dao-dao/state/clients/cw20-staked-balance-voting'
@@ -14,6 +18,19 @@ import {
 import { PageWrapperProps } from '@/components'
 import { DAO_ADDRESS } from '@/util'
 
+// Swap order of arguments and use error fallback string if client null.
+// It shouldn't be null as long as we only call this on the server once the
+// translations have been loaded by awaiting `serverSideTranslations`.
+const serverT = (
+  key: TFunctionKeys | TFunctionKeys[],
+  options?: string | TOptions<StringMap> | undefined,
+  defaultValue?: string | undefined
+) =>
+  // Ok to use here as long as it's used after `serverSideTranslations`.
+  // eslint-disable-next-line regex/invalid
+  _probablyDontUseThisI18n?.t(key, defaultValue, options) ??
+  'internal error: translations not loaded'
+
 interface GetStaticPropsMakerProps {
   leadingTitle?: string
   followingTitle?: string
@@ -21,18 +38,20 @@ interface GetStaticPropsMakerProps {
   overrideDescription?: string
 }
 type GetStaticPropsMaker = (
-  props?: GetStaticPropsMakerProps
+  getProps?: (t: typeof serverT) => GetStaticPropsMakerProps
 ) => GetStaticProps<PageWrapperProps>
 
 // Computes PageWrapperProps for the DAO with optional alterations.
 export const makeGetStaticProps: GetStaticPropsMaker =
-  ({ leadingTitle, followingTitle, overrideTitle, overrideDescription } = {}) =>
+  (getProps) =>
   async ({ locale }) => {
     // Don't query chain if running in CI.
     if (CI) {
       return { notFound: true }
     }
 
+    // Run before any `t` call since i18n is not loaded globally on the
+    // server before this is awaited.
     const i18nProps = await serverSideTranslations(locale, ['translation'])
 
     try {
@@ -53,6 +72,16 @@ export const makeGetStaticProps: GetStaticPropsMaker =
       if (!votingModuleType) {
         throw new Error('Failed to determine voting module type.')
       }
+
+      // Must be called after server side translations has been awaited,
+      // because props may use the `t` function, and it won't be available
+      // until after.
+      const {
+        leadingTitle,
+        followingTitle,
+        overrideTitle,
+        overrideDescription,
+      } = getProps?.(serverT) ?? {}
 
       return {
         props: {
