c503f7894a827c8c317ecedb6130a037d05fa07b
Noah Saso <noahsaso@gmail.com>
2022-06-23 08:17:49 -0700

    Removed token image from featured cards and fixed securityDAO image.

--- apps/dapp/components/FeaturedDaos/FeaturedCard.tsx
+++ apps/dapp/components/FeaturedDaos/FeaturedCard.tsx
@@ -37,22 +37,11 @@ export const FeaturedCard: FC<FeaturedCardProps> = ({
     >
       <div className="absolute top-0 left-0 w-full h-[160px] bg-gradient-to-t from-transparent to-dark rounded-lg opacity-[8%] "></div>
       <div
-        className="flex relative justify-center items-center bg-center bg-cover rounded-full"
+        className="relative w-[80px] h-[80px] bg-center bg-cover rounded-full"
         style={{
           backgroundImage: `url(${image})`,
-          width: '80px',
-          height: '80px',
         }}
-      >
-        <div
-          className="absolute -right-[10px] -bottom-1 bg-center rounded-full border border-light"
-          style={{
-            width: '32px',
-            height: '32px',
-            backgroundImage: 'url(/daotoken.jpg)',
-          }}
-        ></div>
-      </div>
+      ></div>
       <h3 className="mt-5 title-text">{name}</h3>
       <p className="mt-2 font-mono text-xs text-center text-secondary break-words line-clamp-3">
         {description}
