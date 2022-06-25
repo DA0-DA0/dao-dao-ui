4013567d07b953033082efeba375d8a26b36a058
Noah Saso <noahsaso@gmail.com>
2022-06-25 13:36:36 -0700

    usePlatform only on browser.

--- packages/utils/hooks/usePlatform.ts
+++ packages/utils/hooks/usePlatform.ts
@@ -3,6 +3,14 @@ import { useEffect, useState } from 'react'
 export const usePlatform = () => {
   const [isMac, setIsMac] = useState(false)
   useEffect(() => {
+    // Only on browser.
+    if (
+      typeof navigator === 'undefined' ||
+      typeof navigator.platform === 'undefined'
+    ) {
+      return
+    }
+
     setIsMac(navigator.platform.toUpperCase().indexOf('MAC') >= 0)
   }, [setIsMac])
 
