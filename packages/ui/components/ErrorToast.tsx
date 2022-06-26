374da6f2044eaf4c0fbe4d7bd2244df2cfeb8173
Jake Hartnell <jake.hartnell@gmail.com>
2022-06-25 19:32:12 -0700

    Toast dismiss button on the right

--- packages/ui/components/ErrorToast.tsx
+++ packages/ui/components/ErrorToast.tsx
@@ -10,13 +10,13 @@ export const ErrorToast: FC<ErrorToastProps> = ({ toast }) => (
   <ToastBar toast={toast}>
     {({ message }) => (
       <div className="flex gap-2 items-center p-3 font-mono text-sm text-[#ffffff] bg-error rounded">
+        {message}
         <button
           className="p-[1px] rounded-full transition-outline"
           onClick={() => hotToast.dismiss(toast.id)}
         >
           <XIcon className="w-4" />
         </button>
-        {message}
       </div>
     )}
   </ToastBar>
--- packages/ui/components/SuccessToast.tsx
+++ packages/ui/components/SuccessToast.tsx
@@ -11,13 +11,13 @@ export const SuccessToast: FC<SuccessToastProps> = ({ toast }) => (
     {({ message }) => {
       return (
         <div className="flex flex-wrap gap-2 items-center p-3 font-mono text-sm text-light bg-dark rounded">
+          {message}
           <button
             className="p-[1px] rounded-full transition-outline"
             onClick={() => hotToast.dismiss(toast.id)}
           >
             <XIcon className="w-4" />
           </button>
-          {message}
         </div>
       )
     }}
