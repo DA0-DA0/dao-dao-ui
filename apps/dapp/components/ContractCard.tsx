6c3fa07009b6f3271cd95c455600d927255b2d21
Noah Saso <noahsaso@gmail.com>
2022-06-27 12:37:51 -0700

    Loading DAO Card and Simplify search modal (#662)
    * Spin DAO image when selected to indicate its page is being loaded and navigation will happen.
    
    * Scroll searched DAO card into view on key nav.
    
    * Made search modal only one dimension instead of two to prevent complicated arrow nav.
    
    * Changed all `animate-spin` to `animate-spin-medium` for chiller vibes.

--- apps/dapp/components/ContractCard.tsx
+++ apps/dapp/components/ContractCard.tsx
@@ -2,7 +2,7 @@ import { HeartIcon as HeartIconOutline } from '@heroicons/react/outline'
 import { HeartIcon as HeartIconSolid } from '@heroicons/react/solid'
 import clsx from 'clsx'
 import Link from 'next/link'
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useState } from 'react'
 
 import { useTranslation } from '@dao-dao/i18n'
 import { Dao, Pencil, Votes } from '@dao-dao/icons'
@@ -24,6 +24,7 @@ interface ContractCardBaseProps {
   balance?: string
   children: ReactNode
   selected?: boolean
+  setLoading: (loading: boolean) => void
 }
 
 const ContractCardBase: FC<ContractCardBaseProps> = ({
@@ -35,12 +36,13 @@ const ContractCardBase: FC<ContractCardBaseProps> = ({
   balance,
   children,
   selected,
+  setLoading,
 }) => {
   const { t } = useTranslation()
 
   return (
     <Link href={href}>
-      <a>
+      <a onClick={() => setLoading(true)}>
         <div
           className={clsx(
             'flex relative flex-col justify-between items-center p-6 w-[260px] h-[320px] bg-card from-transparent rounded-lg hover:outline-1 hover:outline-brand hover:outline',
@@ -105,6 +107,7 @@ interface ContractCardProps {
   onPin?: Function
   imgUrl?: string | null
   selected?: boolean
+  loading?: boolean
 }
 
 export const ContractCard: FC<ContractCardProps> = ({
@@ -118,8 +121,13 @@ export const ContractCard: FC<ContractCardProps> = ({
   onPin,
   imgUrl,
   selected,
+  loading: _loading,
 }) => {
   const { t } = useTranslation()
+
+  // Next.js takes some time to render a DAO page on first load. Let's
+  // indicate to the user something is happening while this is loading.
+  const [loading, setLoading] = useState(false)
 
   return (
     <div className="relative w-min">
@@ -129,21 +137,24 @@ export const ContractCard: FC<ContractCardProps> = ({
         href={href}
         proposals={proposals}
         selected={selected}
+        setLoading={setLoading}
         title={name}
         votingPowerPercent={votingPowerPercent}
       >
-        {imgUrl && CARD_IMAGES_ENABLED ? (
-          <div
-            aria-label={t('info.daosLogo')}
-            className="w-[80px] h-[80px] bg-center bg-cover rounded-full"
-            role="img"
-            style={{
-              backgroundImage: `url(${imgUrl})`,
-            }}
-          ></div>
-        ) : (
-          <Logo alt={name} height={80} width={80} />
-        )}
+        <div className={clsx({ 'animate-spin-medium': _loading || loading })}>
+          {imgUrl && CARD_IMAGES_ENABLED ? (
+            <div
+              aria-label={t('info.daosLogo')}
+              className="w-[80px] h-[80px] bg-center bg-cover rounded-full"
+              role="img"
+              style={{
+                backgroundImage: `url(${imgUrl})`,
+              }}
+            ></div>
+          ) : (
+            <Logo alt={name} height={80} width={80} />
+          )}
+        </div>
       </ContractCardBase>
       <button
         className="absolute top-[18px] right-[18px] text-brand"
@@ -165,7 +176,7 @@ export const LoadingContractCard = () => (
   <div className="flex relative flex-col justify-center items-center p-6 w-[260px]  h-[320px] bg-card from-transparent rounded-lg shadow transition-shadow">
     <div className="absolute top-0 left-0 w-full h-[110px] bg-gradient-to-t from-transparent to-dark rounded-lg opacity-[8%] "></div>
     <div className="flex justify-center items-center w-[70px] h-[70px]">
-      <div className="inline-block animate-spin">
+      <div className="inline-block animate-spin-medium">
         <Logo height={72} width={72} />
       </div>
     </div>
--- packages/ui/components/Loader.tsx
+++ packages/ui/components/Loader.tsx
@@ -2,7 +2,7 @@ import { Logo } from './Logo'
 
 export const Loader = ({ size = 42 }: { size?: number }) => (
   <div className="flex justify-center items-center p-4 w-full h-full">
-    <div className="animate-spin">
+    <div className="animate-spin-medium">
       <Logo height={size} width={size} />
     </div>
   </div>
