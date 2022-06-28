3999b0124acde30944b8959aa94e480e4b8da768
Janita Chalam <janitachalam@gmail.com>
2022-06-28 11:52:39 -0700

    lint

--- packages/state/recoil/selectors/clients/cw-admin-factory.ts
+++ packages/state/recoil/selectors/clients/cw-admin-factory.ts
@@ -1,26 +1,25 @@
-import { selectorFamily } from "recoil"
-import { signingCosmWasmClientSelector } from "../chain"
-import {
-    CwAdminFactoryClient as ExecuteClient
-} from '../../../clients/cw-admin-factory'
+import { selectorFamily } from 'recoil'
+
+import { CwAdminFactoryClient as ExecuteClient } from '../../../clients/cw-admin-factory'
+import { signingCosmWasmClientSelector } from '../chain'
 
 export type ExecuteClientParams = {
-    contractAddress: string
-    sender: string
+  contractAddress: string
+  sender: string
 }
-  
+
 export const executeClient = selectorFamily<
-    ExecuteClient | undefined,
-    ExecuteClientParams
-  >({
-    key: 'cwAdminFactoryExecuteClient',
-    get:
-      ({ contractAddress, sender }) =>
-      ({ get }) => {
-        const client = get(signingCosmWasmClientSelector)
-        if (!client) return
-  
-        return new ExecuteClient(client, sender, contractAddress)
-      },
-    dangerouslyAllowMutability: true,
-  })
+  ExecuteClient | undefined,
+  ExecuteClientParams
+>({
+  key: 'cwAdminFactoryExecuteClient',
+  get:
+    ({ contractAddress, sender }) =>
+    ({ get }) => {
+      const client = get(signingCosmWasmClientSelector)
+      if (!client) return
+
+      return new ExecuteClient(client, sender, contractAddress)
+    },
+  dangerouslyAllowMutability: true,
+})
