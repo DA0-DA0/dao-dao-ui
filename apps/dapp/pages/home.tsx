df077d06954a7275a4f93110cb2895cf767e2cdf
Noah Saso <noahsaso@gmail.com>
2022-06-30 10:35:10 -0700

    Update apps/dapp/pages/home.tsx
    Co-authored-by: ekez <30676292+ezekiiel@users.noreply.github.com>

--- apps/dapp/pages/home.tsx
+++ apps/dapp/pages/home.tsx
@@ -13,7 +13,7 @@ const HomePage: NextPage = () => (
   <>
     <SmallScreenNav />
 
-    <div className="gap-y-6 px-4 md:py-6 md:px-6">
+    <div className="gap-y-6 px-4 md:p-6">
       <PinnedProposalsList />
       <PinnedDAOsList />
       <FeaturedDAOsList />
