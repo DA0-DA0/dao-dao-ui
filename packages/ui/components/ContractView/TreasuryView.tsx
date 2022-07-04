6f2679a33c05176cd1efaac0e8908ec0c286b091
Eliot Baker <430383+ebaker@users.noreply.github.com>
2022-07-04 10:39:01 -0700

    use i18next formatting

--- packages/ui/components/ContractView/TreasuryView.tsx
+++ packages/ui/components/ContractView/TreasuryView.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 
+import { useTranslation } from '@dao-dao/i18n'
 import {
   NATIVE_DENOM,
   convertMicroDenomToDenomWithDecimals,
@@ -32,50 +33,52 @@ export const TreasuryBalances: FC<TreasuryBalancesProps> = ({
   nativeTokens,
   cw20Tokens,
   tvl,
-}) => (
-  <ul className="flex flex-col gap-2 mt-6 list-none">
-    {tvl.amount > 0 && (
-      <BalanceListItem>
-        <span className="text-lg font-bold text-slate-50">{`${new Intl.NumberFormat(
-          'en-US',
-          { style: 'currency', currency: tvl.denom }
-        ).format(tvl.amount)} ${tvl.denom}`}</span>
-      </BalanceListItem>
-    )}
-    {nativeTokens.map(({ denom, amount, decimals }) => {
-      const symbol = nativeTokenLabel(denom)
-      const icon = nativeTokenLogoURI(denom)
-      return (
+}) => {
+  const { t } = useTranslation()
+  return (
+    <ul className="flex flex-col gap-2 mt-6 list-none">
+      {tvl.amount > 0 && (
+        <BalanceListItem>
+          <span className="text-lg font-bold text-slate-50">
+            {t('format.currency', { val: tvl.amount })}
+          </span>
+        </BalanceListItem>
+      )}
+      {nativeTokens.map(({ denom, amount, decimals }) => {
+        const symbol = nativeTokenLabel(denom)
+        const icon = nativeTokenLogoURI(denom)
+        return (
+          <BalanceListItem key={symbol}>
+            <BalanceIcon iconURI={icon} />
+            {convertMicroDenomToDenomWithDecimals(
+              amount,
+              decimals
+            ).toLocaleString(undefined, {
+              maximumFractionDigits: decimals,
+            }) +
+              ' $' +
+              symbol}
+          </BalanceListItem>
+        )
+      })}
+      {!nativeTokens.length && (
+        <BalanceListItem>
+          {/* eslint-disable-next-line i18next/no-literal-string */}
+          <BalanceIcon /> 0 ${nativeTokenLabel(NATIVE_DENOM)}
+        </BalanceListItem>
+      )}
+      {cw20Tokens.map(({ symbol, amount, decimals, imageUrl }) => (
         <BalanceListItem key={symbol}>
-          <BalanceIcon iconURI={icon} />
+          <BalanceIcon iconURI={imageUrl} />
           {convertMicroDenomToDenomWithDecimals(
             amount,
             decimals
           ).toLocaleString(undefined, {
             maximumFractionDigits: decimals,
-          }) +
-            ' $' +
-            symbol}
+          })}{' '}
+          ${symbol}
         </BalanceListItem>
-      )
-    })}
-    {!nativeTokens.length && (
-      <BalanceListItem>
-        {/* eslint-disable-next-line i18next/no-literal-string */}
-        <BalanceIcon /> 0 ${nativeTokenLabel(NATIVE_DENOM)}
-      </BalanceListItem>
-    )}
-    {cw20Tokens.map(({ symbol, amount, decimals, imageUrl }) => (
-      <BalanceListItem key={symbol}>
-        <BalanceIcon iconURI={imageUrl} />
-        {convertMicroDenomToDenomWithDecimals(amount, decimals).toLocaleString(
-          undefined,
-          {
-            maximumFractionDigits: decimals,
-          }
-        )}{' '}
-        ${symbol}
-      </BalanceListItem>
-    ))}
-  </ul>
-)
+      ))}
+    </ul>
+  )
+}
