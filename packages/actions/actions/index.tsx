403f94debc1f75c49f1f3dfa3b06713997e270d7
Noah Saso <noahsaso@gmail.com>
2022-08-08 13:06:23 -0700

    Don't display CW721 actions for now.

--- packages/actions/actions/index.tsx
+++ packages/actions/actions/index.tsx
@@ -1,12 +1,10 @@
 import { Action } from '../types'
 import { addCw20Action } from './AddCw20'
-import { addCw721Action } from './AddCw721'
 import { customAction } from './Custom'
 import { executeAction } from './Execute'
 import { instantiateAction } from './Instantiate'
 import { migrateAction } from './MigrateContract'
 import { removeCw20Action } from './RemoveCw20'
-import { removeCw721Action } from './RemoveCw721'
 import { spendAction } from './Spend'
 import { stakeAction } from './Stake'
 import { updateAdminAction } from './UpdateAdmin'
@@ -18,8 +16,9 @@ export const commonActions: Action[] = [
   updateInfoAction,
   addCw20Action,
   removeCw20Action,
-  addCw721Action,
-  removeCw721Action,
+  // TODO: Add back in once CW721s are displayed.
+  // addCw721Action,
+  // removeCw721Action,
   instantiateAction,
   executeAction,
   migrateAction,
