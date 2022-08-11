85850ef1a08f2c2430ba4d4baad16c530d58b7e6
Noah Saso <noahsaso@gmail.com>
2022-08-10 17:22:02 -0700

    Formatted.

--- apps/dapp/components/HomepageLayout.tsx
+++ apps/dapp/components/HomepageLayout.tsx
@@ -1,6 +1,5 @@
 // GNU AFFERO GENERAL PUBLIC LICENSE Version 3. Copyright (C) 2022 DAO DAO Contributors.
-// See the "LICENSE" file in the root directory of this package for more
-// copyright information.
+// See the "LICENSE" file in the root directory of this package for more copyright information.
 
 import { ReactNode } from 'react'
 
