ec83fcf9110a53b75d04ddf28910459edea010b9
Noah Saso <noahsaso@gmail.com>
2022-08-16 02:30:34 -0700

    Run storyboard generate script on one file.

--- packages/ui/scripts/storybook-generate.ts
+++ packages/ui/scripts/storybook-generate.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-import { Project, SyntaxKind } from 'ts-morph'
+import { Project, SourceFile, SyntaxKind } from 'ts-morph'
 
 const TEMPLATE = `
 import { ComponentMeta, ComponentStory } from '@storybook/react'
@@ -26,108 +26,115 @@ const project = new Project({
 // Only add components.
 project.addSourceFilesAtPaths('components/**/*.{ts,tsx}')
 
-// Add missing stories.
-;(async () => {
-  try {
-    const sourceFiles = project.getSourceFiles()
-    for (const sourceFile of sourceFiles) {
-      const baseName = sourceFile.getBaseNameWithoutExtension()
-      // Ignore index files or story files.
-      if (baseName === 'index' || baseName.endsWith('.stories')) {
+const addMissingStoriesForSourceFile = async (sourceFile: SourceFile) => {
+  const baseName = sourceFile.getBaseNameWithoutExtension()
+  // Ignore index files or story files.
+  if (baseName === 'index' || baseName.endsWith('.stories')) {
+    return
+  }
+
+  const extension = sourceFile.getExtension()
+
+  let titlePrefix = sourceFile
+    .getDirectoryPath()
+    .split('packages/ui/components')[1]
+    // Remove leading slash. Root directory path ends with split
+    // string above, so we have to manually remove the slash while allowing
+    // for it not to exist.
+    .substring(1)
+    .replace(/\//g, ' / ')
+  if (titlePrefix) {
+    titlePrefix += ' / '
+  }
+
+  const componentNames = sourceFile
+    .getVariableDeclarations()
+    .filter(
+      (declaration) =>
+        declaration.isExported() &&
+        // forwardRef'd components, like Button
+        (declaration
+          .getVariableStatement()
+          ?.getChildrenOfKind(SyntaxKind.VariableDeclarationList)[0]
+          ?.getChildrenOfKind(SyntaxKind.SyntaxList)[0]
+          ?.getChildrenOfKind(SyntaxKind.VariableDeclaration)[0]
+          ?.getChildrenOfKind(SyntaxKind.CallExpression)[0]
+          ?.getChildrenOfKind(SyntaxKind.Identifier)[0]
+          ?.getText() === 'forwardRef' ||
+          // Arrow functions, like most components.
+          declaration
+            .getInitializerIfKind(SyntaxKind.ArrowFunction)
+            ?.getReturnType()
+            .getText()
+            .includes('JSX.Element'))
+    )
+    .flatMap((declaration) => declaration.getName())
+    .concat(
+      sourceFile
+        .getFunctions()
+        .filter(
+          (fn) =>
+            // Normal functions.
+            fn.isExported() &&
+            fn.getName() &&
+            fn.getReturnType().getText().includes('JSX.Element')
+        )
+        .map((fn) => fn.getName()!)
+    )
+
+  if (componentNames.length === 1) {
+    const storyFileName = baseName + '.stories' + extension
+    const storyFilePath = path.resolve(
+      sourceFile.getDirectoryPath(),
+      storyFileName
+    )
+
+    // If stories already exist, ignore.
+    if (fs.existsSync(storyFilePath)) {
+      return
+    }
+
+    const componentName = componentNames[0]
+    const data = TEMPLATE.replace(/FILE/g, baseName)
+      .replace(/COMPONENT/g, componentName)
+      .replace(/TITLE/g, titlePrefix + componentName)
+      .trimStart()
+
+    await fs.promises.writeFile(storyFilePath, data)
+  } else {
+    for (const componentName of componentNames) {
+      const storyFileName =
+        baseName + '.' + componentName + '.stories' + extension
+      const storyFilePath = path.resolve(
+        sourceFile.getDirectoryPath(),
+        storyFileName
+      )
+
+      // If stories already exist, ignore.
+      if (fs.existsSync(storyFilePath)) {
         continue
       }
 
-      const extension = sourceFile.getExtension()
+      const data = TEMPLATE.replace(/FILE/g, baseName)
+        .replace(/COMPONENT/g, componentName)
+        .replace(/TITLE/g, titlePrefix + componentName)
+        .trimStart()
 
-      let titlePrefix = sourceFile
-        .getDirectoryPath()
-        .split('packages/ui/components')[1]
-        // Remove leading slash. Root directory path ends with split
-        // string above, so we have to manually remove the slash while allowing
-        // for it not to exist.
-        .substring(1)
-        .replace(/\//g, ' / ')
-      if (titlePrefix) {
-        titlePrefix += ' / '
-      }
-
-      const componentNames = sourceFile
-        .getVariableDeclarations()
-        .filter(
-          (declaration) =>
-            declaration.isExported() &&
-            // forwardRef'd components, like Button
-            (declaration
-              .getVariableStatement()
-              ?.getChildrenOfKind(SyntaxKind.VariableDeclarationList)[0]
-              ?.getChildrenOfKind(SyntaxKind.SyntaxList)[0]
-              ?.getChildrenOfKind(SyntaxKind.VariableDeclaration)[0]
-              ?.getChildrenOfKind(SyntaxKind.CallExpression)[0]
-              ?.getChildrenOfKind(SyntaxKind.Identifier)[0]
-              ?.getText() === 'forwardRef' ||
-              // Arrow functions, like most components.
-              declaration
-                .getInitializerIfKind(SyntaxKind.ArrowFunction)
-                ?.getReturnType()
-                .getText()
-                .includes('JSX.Element'))
-        )
-        .flatMap((declaration) => declaration.getName())
-        .concat(
-          sourceFile
-            .getFunctions()
-            .filter(
-              (fn) =>
-                // Normal functions.
-                fn.isExported() &&
-                fn.getName() &&
-                fn.getReturnType().getText().includes('JSX.Element')
-            )
-            .map((fn) => fn.getName()!)
-        )
-
-      if (componentNames.length === 1) {
-        const storyFileName = baseName + '.stories' + extension
-        const storyFilePath = path.resolve(
-          sourceFile.getDirectoryPath(),
-          storyFileName
-        )
-
-        // If stories already exist, ignore.
-        if (fs.existsSync(storyFilePath)) {
-          continue
-        }
-
-        const componentName = componentNames[0]
-        const data = TEMPLATE.replace(/FILE/g, baseName)
-          .replace(/COMPONENT/g, componentName)
-          .replace(/TITLE/g, titlePrefix + componentName)
-          .trimStart()
-
-        await fs.promises.writeFile(storyFilePath, data)
-      } else {
-        for (const componentName of componentNames) {
-          const storyFileName =
-            baseName + '.' + componentNames + '.stories' + extension
-          const storyFilePath = path.resolve(
-            sourceFile.getDirectoryPath(),
-            storyFileName
-          )
-
-          // If stories already exist, ignore.
-          if (fs.existsSync(storyFilePath)) {
-            continue
-          }
-
-          const data = TEMPLATE.replace(/FILE/g, baseName)
-            .replace(/COMPONENT/g, componentName)
-            .replace(/TITLE/g, titlePrefix + componentName)
-            .trimStart()
-
-          await fs.promises.writeFile(storyFilePath, data)
-        }
-      }
+      await fs.promises.writeFile(storyFilePath, data)
     }
+  }
+}
+
+// Add missing stories.
+;(async () => {
+  try {
+    // Use arg for source files glob pattern if present.
+    const arg = process.argv[2]
+    await Promise.all(
+      (arg ? project.getSourceFiles(arg) : project.getSourceFiles()).map(
+        (sourceFile) => addMissingStoriesForSourceFile(sourceFile)
+      )
+    )
   } catch (e) {
     // Catch anything bad that happens
     console.error('Error', e)
