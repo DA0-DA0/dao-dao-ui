2639fc8cfb428a5e9bd2ca59cfe4b4e1016b004f
Noah Saso <noahsaso@gmail.com>
2022-08-16 02:42:20 -0700

    Storybook webpack5.

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../**/*.stories.@(ts|tsx)'],
   addons: [
     '@storybook/addon-links',
@@ -14,34 +17,4 @@ module.exports = {
     },
   ],
   framework: '@storybook/react',
-  // webpackFinal: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.tsx?$/,
-  //     exclude: /node_modules/,
-  //     use: [
-  //       {
-  //         loader: require.resolve('babel-loader'),
-  //         options: {
-  //           presets: [
-  //             require('@babel/preset-typescript').default,
-  //             [require('@babel/preset-react').default, { runtime: 'automatic' }],
-  //             require('@babel/preset-env').default,
-  //           ],
-  //         },
-  //       },
-  //     ],
-  //   })
-
-  //   config.resolve.extensions.push('.ts', '.tsx')
-
-  //   config.module.rules.push({
-  //     test: /\.mjs$/,
-  //     include: /node_modules/,
-  //     type: 'javascript/auto',
-  //   })
-
-  //   config.resolve.extensions.push('.mjs')
-
-  //   return config
-  // },
 }
