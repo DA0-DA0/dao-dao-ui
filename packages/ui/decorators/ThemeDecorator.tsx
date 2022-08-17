b6289fcf9e9ecebbbf04f75bb41961dc5c16c83a
Noah Saso <noahsaso@gmail.com>
2022-08-17 03:29:42 -0700

    FIx theme decorator making padding disappear.

--- packages/ui/decorators/ThemeDecorator.tsx
+++ packages/ui/decorators/ThemeDecorator.tsx
@@ -43,12 +43,9 @@ export const ThemeDecorator: DecoratorFn = (Story) => {
       updateTheme={setTheme}
     >
       <div
-        className={clsx(
-          'absolute top-0 right-0 bottom-0 left-0 antialiased bg-white body-text',
-          {
-            dark: theme === Theme.Dark,
-          }
-        )}
+        className={clsx({
+          dark: theme === Theme.Dark,
+        })}
       >
         <Story />
       </div>
