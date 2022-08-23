c90f2f4ef723bff3fe8f04efb87d0bb1bdebdb09
bao <baoskee@users.noreply.github.com>
2022-08-23 12:57:03 -0700

    Update packages/ui/components/proposal/ProposalYourVote.tsx
    Co-authored-by: Noah Saso <noahsaso@gmail.com>

--- packages/ui/components/proposal/ProposalYourVote.tsx
+++ packages/ui/components/proposal/ProposalYourVote.tsx
@@ -10,7 +10,7 @@ export const ProposalYourVote = ({ variant }: YourVoteProps) => {
       className={clsx(
         'flex relative justify-center items-center py-1 px-4 w-20 text-sm font-medium rounded-full',
         variant === 'abstain'
-          ? 'text-text-secondary bg-primary'
+          ? 'text-text-secondary bg-background-secondary'
           : variant === 'pending'
           ? 'border-2 border-inactive'
           : variant === 'no'
