8fef37e671cd0593a954d6514b10e83634441966
ebaker <430383+ebaker@users.noreply.github.com>
2022-08-24 12:02:52 -0700

    created ProfileNotMemberCard component (#835)
    * created ProfileNotMemberCard component
    
    * add X icon from svg
    
    * styling fixes
    
    * update light mode text-interactive-disabled
    
    * add dog_nft.png
    
    * refactor into files
    
    * update text-interactive-disabled
    
    * change prop to established
    
    * fix border

---
+++ packages/ui/components/profile/MembershipPill.tsx
@@ -0,0 +1,30 @@
+import { useTranslation } from 'react-i18next'
+
+import { MemberCheck, X } from '@dao-dao/icons'
+
+export interface MembershipPillProps {
+  isMember: boolean
+  daoName: string
+}
+export const MembershipPill = ({ isMember, daoName }: MembershipPillProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <div
+      className={`flex flex-row gap-2 items-center py-1 px-[10px] rounded-full border-2 border-default link-text ${
+        isMember ? '' : 'text-text-interactive-disabled'
+      }`}
+    >
+      {isMember && (
+        <div className="shrink-0">{t('profile.member.memberOf')}</div>
+      )}
+      {isMember && <MemberCheck className="w-[16px]" />}
+      {!isMember && (
+        <div className="shrink-0">
+          {t('profile.notMember.notAMember', { daoName })}
+        </div>
+      )}
+      {!isMember && <X className="w-[16px]" />}
+    </div>
+  )
+}
--- packages/ui/components/profile/ProfileNotMemberCard.tsx
+++ packages/ui/components/profile/ProfileNotMemberCard.tsx
@@ -1,5 +1,67 @@
-export interface ProfileNotMemberCardProps {}
+import { useTranslation } from 'react-i18next'
 
-export const ProfileNotMemberCard = ({}: ProfileNotMemberCardProps) => {
-  return <p>placeholder</p>
+import { ArrowUpRight } from '@dao-dao/icons'
+
+import { Button } from '../Button'
+import { ProfileCardTemplate } from './ProfileCardTemplate'
+
+export interface ProfileNotMemberCardProps {
+  tokenSymbol: string
+  tokenBalance: number
+  daoName: string
+  walletName: string
+  profileImgUrl: string
+  established: Date
+}
+export const ProfileNotMemberCard = ({
+  tokenSymbol,
+  tokenBalance,
+  daoName,
+  walletName,
+  profileImgUrl,
+  established,
+}: ProfileNotMemberCardProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <ProfileCardTemplate
+      daoName={daoName}
+      established={established}
+      imgUrl={profileImgUrl}
+      isMember={false}
+      walletName={walletName}
+    >
+      <div className="flex flex-col items-stretch p-6 mt-7 border-t border-t-border-primary">
+        <div className="link-text text-text-body">
+          {t('profile.notMember.membership')}
+        </div>
+        <p className="pt-1 pb-3 secondary-text">
+          {t('profile.notMember.stakeYourTokens', { tokenSymbol, daoName })}
+        </p>
+        <div className="flex flex-row justify-between pb-7 secondary-text">
+          <div>{t('profile.notMember.yourHoldings')}</div>
+          <div className="font-mono">
+            {t('format.token', { val: tokenBalance, tokenSymbol })}
+          </div>
+        </div>
+        <Button
+          className="mb-2"
+          contentContainerClassName="justify-center primary-text"
+          disabled={true}
+          size="lg"
+          variant="secondary"
+        >
+          {t('profile.notMember.stakeToken', { tokenSymbol })}
+        </Button>
+        <Button
+          contentContainerClassName="justify-center primary-text"
+          size="lg"
+          variant="secondary"
+        >
+          {t('profile.notMember.getTokens')}
+          <ArrowUpRight />
+        </Button>
+      </div>
+    </ProfileCardTemplate>
+  )
 }
