05f7137d1b4c372be3163060ab3cdeb338549567
bao <baoskee@users.noreply.github.com>
2022-08-24 00:09:08 -0700

    ProposalYourVote component (#837)
    * baoskee/ProposalYourVote
    
    * Proposal your vote
    
    * visual updates to match spec
    
    * Smaller py
    
    * Up to spec
    
    * Update packages/ui/components/proposal/ProposalYourVote.tsx
    
    * Update packages/ui/stories/proposal/ProposalYourVote.stories.tsx
    
    * Update packages/ui/components/proposal/ProposalYourVote.tsx
    
    * Update packages/ui/components/proposal/ProposalYourVote.tsx
    
    * Update packages/ui/components/proposal/ProposalYourVote.tsx

--- packages/ui/components/proposal/ProposalYourVote.tsx
+++ packages/ui/components/proposal/ProposalYourVote.tsx
@@ -1,5 +1,27 @@
-export interface ProposalYourVoteProps {}
+import clsx from 'clsx'
 
-export const ProposalYourVote = ({}: ProposalYourVoteProps) => {
-  return <p>placeholder</p>
+export type YourVoteProps = {
+  variant: 'abstain' | 'pending' | 'no' | 'yes'
+}
+
+export const ProposalYourVote = ({ variant }: YourVoteProps) => {
+  return (
+    <div
+      className={clsx(
+        'flex relative justify-center items-center py-1 px-4 w-20 text-sm font-medium rounded-full',
+        variant === 'abstain'
+          ? 'text-text-secondary bg-background-secondary'
+          : variant === 'pending'
+          ? 'border-2 border-border-primary'
+          : variant === 'no'
+          ? 'text-text-interactive-error bg-background-interactive-error'
+          : 'text-text-valid bg-color-valid/[0.08]'
+      )}
+    >
+      {variant[0].toUpperCase() + variant.slice(1)}
+      {variant === 'pending' && (
+        <div className="absolute top-0 right-0 w-2 h-2 bg-[#F53E86] rounded-full"></div>
+      )}
+    </div>
+  )
 }
