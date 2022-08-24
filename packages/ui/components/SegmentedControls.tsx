82003cdf57acece09c584a07a2ee6c7d2319eb13
Noah Saso <noahsaso@gmail.com>
2022-08-24 01:26:12 -0700

    Added ability to display loading for a segmented control tab, and don't show selected and hovering at the same time.

--- packages/ui/components/SegmentedControls.tsx
+++ packages/ui/components/SegmentedControls.tsx
@@ -1,10 +1,12 @@
 import clsx from 'clsx'
+import { useState } from 'react'
 
 import { Button } from './Button'
 
 export interface SegmentedControlsTab {
   name: string
   selected?: boolean
+  loading?: boolean
   onClick: () => void
 }
 
@@ -12,25 +14,57 @@ export interface SegmentedControlsProps {
   tabs: SegmentedControlsTab[]
 }
 
-export const SegmentedControls = ({ tabs }: SegmentedControlsProps) => (
-  <div className="grid grid-flow-col auto-cols-fr bg-background-tertiary">
-    {tabs.map(({ name, onClick, selected }, index) => (
-      <div key={name} className="flex flex-row items-center">
-        {index !== 0 && !tabs[index - 1].selected && !selected && (
-          <div className="w-[1px] h-4 bg-border-primary"></div>
-        )}
+export const SegmentedControls = ({ tabs }: SegmentedControlsProps) => {
+  const [hovering, setHovering] = useState<number>()
 
-        <Button
-          className={clsx(
-            'flex justify-center items-center w-full',
-            selected && 'bg-background-primary body-text'
-          )}
-          onClick={onClick}
-          variant="ghost"
-        >
-          {name}
-        </Button>
-      </div>
-    ))}
-  </div>
-)
+  return (
+    <div
+      className="group grid grid-flow-col auto-cols-fr bg-background-tertiary"
+      onMouseLeave={() => setHovering(undefined)}
+    >
+      {tabs.map(({ name, onClick, selected, loading }, index) => (
+        <div key={name} className="flex flex-row items-center">
+          <div
+            className={clsx(
+              'w-[1px] h-4 bg-border-primary opacity-100 transition-opacity',
+              {
+                // Do not show left border if...
+                '!opacity-0':
+                  // first element.
+                  index === 0 ||
+                  // left tab selected.
+                  tabs[index - 1].selected ||
+                  // current tab selected.
+                  selected ||
+                  // left tab hovering.
+                  hovering === index - 1 ||
+                  // current tab hovering.
+                  hovering === index,
+              }
+            )}
+          ></div>
+
+          <Button
+            className={clsx(
+              'flex justify-center items-center w-full',
+              selected || hovering === index
+                ? // Brighten text when selected or hovering over this tab.
+                  'body-text'
+                : // Dim text when not selected and not hovering over this tab.
+                  'text-text-secondary',
+              // Highlight background when selected and not hovering over any
+              // tab. Button contains its own hover background class.
+              selected && hovering === undefined && 'bg-background-primary'
+            )}
+            loading={loading}
+            onClick={onClick}
+            onMouseOver={() => setHovering(index)}
+            variant="ghost"
+          >
+            {name}
+          </Button>
+        </div>
+      ))}
+    </div>
+  )
+}
