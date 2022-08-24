b40f5a5ce7dc753e525391b9f0cc6c418bbdc676
Noah Saso <noahsaso@gmail.com>
2022-08-24 01:41:07 -0700

    Improved ProposalYourVote formatting and renamed props interface.

--- packages/ui/components/proposal/ProposalYourVote.tsx
+++ packages/ui/components/proposal/ProposalYourVote.tsx
@@ -1,27 +1,25 @@
 import clsx from 'clsx'
 
-export type YourVoteProps = {
+export interface ProposalYourVoteProps {
   variant: 'abstain' | 'pending' | 'no' | 'yes'
 }
 
-export const ProposalYourVote = ({ variant }: YourVoteProps) => {
-  return (
-    <div
-      className={clsx(
-        'flex relative justify-center items-center py-1 px-4 w-20 text-sm font-medium rounded-full',
-        variant === 'abstain'
-          ? 'text-text-secondary bg-background-secondary'
-          : variant === 'pending'
-          ? 'border-2 border-border-primary'
-          : variant === 'no'
-          ? 'text-text-interactive-error bg-background-interactive-error'
-          : 'text-text-interactive-valid bg-background-interactive-valid'
-      )}
-    >
-      {variant[0].toUpperCase() + variant.slice(1)}
-      {variant === 'pending' && (
-        <div className="absolute top-0 right-0 w-2 h-2 bg-[#F53E86] rounded-full"></div>
-      )}
-    </div>
-  )
-}
+export const ProposalYourVote = ({ variant }: ProposalYourVoteProps) => (
+  <div
+    className={clsx(
+      'flex relative justify-center items-center py-1 px-4 w-20 text-sm font-medium rounded-full',
+      variant === 'abstain'
+        ? 'text-text-secondary bg-background-secondary'
+        : variant === 'pending'
+        ? 'border-2 border-border-primary'
+        : variant === 'no'
+        ? 'text-text-interactive-error bg-background-interactive-error'
+        : 'text-text-interactive-valid bg-background-interactive-valid'
+    )}
+  >
+    {variant[0].toUpperCase() + variant.slice(1)}
+    {variant === 'pending' && (
+      <div className="absolute top-0 right-0 w-2 h-2 bg-[#F53E86] rounded-full"></div>
+    )}
+  </div>
+)
