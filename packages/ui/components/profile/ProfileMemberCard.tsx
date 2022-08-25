3b10a5460752776d2b3f60c98b85c7176bc70eca
elsehow <else.how@else.how>
2022-08-25 11:54:24 -0700

    member card layout

--- packages/ui/components/profile/ProfileMemberCard.tsx
+++ packages/ui/components/profile/ProfileMemberCard.tsx
@@ -1,5 +1,117 @@
-export interface ProfileMemberCardProps {}
+import { useTranslation } from 'react-i18next'
 
-export const ProfileMemberCard = ({}: ProfileMemberCardProps) => {
-  return <p>placeholder</p>
+import { Button } from '../Button'
+import {
+  convertMicroDenomToDenomWithDecimals,
+} from '@dao-dao/utils'
+
+import { ProfileCardWrapper } from './ProfileCardWrapper'
+
+// Represents a tranch of tokens that is currently unstaking and will become
+// available at some well-known later date.
+export interface UnstakingTokensTranch {
+  tokenSymbol: string
+  unstakingTokens: number
+  available: Date
+}
+
+export interface ProfileMemberCardProps {
+  votingPower: number
+  daoName: string
+  walletName: string
+  profileImgUrl: string
+  tokenSymbol: string
+  stakedTokens: number
+  tokenDecimals: number
+  unstakingTokensTranches: UnstakingTokensTranch[]
+  unstakedTokens: number
+}
+
+export const ProfileMemberCard = ({
+  votingPower,
+  daoName,
+  walletName,
+  profileImgUrl,
+  tokenSymbol,
+  stakedTokens,
+  tokenDecimals,
+  unstakingTokensTranches,
+  unstakedTokens,
+}: ProfileMemberCardProps) => {
+  const { t } = useTranslation()
+ // TODO translate titles
+  return (
+    <ProfileCardWrapper
+      childContainerClassName="p-0 border-t-0"
+      daoName={daoName}
+      imgUrl={profileImgUrl}
+      isMember
+      walletName={walletName}
+    >
+
+      <div className = "py-3 px-6 border-t border-t-border-primary">
+        <div className="flex flex-row justify-between items-center pb-3 text-xs text-bold">
+          <p>Contribution</p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center secondary-text pb-3">
+          <p>Staked tokens</p>
+          <p className="font-mono text-text-primary">
+          {stakedTokens.toLocaleString(undefined, {
+            maximumFractionDigits: tokenDecimals,
+          })}{' '} ${tokenSymbol}</p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center secondary-text pb-3">
+          <p>{t('title.votingPower')}</p>
+          <p className="font-mono text-text-primary">{votingPower}%</p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center secondary-text pb-3">
+          <p>Unstaking tokens</p>
+          <p className="font-mono text-text-primary">1 tranch</p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center pb-6">
+          <p className="secondary-text">Available tokens</p>
+          <p className="font-mono text-xs text-icon-interactive-valid">
+          {unstakedTokens.toLocaleString(undefined, {
+            maximumFractionDigits: tokenDecimals,
+          })}{' '} ${tokenSymbol}</p>
+        </div>
+
+        <Button
+          className="mb-2 w-full"
+          contentContainerClassName="justify-center"
+          size="lg"
+          variant="primary"
+        >
+          {`Claim your ${unstakedTokens.toLocaleString(undefined, {
+            maximumFractionDigits: tokenDecimals,
+          })} $${tokenSymbol}` }
+        </Button>
+
+        <Button
+          className="mb-0 w-full"
+          contentContainerClassName="justify-center primary-text"
+          size="lg"
+          variant="secondary"
+        >
+         {`Manage $${tokenSymbol} stake`}
+        </Button>
+      </div>
+
+      <div className = "py-3 px-6 border-t border-t-border-primary">
+        <Button
+          className='w-full'
+          contentContainerClassName="justify-center primary-text"
+          size="lg"
+          variant="secondary"
+        >
+         Open proposals
+        </Button>
+      </div>
+
+    </ProfileCardWrapper>
+  )
 }
--- packages/ui/stories/profile/ProfileMemberCard.stories.tsx
+++ packages/ui/stories/profile/ProfileMemberCard.stories.tsx
@@ -8,11 +8,29 @@ export default {
 } as ComponentMeta<typeof ProfileMemberCard>
 
 const Template: ComponentStory<typeof ProfileMemberCard> = (args) => (
-  <ProfileMemberCard {...args} />
+  <div className="max-w-[320px]">
+    <ProfileMemberCard {...args} />
+  </div>
 )
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  votingPower: 34.2,
+  daoName: 'Dog Dao',
+  walletName: '@Modern-Edamame',
+  profileImgUrl: '/dog_nft.png',
+  tokenSymbol: 'DOG',
+  stakedTokens: 50,
+  tokenDecimals: 1,
+  unstakingTokensTranches: [
+    {
+      tokenSymbol: '$DOG',
+      unstakingTokens: 50,
+      available: new Date(),
+    },
+  ],
+  unstakedTokens: 45,
+}
 
 Default.parameters = {
   design: {
