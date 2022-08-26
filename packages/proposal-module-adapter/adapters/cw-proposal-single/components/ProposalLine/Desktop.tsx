056877c0289eefb4f1fca3e2577303aae8ba150d
Janita Chalam <janitachalam@gmail.com>
2022-08-26 12:40:01 -0700

    yarn format

--- packages/proposal-module-adapter/adapters/cw-proposal-single/components/ProposalLine/Desktop.tsx
+++ packages/proposal-module-adapter/adapters/cw-proposal-single/components/ProposalLine/Desktop.tsx
@@ -38,32 +38,35 @@ export const ProposalLineDesktop = ({ className }: BaseProposalLineProps) => {
   const proposalModuleVersion = useRecoilValue(
     contractVersionSelector(proposalModuleAddress)
   )
-  const contents = (<div
-    className={clsx(
-      'grid grid-cols-6 items-center p-4 text-sm rounded-lg transition bg-primary hover:bg-secondary',
-      {
-        'bg-gray-500/30': msSinceUpdated < 24 * 60 * 60 * 1000,
-        'bg-card': proposal.status === Status.Open,
-        'bg-disabled': proposal.status !== Status.Open,
-      },
-      className
-    )}
-  >
-    <div className="flex flex-row flex-wrap col-span-2 gap-4 items-center">
-      <p className="font-mono caption-text">
-        <ProposalIdDisplay
-          proposalNumber={proposalNumber}
-          proposalPrefix={proposalPrefix}
-        />
-      </p>
-      <ProposalStatus status={proposal.status} />
+  const contents = (
+    <div
+      className={clsx(
+        'grid grid-cols-6 items-center p-4 text-sm rounded-lg transition bg-primary hover:bg-secondary',
+        {
+          'bg-gray-500/30': msSinceUpdated < 24 * 60 * 60 * 1000,
+          'bg-card': proposal.status === Status.Open,
+          'bg-disabled': proposal.status !== Status.Open,
+        },
+        className
+      )}
+    >
+      <div className="flex flex-row flex-wrap col-span-2 gap-4 items-center">
+        <p className="font-mono caption-text">
+          <ProposalIdDisplay
+            proposalNumber={proposalNumber}
+            proposalPrefix={proposalPrefix}
+          />
+        </p>
+        <ProposalStatus status={proposal.status} />
+      </div>
+      <p className="col-span-3 truncate link-text">{proposal.title}</p>
+      <p className="text-right truncate body-text">{expirationString}</p>
     </div>
-    <p className="col-span-3 truncate link-text">{proposal.title}</p>
-    <p className="text-right truncate body-text">{expirationString}</p>
-  </div>)
-  
-  return (
-    proposalModuleVersion === ContractVersion.V0_2_0 ? (
-    <Tooltip label={`Last updated: ${localDateString}`}>{contents}</Tooltip>) : (contents)
+  )
+
+  return proposalModuleVersion === ContractVersion.V0_2_0 ? (
+    <Tooltip label={`Last updated: ${localDateString}`}>{contents}</Tooltip>
+  ) : (
+    contents
   )
 }
