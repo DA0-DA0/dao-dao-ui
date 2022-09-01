33c50a1ebc98446ca57dff4d5e1970f068adc6eb
Noah Saso <noahsaso@gmail.com>
2022-08-31 20:57:49 -0700

    Fixed tooltip width constraint and added arrow by default.

--- packages/ui/components/Tooltip.tsx
+++ packages/ui/components/Tooltip.tsx
@@ -1,23 +1,33 @@
-import { Tooltip as MaterialTooltip } from '@mui/material'
+import {
+  Tooltip as MaterialTooltip,
+  TooltipProps as MaterialTooltipProps,
+} from '@mui/material'
 import { ReactElement, ReactNode } from 'react'
 
-export interface TooltipProps {
+export interface TooltipProps extends MaterialTooltipProps {
   label: ReactNode | undefined
   children: ReactElement
 }
 
-export const Tooltip = ({ label, children }: TooltipProps) =>
-  !!label ? (
+export const Tooltip = ({
+  label,
+  children,
+  classes,
+  arrow,
+  ...props
+}: TooltipProps) =>
+  label ? (
     <MaterialTooltip
+      arrow={arrow ?? true}
       classes={{
+        ...classes,
+        arrow: classes?.arrow ?? '!text-component-tooltip',
         tooltip:
-          '!text-xs !font-medium !text-text-body !bg-component-tooltip !rounded-md !border !border-border-primary',
+          classes?.tooltip ??
+          '!font-sans !text-xs !font-normal !text-text-body !bg-component-tooltip !rounded-md !border !border-border-primary',
       }}
-      componentsProps={{
-        popper: {
-          disablePortal: true,
-        },
-      }}
+      open
+      {...props}
       title={label}
     >
       {children}
