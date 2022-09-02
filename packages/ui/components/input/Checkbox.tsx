cab7c7ca79aeab72df67ce2efa28b2ef85d5bbb0
Noah Saso <noahsaso@gmail.com>
2022-09-02 14:34:07 -0700

    Fixed checkbox border styling.

--- packages/ui/components/input/Checkbox.tsx
+++ packages/ui/components/input/Checkbox.tsx
@@ -16,17 +16,17 @@ export const Checkbox = ({
 }: CheckboxProps) => (
   <div
     className={clsx(
-      'inline-flex justify-center items-center rounded-md border border-border-primary transition-all',
+      'group inline-flex justify-center items-center rounded-md outline-1 outline-border-primary outline transition-all',
       checked ? 'bg-component-pill' : 'bg-background-button',
       !readOnly &&
-        'hover:bg-background-button-hover active:bg-background-button-pressed active:ring ring-inset ring-border-primary cursor-pointer',
+        'hover:bg-background-button-hover active:bg-background-button-pressed active:outline-2 cursor-pointer',
       className
     )}
     onClick={readOnly ? undefined : onClick}
   >
     <Check
       className={clsx(
-        'w-5 h-5 text-icon-primary hover:text-icon-button-primary transition-all',
+        'w-5 h-5 text-icon-primary group-hover:text-icon-button-primary transition-all',
         checked ? 'opacity-100' : 'opacity-0'
       )}
     />
