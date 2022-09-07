acf9ae63da93d830990d9081081e21a9279c4ef7
Noah Saso <noahsaso@gmail.com>
2022-09-07 01:33:33 -0700

    Added comments explaining adapter core lazy load adapter situation.

--- packages/proposal-module-adapter/core.ts
+++ packages/proposal-module-adapter/core.ts
@@ -8,6 +8,14 @@ import {
   ProposalModuleAdapter,
 } from './types'
 
+// Adapters need to be loaded lazily like this, as opposed to just defining a
+// global array, due to cyclic dependencies. The adapter defintion files include
+// components, which include the react folder index, which includes the provider
+// file, which includes the core because it uses the matching helpers below,
+// which depend on this adapter list. The fix is that no internal components
+// should have a dependency chain that leads back to the matching functions
+// below, except the react provider, which we should only be used externally.
+// This is a problem to solve later.
 export const getAdapters = (): readonly ProposalModuleAdapter[] => [
   CwProposalSingleAdapter,
 ]
