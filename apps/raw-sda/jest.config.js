ab7eeb0ac2fbb6dc8cf9840fb8ea67b950752c62
Noah Saso <noahsaso@gmail.com>
2022-10-10 18:01:54 -0700

    Removed RAW SDA from apps.

--- apps/raw-sda/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  moduleDirectories: ['node_modules', '<rootDir>'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-}
--- apps/raw-sda/mission.md
+++
@@ -1,3 +0,0 @@
-## Raw DAO&apos;s mission statement
-
-RAW DAO governing the JunoSwap AMM [@junoswapdex](https://twitter.com/junoswapdex)
