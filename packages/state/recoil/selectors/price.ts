a07e691c09c52922a118da6d2b8324676c3b96c7
Noah Saso <noahsaso@gmail.com>
2022-10-13 19:22:37 -0700

    Fixed price calculation when app not on juno mainnet chain, and improved DAO TVL to take into account all native tokens.

--- packages/state/recoil/selectors/price.ts
+++ packages/state/recoil/selectors/price.ts
@@ -3,19 +3,20 @@ import { selectorFamily } from 'recoil'
 
 import { WithChainId } from '@dao-dao/tstypes'
 import {
-  NATIVE_DECIMALS,
-  NATIVE_DENOM,
   USDC_SWAP_ADDRESS,
   convertMicroDenomToDenomWithDecimals,
 } from '@dao-dao/utils'
 
 import { refreshTokenUsdcPriceIdAtom } from '../atoms/refresh'
-import { cosmWasmClientForChainSelector, nativeBalanceSelector } from './chain'
+import { cosmWasmClientForChainSelector, nativeBalancesSelector } from './chain'
 import { CwdCoreV2Selectors } from './clients'
 import { junoswapPoolsListSelector } from './pools'
 
 // TODO(multichain): Figure out how to match CW20s on other chains to CW20s in
 // Junoswap pools so we can check prices.
+
+const BASE_SWAP_DENOM = 'ujuno'
+const BASE_SWAP_DECIMALS = 6
 
 // Gets the price of a token in USDC / TOKEN. DENOM may either be a native
 // denomination or the address of a cw20 token. Price data is only available for
@@ -35,20 +36,6 @@ export const usdcPerMacroTokenSelector = selectorFamily<
         return
       }
 
-      // Find swap for denom
-      const denomSwap = tokens.pools.find(
-        ({ pool_assets }) =>
-          pool_assets.find(
-            ({ denom: pool_denom, token_address }) =>
-              pool_denom === denom || token_address === denom
-          ) !== undefined
-      )
-
-      // No price information available.
-      if (!denomSwap) {
-        return
-      }
-
       // Find USDC swap by USDC_SWAP_ADDRESS
       const usdcSwap = tokens.pools.find(
         ({ swap_address }) => swap_address === USDC_SWAP_ADDRESS
@@ -57,11 +44,12 @@ export const usdcPerMacroTokenSelector = selectorFamily<
       // Junoswap exists on Juno mainnet.
       const client = get(cosmWasmClientForChainSelector(ChainInfoID.Juno1))
 
-      // Query for price of 1000000 tokens since decimals are not returned
-      // by API. This will give us up to 10^-6 precision for calculations
-      const tokenAmount = Math.pow(10, NATIVE_DECIMALS)
+      // Query for price of 1*10^BASE_SWAP_DECIMALS tokens since decimals are
+      // not returned by API. This will give us up to 10^-BASE_SWAP_DECIMALS
+      // precision for calculations.
+      const tokenAmount = Math.pow(10, BASE_SWAP_DECIMALS)
 
-      // Query and calculate price for 1 native token
+      // Query and calculate price for 1 native token.
       // uUSDC / uJUNO
       const nativeUSDC =
         Number(
@@ -76,12 +64,26 @@ export const usdcPerMacroTokenSelector = selectorFamily<
         ) / tokenAmount
 
       // Don't need to query again for price of native token.
-      if (denom === NATIVE_DENOM) {
+      if (denom === BASE_SWAP_DENOM) {
         // USDC / JUNO
         return Number(nativeUSDC)
       }
 
-      // Get juno price in terms of the native token.
+      // Find swap for denom.
+      const denomSwap = tokens.pools.find(
+        ({ pool_assets }) =>
+          pool_assets.find(
+            ({ denom: pool_denom, token_address }) =>
+              pool_denom === denom || token_address === denom
+          ) !== undefined
+      )
+
+      // No price information available.
+      if (!denomSwap) {
+        return
+      }
+
+      // Get juno price in terms of the token.
       // uJUNO / uTOKEN
       const tokenPairPrice =
         Number(
@@ -98,7 +100,7 @@ export const usdcPerMacroTokenSelector = selectorFamily<
         // (uUSDC / uJUNO) * (uJUNO / uTOKEN) = uUSDC / uTOKEN
         ((Number(nativeUSDC) * Number(tokenPairPrice)) /
           // Divide out the number of decimals in USDC.
-          Math.pow(10, NATIVE_DECIMALS)) *
+          Math.pow(10, BASE_SWAP_DECIMALS)) *
         // Mutltiply by the number of decimals in TOKEN.
         Math.pow(10, decimals)
 
@@ -114,8 +116,8 @@ export const daoTvlSelector = selectorFamily<
   get:
     ({ coreAddress, chainId }) =>
     async ({ get }) => {
-      const nativeBalance = get(
-        nativeBalanceSelector({ address: coreAddress, chainId })
+      const nativeBalances = get(
+        nativeBalancesSelector({ address: coreAddress, chainId })
       )
       const cw20Balances = get(
         CwdCoreV2Selectors.cw20BalancesInfoSelector({
@@ -124,18 +126,18 @@ export const daoTvlSelector = selectorFamily<
         })
       )
 
-      let balances = cw20Balances
-        ? cw20Balances.map(({ amount, denom, decimals }) => ({
-            amount,
-            denom,
-            decimals,
-          }))
-        : []
-      if (nativeBalance) {
-        balances = [{ ...nativeBalance, decimals: NATIVE_DECIMALS }].concat(
-          balances
-        )
-      }
+      const balances = [
+        ...nativeBalances.map(({ amount, denom, decimals }) => ({
+          amount,
+          denom,
+          decimals,
+        })),
+        ...cw20Balances.map(({ amount, denom, decimals }) => ({
+          amount,
+          denom,
+          decimals,
+        })),
+      ]
 
       const prices = balances.map(({ amount, denom, decimals }) => {
         const price = get(usdcPerMacroTokenSelector({ denom, decimals }))
