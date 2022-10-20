e56b7d8720063f0f3257e5653e1d557bafdff24d
Noah Saso <noahsaso@gmail.com>
2022-10-20 13:55:08 -0700

    Fixed no mobile wallet button variant style.

--- packages/ui/components/wallet/NoMobileWallet.tsx
+++ packages/ui/components/wallet/NoMobileWallet.tsx
@@ -9,14 +9,19 @@ import { Button, ButtonProps } from '../buttons/Button'
 import { Modal } from '../modals/Modal'
 
 export const NoMobileWallet = (
-  props: Partial<Omit<ButtonProps, 'onClick' | 'size'>>
+  props: Partial<Omit<ButtonProps, 'onClick' | 'size' | 'variant'>>
 ) => {
   const { t } = useTranslation()
   const [showInfo, setShowInfo] = useState(false)
 
   return (
     <>
-      <Button {...props} onClick={() => setShowInfo(true)} size="lg">
+      <Button
+        {...props}
+        onClick={() => setShowInfo(true)}
+        size="lg"
+        variant="ghost"
+      >
         <p className="link-text text-xs italic">{t('info.testnet')}</p>
         <InfoOutlined className="!h-3 !w-3" />
       </Button>
