7a8fff4b1e2f3e9b25b837b8ca4fc7ca585f2b96
Noah Saso <noahsaso@gmail.com>
2022-10-20 20:04:30 -0700

    Fixed missing props type.

--- packages/ui/components/splash/SplashStatsCard.tsx
+++ packages/ui/components/splash/SplashStatsCard.tsx
@@ -1,7 +1,14 @@
 // GNU AFFERO GENERAL PUBLIC LICENSE Version 3. Copyright (C) 2022 DAO DAO Contributors.
-// See the "LICENSE" file in the root directory of this package for more copyright information.
+// See the "LICENSE" file in the root directory of this package for more
+// copyright information.
 
-export const SplashStatsCard = ({ children }) => (
+import { ReactNode } from 'react'
+
+export interface SplashStatsCardProps {
+  children: ReactNode | ReactNode[]
+}
+
+export const SplashStatsCard = ({ children }: SplashStatsCardProps) => (
   <div className="flex flex-col items-center gap-1 px-6 md:px-6">
     {children}
   </div>
