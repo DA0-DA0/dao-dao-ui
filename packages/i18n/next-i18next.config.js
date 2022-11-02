7c6e70156aa50c58f5fa83d6a3a17970a39c35b1
Noah Saso <noahsaso@gmail.com>
2022-11-02 15:50:02 -0700

    Ordered language codes alphabetically.

--- packages/i18n/next-i18next.config.js
+++ packages/i18n/next-i18next.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 const config = {
   i18n: {
     // Add your language ISO 639-1 code here.
-    locales: ['en', 'es', 'it', 'pl', 'uk', 'zh', 'zh-tw', 'ko', 'fr'],
+    locales: ['en', 'es', 'fr', 'it', 'ko', 'pl', 'uk', 'zh', 'zh-tw'],
     defaultLocale: 'en',
     lowerCaseLng: true,
   },
