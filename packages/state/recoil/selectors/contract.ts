fd743b4add0a579df699531106591831f4a60915
Noah Saso <noahsaso@gmail.com>
2022-12-05 03:31:45 -0800

    Use indexer for contract instantiated dates.

--- packages/state/recoil/selectors/contract.ts
+++ packages/state/recoil/selectors/contract.ts
@@ -8,6 +8,7 @@ import {
   cosmWasmClientForChainSelector,
 } from './chain'
 import { CwdCoreV2Selectors } from './contracts'
+import { queryIndexerSelector } from './indexer'
 
 export const contractInstantiateTimeSelector = selectorFamily<
   Date | undefined,
@@ -17,12 +18,28 @@ export const contractInstantiateTimeSelector = selectorFamily<
   get:
     ({ address, chainId }) =>
     async ({ get }) => {
-      const client = get(cosmWasmClientForChainSelector(chainId))
+      const instantiatedAt = get(
+        queryIndexerSelector({
+          contractAddress: address,
+          chainId,
+          formulaName: 'instantiatedAt',
+        })
+      )
+      // Null when indexer fails.
+      if (instantiatedAt) {
+        return new Date(instantiatedAt)
+      }
 
+      // If indexer fails, fallback to querying chain.
+
+      const client = get(cosmWasmClientForChainSelector(chainId))
       const events = await client.searchTx({
         tags: [{ key: 'instantiate._contract_address', value: address }],
       })
-      if (events.length === 0) return
+
+      if (events.length === 0) {
+        return
+      }
 
       return get(
         blockHeightTimestampSafeSelector({
