e1a8582daba2ddf8cf4c8087d8fd85390a42cdb2
Noah Saso <noahsaso@gmail.com>
2022-12-06 11:26:29 -0800

    Moved retroactive compensation API to env var.

--- packages/stateful/payroll/adapters/Retroactive/constants.ts
+++ packages/stateful/payroll/adapters/Retroactive/constants.ts
@@ -1,5 +1,3 @@
 export const API_BASE =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8787'
-    : 'https://retroactive-compensation-cf-worker.dao-dao.workers.dev'
+  process.env.NEXT_PUBLIC_RETROACTIVE_COMPENSATION_API_BASE
 export const SIGNATURE_TYPE = 'Retroactive Compensation'
