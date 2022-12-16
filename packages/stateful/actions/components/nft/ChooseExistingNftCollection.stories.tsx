acf8c306f9a86755975999bf2ab04bede81910e7
Noah Saso <noahsaso@gmail.com>
2022-12-15 21:13:32 -0800

    Fixed stories.

--- packages/stateful/actions/components/nft/ChooseExistingNftCollection.stories.tsx
+++ packages/stateful/actions/components/nft/ChooseExistingNftCollection.stories.tsx
@@ -25,6 +25,8 @@ export default {
       metadata: {
         name: '',
         description: '',
+        includeExtra: false,
+        extra: '',
       },
     }),
     makeActionsProviderDecorator({
