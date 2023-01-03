9ee480f13f1edac42d450bdb0c29c113bb692f21
Noah Saso <noahsaso@gmail.com>
2023-01-02 18:07:15 -0800

    Undid ActionCard format change.

--- packages/stateful/actions/components/ActionCard.tsx
+++ packages/stateful/actions/components/ActionCard.tsx
@@ -15,29 +15,22 @@ export const ActionCard = ({
   title,
   onRemove,
   children,
-}: ActionCardProps) => {
-  return (
-    <div className="flex flex-col rounded-lg bg-background-tertiary">
-      <div className="primary-text flex flex-row items-start justify-between gap-4 border-b border-border-base p-4 text-text-body">
-        <div className="flex flex-row items-center gap-3">
-          <p className="text-xl">
-            <Icon />
-          </p>
+}: ActionCardProps) => (
+  <div className="flex flex-col rounded-lg bg-background-tertiary">
+    <div className="primary-text flex flex-row items-start justify-between gap-4 border-b border-border-base p-4 text-text-body">
+      <div className="flex flex-row items-center gap-3">
+        <p className="text-xl">
+          <Icon />
+        </p>
 
-          <p>{title}</p>
-        </div>
-
-        {onRemove && (
-          <IconButton
-            Icon={Close}
-            onClick={onRemove}
-            size="sm"
-            variant="ghost"
-          />
-        )}
+        <p>{title}</p>
       </div>
 
-      <div className="flex flex-col gap-2 p-6 pt-4">{children}</div>
+      {onRemove && (
+        <IconButton Icon={Close} onClick={onRemove} size="sm" variant="ghost" />
+      )}
     </div>
-  )
-}
+
+    <div className="flex flex-col gap-2 p-6 pt-4">{children}</div>
+  </div>
+)
