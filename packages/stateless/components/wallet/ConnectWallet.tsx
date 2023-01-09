41a007e25128d3a4429aec9310f9d4759f001689
Noah Saso <noahsaso@gmail.com>
2023-01-09 10:17:28 -0800

    Undo ConnectWallet changes.

--- packages/stateless/components/wallet/ConnectWallet.tsx
+++ packages/stateless/components/wallet/ConnectWallet.tsx
@@ -8,7 +8,7 @@ import { Button, ButtonProps } from '../buttons'
 import { NoMobileWallet } from './NoMobileWallet'
 
 export interface ConnectWalletProps
-  extends Partial<Omit<ButtonProps, 'onClick'>> {
+  extends Partial<Omit<ButtonProps, 'onClick' | 'size'>> {
   onConnect: () => void
   className?: string
 }
@@ -22,7 +22,7 @@ export const ConnectWallet = ({ onConnect, ...props }: ConnectWalletProps) => {
   }
 
   return (
-    <Button onClick={onConnect} size="lg" {...props}>
+    <Button {...props} onClick={onConnect} size="lg">
       <Sensors className="!h-6 !w-6" />
       <p>{t('button.connectWallet')}</p>
     </Button>
