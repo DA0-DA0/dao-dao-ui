30ad2bd91fa89c586e1848b7b12282139a7c782f
noah <noahsaso@gmail.com>
2023-02-04 15:57:22 -0800

    Fixed fiat modal height, scrolling, and address truncation (#1084)

--- packages/stateless/components/modals/KadoModal.tsx
+++ packages/stateless/components/modals/KadoModal.tsx
@@ -1,12 +1,14 @@
 import { WarningRounded } from '@mui/icons-material'
 import clsx from 'clsx'
 import queryString from 'query-string'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { KadoModalProps } from '@dao-dao/types'
 import { KADO_API_KEY } from '@dao-dao/utils'
 
 import { CopyToClipboard } from '../CopyToClipboard'
+import { Loader } from '../logo'
 import { Modal } from './Modal'
 
 export const KadoModal = ({
@@ -17,9 +19,26 @@ export const KadoModal = ({
 }: KadoModalProps) => {
   const { t } = useTranslation()
 
+  // iframe hijacks clicks on mobile Safari even when pointer-events are set to
+  // none (which happens on the modal when visible=false), so we need to
+  // completely hide the iframe when invisible.
+  //
+  // https://stackoverflow.com/questions/39533016/iframe-with-pointer-eventsnone-hijacks-clicks-in-safari-on-ios
+  const [iframeVisible, setIframeVisible] = useState(false)
+  useEffect(() => {
+    // Hide iframe at a delay after modal hides so it doesn't look weird when
+    // disappearing, but show immediately when displaying.
+    if (modalProps.visible) {
+      setIframeVisible(true)
+    } else {
+      const timeout = setTimeout(() => setIframeVisible(false), 200)
+      return () => clearTimeout(timeout)
+    }
+  }, [modalProps.visible])
+
   return (
     <Modal
-      containerClassName={clsx('!h-[90vh] !max-w-lg', containerClassName)}
+      containerClassName={clsx('!h-[80vh] !max-w-lg', containerClassName)}
       {...modalProps}
     >
       {toAddress && (
@@ -30,29 +49,44 @@ export const KadoModal = ({
             <p>{t('info.verifyAddressMatches')}</p>
 
             <CopyToClipboard
-              takeStartEnd={{ start: 16, end: 16 }}
+              className="w-full"
+              takeStartEnd={{ start: 10, end: 8 }}
               value={toAddress}
             />
           </div>
         </div>
       )}
 
-      <iframe
-        className="rounded-md"
-        height="100%"
-        src={`https://app.kado.money/?${queryString.stringify({
-          apiKey: KADO_API_KEY,
-          onRevCurrency: 'USDC',
-          offPayCurrency: 'USDC',
-          offRevCurrency: 'USDC',
-          product: defaultMode?.toUpperCase(),
-          onToAddress: toAddress,
-          network: 'JUNO',
-          cryptoList: 'USDC',
-          networkList: 'JUNO',
-        })}`}
-        width="100%"
-      />
+      <div className="relative max-h-full min-h-[48rem] w-full shrink-0 grow">
+        <div className="absolute z-10 flex w-full flex-col items-center justify-center p-10">
+          <Loader />
+        </div>
+
+        {iframeVisible && (
+          <iframe
+            className="relative z-20 h-full w-full rounded-md"
+            ref={(ref) => {
+              if (ref) {
+                ref.parentElement?.parentElement?.scrollTo({
+                  top: 0,
+                  behavior: 'smooth',
+                })
+              }
+            }}
+            src={`https://app.kado.money/?${queryString.stringify({
+              apiKey: KADO_API_KEY,
+              onRevCurrency: 'USDC',
+              offPayCurrency: 'USDC',
+              offRevCurrency: 'USDC',
+              product: defaultMode?.toUpperCase(),
+              onToAddress: toAddress,
+              network: 'JUNO',
+              cryptoList: 'USDC',
+              networkList: 'JUNO',
+            })}`}
+          />
+        )}
+      </div>
     </Modal>
   )
 }
