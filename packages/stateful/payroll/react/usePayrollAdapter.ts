86ab891d2163479704b81e5acfa4a490307e6004
Noah Saso <noahsaso@gmail.com>
2023-02-14 19:10:10 -0800

    Don't use action options in payroll adapter getter, instead accept as argument to function.

--- packages/stateful/payroll/react/usePayrollAdapter.ts
+++ packages/stateful/payroll/react/usePayrollAdapter.ts
@@ -1,19 +1,21 @@
 // External API
 
-import { useMemo } from 'react'
+import { useCallback } from 'react'
 
 import { DaoCoreV2Selectors } from '@dao-dao/state/recoil'
 import { useCachedLoadable, useDaoInfoContext } from '@dao-dao/stateless'
-import { Action, PayrollAdapterWithActions } from '@dao-dao/types'
+import {
+  Action,
+  ActionOptions,
+  PayrollAdapterWithActions,
+} from '@dao-dao/types'
 
-import { useActionOptions } from '../../actions'
 import { getAdapterById } from '../core'
 
 // Get payroll adapter for the DAO based on its configured payroll system. If
 // nothing configured or no system found, returns undefined.
 export const usePayrollAdapter = (): PayrollAdapterWithActions | undefined => {
   const { chainId, coreAddress } = useDaoInfoContext()
-  const actionOptions = useActionOptions()
 
   const payrollConfig = useCachedLoadable(
     DaoCoreV2Selectors.payrollConfigSelector({
@@ -27,18 +29,20 @@ export const usePayrollAdapter = (): PayrollAdapterWithActions | undefined => {
     payrollConfig.state === 'hasValue' && payrollConfig.contents
       ? getAdapterById(payrollConfig.contents.type)
       : undefined
-  const actions = useMemo(
-    () =>
-      adapter?.actionMakers
-        ?.map((makeAction) => makeAction(actionOptions))
-        .filter(Boolean) as Action[],
-    [adapter, actionOptions]
+
+  // Create hook to generate actions for the adapter.
+  const makeActions = useCallback(
+    (options: ActionOptions) =>
+      (adapter?.actionMakers
+        ?.map((makeAction) => makeAction(options))
+        .filter(Boolean) ?? []) as Action[],
+    [adapter]
   )
 
   return (
     adapter && {
       ...adapter,
-      actions,
+      makeActions,
     }
   )
 }
