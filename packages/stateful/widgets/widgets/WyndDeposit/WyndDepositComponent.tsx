64bb454485c6d17b9f0852f66dc72104598017e3
Noah Saso <noahsaso@gmail.com>
2023-03-15 18:06:09 -0700

    Styled transaction info better.

--- packages/stateful/widgets/widgets/WyndDeposit/WyndDepositComponent.tsx
+++ packages/stateful/widgets/widgets/WyndDeposit/WyndDepositComponent.tsx
@@ -454,13 +454,20 @@ export const WyndDepositComponent = ({
       )}
 
       {txHash && (
-        <div className="flex flex-col items-end gap-2 self-end text-text-interactive-valid">
-          <CopyToClipboard value={txHash} />
+        <div className="flex flex-col gap-2">
+          <p className="title-text">{t('title.transaction')}:</p>
 
-          <ButtonLink href={CHAIN_TXN_URL_PREFIX + txHash} variant="ghost">
-            {t('button.openInChainExplorer')}{' '}
-            <ArrowOutwardRounded className="!h-4 !w-4" />
-          </ButtonLink>
+          <div className="flex flex-row flex-wrap justify-between gap-x-4 gap-y-2 rounded-md bg-background-secondary px-4 py-2">
+            <CopyToClipboard
+              className="text-text-interactive-valid"
+              value={txHash}
+            />
+
+            <ButtonLink href={CHAIN_TXN_URL_PREFIX + txHash} variant="ghost">
+              {t('button.openInChainExplorer')}{' '}
+              <ArrowOutwardRounded className="!h-4 !w-4" />
+            </ButtonLink>
+          </div>
         </div>
       )}
     </div>
