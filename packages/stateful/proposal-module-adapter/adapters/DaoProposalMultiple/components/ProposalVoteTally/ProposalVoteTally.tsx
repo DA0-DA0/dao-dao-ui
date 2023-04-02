8c61397d9296138c4f49672c746a46c82cc359f2
Noah Saso <noahsaso@gmail.com>
2023-04-01 18:58:41 -0700

    Removed unnecessary title from MC vote tally that was taking up lots of space, and added more space below proposal action/choice display.

--- packages/stateful/proposal-module-adapter/adapters/DaoProposalMultiple/components/ProposalVoteTally/ProposalVoteTally.tsx
+++ packages/stateful/proposal-module-adapter/adapters/DaoProposalMultiple/components/ProposalVoteTally/ProposalVoteTally.tsx
@@ -32,124 +32,119 @@ export const ProposalVoteTally = ({
   const { t } = useTranslation()
 
   return (
-    <div className="flex flex-col">
-      <p className="primary-text gap-4 py-4 pt-5 text-text-body">
-        {t('title.votingProgress')}
-      </p>
-      <div className="rounded-lg border border-border-secondary bg-component-widget">
-        <div className="space-y-4 py-4 px-6">
-          {/* Threshold title */}
-          <p className="link-text text-text-body">{t('title.ratioOfVotes')}</p>
+    <div className="rounded-lg border border-border-secondary bg-component-widget">
+      <div className="space-y-4 py-4 px-6">
+        {/* Threshold title */}
+        <p className="link-text text-text-body">{t('title.ratioOfVotes')}</p>
 
-          {/* Votes progress bar */}
-          <div className="my-2">
-            <ProgressBar
-              rows={[
-                {
-                  thickness: 10,
-                  data: processedChoices.map((choice) => {
-                    const tooltipTitle =
-                      formatPercentOf100(choice.turnoutVotePercentage) +
-                      ' ' +
-                      choice.title
+        {/* Votes progress bar */}
+        <div className="my-2">
+          <ProgressBar
+            rows={[
+              {
+                thickness: 10,
+                data: processedChoices.map((choice) => {
+                  const tooltipTitle =
+                    formatPercentOf100(choice.turnoutVotePercentage) +
+                    ' ' +
+                    choice.title
 
-                    return {
-                      value: choice.turnoutVotePercentage,
-                      color: choice.color || '',
-                      tooltipTitle: tooltipTitle,
-                    }
-                  }),
-                },
-              ]}
-            />
-          </div>
-
-          {turnoutTotal > 0 && (
-            <div className="secondary-text flex flex-row items-center justify-between gap-2">
-              <div className="flex flex-row items-center gap-1">
-                <p className="text-text-tertiary">
-                  {status === ProposalStatus.Open
-                    ? t('title.currentWinner')
-                    : status === ProposalStatus.Rejected
-                    ? t('title.noWinner')
-                    : t('title.winner')}
-                </p>
-              </div>
-
-              {/* Winning option display */}
-              <p className="flex flex-row items-center gap-1">
-                <TooltipTruncatedText
-                  className="text-text-body"
-                  text={
-                    isTie
-                      ? t('title.tied')
-                      : status === ProposalStatus.Rejected
-                      ? t('proposalStatusTitle.rejected')
-                      : // If not rejected nor tied, winningChoice should always be defined.
-                        winningChoice?.title ?? t('info.unknown')
+                  return {
+                    value: choice.turnoutVotePercentage,
+                    color: choice.color || '',
+                    tooltipTitle: tooltipTitle,
                   }
-                />
-              </p>
-            </div>
-          )}
+                }),
+              },
+            ]}
+          />
         </div>
 
-        {/* Quorum */}
-        <div className="space-y-4 border-t border-border-secondary py-4 px-6">
-          {/* Quorum title */}
-          <p className="link-text text-text-body">
-            {t('title.percentTurnout', {
-              value: formatPercentOf100(turnoutPercent),
-            })}
-          </p>
+        {turnoutTotal > 0 && (
+          <div className="secondary-text flex flex-row items-center justify-between gap-2">
+            <div className="flex flex-row items-center gap-1">
+              <p className="text-text-tertiary">
+                {status === ProposalStatus.Open
+                  ? t('title.currentWinner')
+                  : status === ProposalStatus.Rejected
+                  ? t('title.noWinner')
+                  : t('title.winner')}
+              </p>
+            </div>
 
-          {/* Quorum progress bar */}
-          <div className="my-2">
-            <ProgressBar
-              caretPosition={
-                quorum.type === ProcessedTQType.Majority ? 50 : quorum.value
-              }
-              rows={[
-                {
-                  thickness: 10,
-                  data: [
-                    {
-                      value: Number(turnoutPercent),
-                      color: 'var(--icon-secondary)',
-                    },
-                  ],
-                },
-              ]}
+            {/* Winning option display */}
+            <p className="flex flex-row items-center gap-1">
+              <TooltipTruncatedText
+                className="text-text-body"
+                text={
+                  isTie
+                    ? t('title.tied')
+                    : status === ProposalStatus.Rejected
+                    ? t('proposalStatusTitle.rejected')
+                    : // If not rejected nor tied, winningChoice should always be defined.
+                      winningChoice?.title ?? t('info.unknown')
+                }
+              />
+            </p>
+          </div>
+        )}
+      </div>
+
+      {/* Quorum */}
+      <div className="space-y-4 border-t border-border-secondary py-4 px-6">
+        {/* Quorum title */}
+        <p className="link-text text-text-body">
+          {t('title.percentTurnout', {
+            value: formatPercentOf100(turnoutPercent),
+          })}
+        </p>
+
+        {/* Quorum progress bar */}
+        <div className="my-2">
+          <ProgressBar
+            caretPosition={
+              quorum.type === ProcessedTQType.Majority ? 50 : quorum.value
+            }
+            rows={[
+              {
+                thickness: 10,
+                data: [
+                  {
+                    value: Number(turnoutPercent),
+                    color: 'var(--icon-secondary)',
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
+
+        {/* Quorum config display */}
+        <div className="secondary-text flex flex-row items-center justify-between gap-2">
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-text-tertiary">{t('title.quorum')}</p>
+            <TooltipInfoIcon
+              iconClassName="text-icon-tertiary"
+              size="sm"
+              title={t('info.proposalQuorumTooltip')}
             />
           </div>
 
-          {/* Quorum config display */}
-          <div className="secondary-text flex flex-row items-center justify-between gap-2">
-            <div className="flex flex-row items-center gap-1">
-              <p className="text-text-tertiary">{t('title.quorum')}</p>
-              <TooltipInfoIcon
-                iconClassName="text-icon-tertiary"
-                size="sm"
-                title={t('info.proposalQuorumTooltip')}
-              />
-            </div>
+          <p className="flex flex-row items-center gap-1">
+            <Tooltip title={t('info.proposalQuorumTooltip')}>
+              <p className="text-text-body">{quorum.display}</p>
+            </Tooltip>
 
-            <p className="flex flex-row items-center gap-1">
-              <Tooltip title={t('info.proposalQuorumTooltip')}>
-                <p className="text-text-body">{quorum.display}</p>
+            {quorumReached ? (
+              <Tooltip title={t('info.reached')}>
+                <Check className="!h-5 !w-5 text-icon-primary" />
               </Tooltip>
-
-              {quorumReached ? (
-                <Tooltip title={t('info.reached')}>
-                  <Check className="!h-5 !w-5 text-icon-primary" />
-                </Tooltip>
-              ) : (
-                <Tooltip title={t('info.notMet')}>
-                  <Close className="!h-5 !w-5 text-icon-primary" />
-                </Tooltip>
-              )}
-            </p>
-          </div>
+            ) : (
+              <Tooltip title={t('info.notMet')}>
+                <Close className="!h-5 !w-5 text-icon-primary" />
+              </Tooltip>
+            )}
+          </p>
         </div>
       </div>
     </div>
