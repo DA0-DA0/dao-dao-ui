1ff5c903abb30b1c16b977799a7aa44b03312617
Noah Saso <noahsaso@gmail.com>
2023-04-16 13:42:48 -0700

    Upgraded to more secure cosmodal.

--- packages/stateful/components/WalletProvider.tsx
+++ packages/stateful/components/WalletProvider.tsx
@@ -95,21 +95,24 @@ export const WalletProvider = ({ children }: WalletProviderProps) => {
           rest: STARGAZE_TESTNET_REST_ENDPOINT,
         },
       ]}
-      classNames={{
-        modalOverlay: '!backdrop-brightness-50 !backdrop-filter',
-        modalContent:
-          '!p-6 !max-w-md !bg-background-base !rounded-lg !border !border-border-secondary !shadow-dp8',
-        modalCloseButton:
-          '!p-1 !text-icon-tertiary bg-transparent hover:!bg-background-interactive-hover active:!bg-background-interactive-pressed !rounded-full !transition !absolute !top-2 !right-2',
-        modalHeader: '!header-text',
-        modalSubheader: '!title-text',
-        wallet:
-          '!rounded-lg !bg-background-secondary !p-4 !shadow-none transition-opacity opacity-100 hover:opacity-80 active:opacity-70',
-        walletName: '!primary-text',
-        walletDescription: '!caption-text',
-        textContent: '!body-text',
-      }}
       defaultChainId={CHAIN_ID}
+      defaultUiConfig={{
+        classNames: {
+          modalOverlay: '!backdrop-brightness-50 !backdrop-filter',
+          modalContent:
+            '!p-6 !max-w-md !bg-background-base !rounded-lg !border !border-border-secondary !shadow-dp8',
+          modalCloseButton:
+            '!p-1 !text-icon-tertiary bg-transparent hover:!bg-background-interactive-hover active:!bg-background-interactive-pressed !rounded-full !transition !absolute !top-2 !right-2',
+          modalHeader: '!header-text',
+          modalSubheader: '!title-text',
+          wallet:
+            '!rounded-lg !bg-background-secondary !p-4 !shadow-none transition-opacity opacity-100 hover:opacity-80 active:opacity-70',
+          walletName: '!primary-text',
+          walletDescription: '!caption-text',
+          textContent: '!body-text',
+        },
+        renderLoader: () => <Loader size={42} />,
+      }}
       enabledWalletTypes={[
         WalletType.Keplr,
         WalletType.Leap,
@@ -134,7 +137,6 @@ export const WalletProvider = ({ children }: WalletProviderProps) => {
         registry: typesRegistry,
       })}
       localStorageKey="connectedWalletId"
-      renderLoader={() => <Loader size={42} />}
       walletConnectClientMeta={{
         name: t('meta.title'),
         description: t('meta.description'),
