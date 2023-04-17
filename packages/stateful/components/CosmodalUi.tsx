c93d6b664e0c864b7ab24ac3ded0454ec0142291
Noah Saso <noahsaso@gmail.com>
2023-04-16 20:40:22 -0700

    Fix mobile wallet connect UI.

--- packages/stateful/components/CosmodalUi.tsx
+++ packages/stateful/components/CosmodalUi.tsx
@@ -2,11 +2,12 @@ import { UiProps, WalletConnectionStatus } from '@noahsaso/cosmodal'
 import { wallets as WEB3AUTH_WALLETS } from '@noahsaso/cosmodal/dist/wallets/web3auth'
 import { isAndroid, isMobile } from '@walletconnect/browser-utils'
 import clsx from 'clsx'
+import Link from 'next/link'
 import QRCode from 'qrcode.react'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Button, LinkWrapper, Loader, Modal, Tooltip } from '@dao-dao/stateless'
+import { Button, Loader, Modal, Tooltip } from '@dao-dao/stateless'
 
 import { Trans } from './Trans'
 
@@ -162,76 +163,74 @@ export const CosmodalUi = ({
       )}
 
       {/* WalletConnect */}
-      {status === WalletConnectionStatus.Connecting &&
-        !!walletConnectUri &&
-        qrShowing && (
-          <>
-            {!!navigateToMobileWalletConnectUrl && (
-              <div className={clsx(qrShowing && 'mb-4')}>
-                <p className="mb-4">
-                  <Trans i18nKey="info.openMobileWalletAndAcceptConnection">
-                    <LinkWrapper
-                      className="underline"
-                      href={navigateToMobileWalletConnectUrl}
-                      openInNewTab
+      {status === WalletConnectionStatus.Connecting && !!walletConnectUri && (
+        <>
+          {!!navigateToMobileWalletConnectUrl && (
+            <div className={clsx(qrShowing && 'mb-4')}>
+              <p className="mb-4">
+                <Trans i18nKey="info.openMobileWalletAndAcceptConnection">
+                  <Link
+                    className="underline"
+                    href={navigateToMobileWalletConnectUrl}
+                  >
+                    Open your mobile wallet
+                  </Link>{' '}
+                  and accept the connection request.
+                </Trans>
+              </p>
+
+              <p className={clsx(showMobileHelp ? 'mb-4' : 'mb-6')}>
+                <Trans i18nKey="info.installKeplrMobileOrScanQrCode">
+                  If you don&apos;t have Keplr Mobile installed,{' '}
+                  <Link
+                    className="underline"
+                    href={
+                      android
+                        ? navigateToMobileWalletConnectUrl
+                        : IOS_KEPLR_MOBILE_URL
+                    }
+                  >
+                    click here to install it
+                  </Link>{' '}
+                  or scan the QR code at the bottom with another device.
+                </Trans>
+              </p>
+
+              {showMobileHelp && (
+                <p className="mb-6">
+                  <Trans i18nKey="info.keplrMobileReset">
+                    If nothing shows up in Keplr Mobile,{' '}
+                    <Button
+                      className="inline"
+                      onClick={reset}
+                      variant="underline"
                     >
-                      Open your mobile wallet
-                    </LinkWrapper>{' '}
-                    and accept the connection request.
+                      click here to reset
+                    </Button>{' '}
+                    and try connecting again. Refresh the page if the problem
+                    persists.
                   </Trans>
                 </p>
+              )}
 
-                <p className={clsx(showMobileHelp ? 'mb-4' : 'mb-6')}>
-                  <Trans i18nKey="info.installKeplrMobileOrScanQrCode">
-                    If you don&apos;t have Keplr Mobile installed,{' '}
-                    <LinkWrapper
-                      href={
-                        android
-                          ? navigateToMobileWalletConnectUrl
-                          : IOS_KEPLR_MOBILE_URL
-                      }
-                      openInNewTab
-                      style={{ textDecoration: 'underline' }}
-                    >
-                      click here to install it
-                    </LinkWrapper>{' '}
-                    or scan the QR code at the bottom with another device.
-                  </Trans>
-                </p>
+              <Button
+                onClick={() => setQrShowing((s) => !s)}
+                variant="underline"
+              >
+                {qrShowing ? t('button.hideQrCode') : t('button.showQrCode')}
+              </Button>
+            </div>
+          )}
 
-                {showMobileHelp && (
-                  <p className="mb-6">
-                    <Trans i18nKey="info.keplrMobileReset">
-                      If nothing shows up in Keplr Mobile,{' '}
-                      <Button
-                        className="inline"
-                        onClick={reset}
-                        variant="underline"
-                      >
-                        click here to reset
-                      </Button>{' '}
-                      and try connecting again. Refresh the page if the problem
-                      persists.
-                    </Trans>
-                  </p>
-                )}
-
-                <Button
-                  onClick={() => setQrShowing((s) => !s)}
-                  variant="underline"
-                >
-                  {qrShowing ? t('button.hideQrCode') : t('button.showQrCode')}
-                </Button>
-              </div>
-            )}
-
+          {qrShowing && (
             <QRCode
               size={500}
               style={{ width: '100%', height: '100%' }}
               value={walletConnectUri}
             />
-          </>
-        )}
+          )}
+        </>
+      )}
 
       {status === WalletConnectionStatus.Resetting && <Loader size={42} />}
     </Modal>
