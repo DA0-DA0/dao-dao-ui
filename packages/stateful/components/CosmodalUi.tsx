b9a90353ee65dea24670b032b0327e1aa1cec14a
Noah Saso <noahsaso@gmail.com>
2023-04-17 12:22:46 -0700

    Deleted comment.

--- packages/stateful/components/CosmodalUi.tsx
+++ packages/stateful/components/CosmodalUi.tsx
@@ -236,20 +236,3 @@ export const CosmodalUi = ({
     </Modal>
   )
 }
-
-/*
-{
-  modalOverlay: '!backdrop-brightness-50 !backdrop-filter',
-  modalContent:
-    '!p-6 !max-w-md !bg-background-base !rounded-lg !border !border-border-secondary !shadow-dp8',
-  modalCloseButton:
-    '!p-1 !text-icon-tertiary bg-transparent hover:!bg-background-interactive-hover active:!bg-background-interactive-pressed !rounded-full !transition !absolute !top-2 !right-2',
-  modalHeader: '!header-text',
-  modalSubheader: '!title-text',
-  wallet:
-    '!rounded-lg !bg-background-secondary !p-4 !shadow-none transition-opacity opacity-100 hover:opacity-80 active:opacity-70',
-  walletName: '!primary-text',
-  walletDescription: '!caption-text',
-  textContent: '!body-text',
-},
-*/
