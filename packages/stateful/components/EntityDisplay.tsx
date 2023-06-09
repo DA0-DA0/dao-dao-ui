fe2ab32b309eb7032ac173b3149e4136871bd7a5
Noah Saso <noahsaso@gmail.com>
2023-06-09 02:09:30 -0700

    Removed TODO.

--- packages/stateful/components/EntityDisplay.tsx
+++ packages/stateful/components/EntityDisplay.tsx
@@ -3,8 +3,6 @@ import { StatefulEntityDisplayProps } from '@dao-dao/types'
 
 import { useEntity } from '../hooks'
 
-// TODO: Extract chain ID from bech32 address prefix, since any addresses can
-// now be rendered in markdown.
 export const EntityDisplay = (props: StatefulEntityDisplayProps) => {
   const loadingEntity = useEntity({
     address: props.address,
