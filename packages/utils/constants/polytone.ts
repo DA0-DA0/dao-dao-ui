4fe175fcb9470db4c203e10cc483f4375700bd9c
Noah Saso <noahsaso@gmail.com>
2023-06-21 15:23:23 -0700

    Updated polytone testnet channel.

--- packages/utils/constants/polytone.ts
+++ packages/utils/constants/polytone.ts
@@ -9,17 +9,21 @@ const junoTestnet: PolytoneNotes = {
 const junoMainnet: PolytoneNotes = {
   [ChainInfoID.Uni6]: {
     // juno-1
-    note: 'juno1w7qxyutgqe7usqn5lp3srm2erqh57hrh4fdwyepl76vz9tmqs2gsspfhxu',
+    note: 'juno13tamtu4qwe6d5lr5268rydalhlmcj79vaqr2ant4x6ejw3gpmncsyf8u62',
     // juno-1
-    listener: 'juno1sfptnhcfpynp52u8jsaytt2nzxy08ypyaxaur0wka55tylxz5ldq4n5elw',
+    listener: 'juno1fn8ug6pktctcsjumcq3n7eqshnzrsj73z83tj4vw03jzj09h9s0qng7dan',
     // juno-1
-    localConnection: 'connection-349',
+    localConnection: 'connection-375',
     // uni-6
-    remoteConnection: 'connection-206',
+    remoteConnection: 'connection-586',
     // juno-1
-    localChannel: 'channel-245',
+    localChannel: 'channel-279',
     // uni-6
-    remoteChannel: 'channel-201',
+    remoteChannel: 'channel-466',
+    // juno-1
+    // localClient: '07-tendermint-381',
+    // uni-6
+    // remoteClient: '07-tendermint-446',
   },
 }
 
