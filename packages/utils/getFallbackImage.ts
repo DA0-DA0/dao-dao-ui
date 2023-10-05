171ece82d01e1467d8f003509cd22f468283ac6f
noah <noahsaso@gmail.com>
2023-10-05 13:50:06 -0700

    Synchronize fallback images. (#1401)

--- packages/utils/getFallbackImage.ts
+++ packages/utils/getFallbackImage.ts
@@ -5,6 +5,7 @@ import { ChainId } from '@dao-dao/types'
 
 import { getImageUrlForChainId } from './chain'
 import { NEUTRON_GOVERNANCE_DAO } from './constants'
+import { toBech32Hash } from './conversion'
 
 // fallback images in the public/placeholders directory.
 export const getFallbackImage = (identifier = '') => {
@@ -12,6 +13,10 @@ export const getFallbackImage = (identifier = '') => {
   if (identifier === NEUTRON_GOVERNANCE_DAO) {
     return getImageUrlForChainId(ChainId.NeutronMainnet)
   }
+
+  // If identitifer is an address, get its bech32 data, so it's consistent
+  // across chains.
+  identifier = toBech32Hash(identifier) || identifier
 
   const hashed = identifier.split('').reduce((p, n) => n.charCodeAt(0) + p, 0)
   const index = (hashed % 5) + 1
