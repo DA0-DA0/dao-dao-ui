349dc47a33d901bfcca5527b76965f9d3d454a9c
Noah Saso <noahsaso@gmail.com>
2023-10-19 15:45:55 -0700

    Added ICA Execute action.

--- packages/stateless/components/inputs/InputErrorMessage.tsx
+++ packages/stateless/components/inputs/InputErrorMessage.tsx
@@ -2,21 +2,31 @@ import clsx from 'clsx'
 import { FieldError } from 'react-hook-form'
 
 export interface InputErrorMessageProps {
-  error?: FieldError | string
+  error?: FieldError | string | Error | unknown
   className?: string
 }
 
 export const InputErrorMessage = ({
   error,
   className,
-}: InputErrorMessageProps) =>
-  typeof error === 'string' || error?.message ? (
+}: InputErrorMessageProps) => {
+  const message =
+    error &&
+    (typeof error === 'string'
+      ? error
+      : error instanceof Error ||
+        (typeof error === 'object' && 'message' in error)
+      ? error.message
+      : `${error}`)
+
+  return message ? (
     <span
       className={clsx(
         'mt-1 ml-1 inline-block max-w-prose break-words text-xs text-text-interactive-error',
         className
       )}
     >
-      {typeof error === 'string' ? error : error.message}
+      {message}
     </span>
   ) : null
+}
