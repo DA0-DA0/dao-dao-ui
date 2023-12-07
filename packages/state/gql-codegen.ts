07beede896df1043aefdd6ca6adfadc274237224
Noah Saso <noahsaso@gmail.com>
2023-12-06 18:51:51 -0800

    Use local Stargaze GraphQL SDL.

--- packages/state/gql-codegen.ts
+++ packages/state/gql-codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: 'https://graphql.mainnet.stargaze-apis.com/graphql',
+  schema: './graphql/stargaze/schema.graphql',
   documents: ['./graphql/**/*.ts'],
   generates: {
     './graphql/__generated__/': {
