f62b307c5a7ae560cece9eb6fd05409e0465ee8b
Noah Saso <noahsaso@gmail.com>
2023-12-19 15:09:57 -0800

    Fixed build.

--- packages/stateful/widgets/react/useWidget.ts
+++ packages/stateful/widgets/react/useWidget.ts
@@ -6,12 +6,8 @@ import { useWidgets } from './useWidgets'
 export const useWidget = <Data extends Record<string, unknown> = any>(
   widgetId: WidgetId
 ): LoadedWidget<Data> | undefined => {
-  const widgets = useWidgets({
-    suspendWhileLoading: true,
-  })
+  const widgets = useWidgets()
 
-  // `suspendWhileLoading` above should make this never be loading here, so this
-  // is just a type-check.
   return widgets.loading
     ? undefined
     : widgets.data.find(({ widget }) => widget.id === widgetId)
