c1eb8ff0747098e3f209ab935b7fffad12ac8d41
Noah Saso <noahsaso@gmail.com>
2023-12-18 21:00:28 -0800

    Disable horizontal scroller autoscroll.

--- packages/stateless/components/HorizontalScroller.tsx
+++ packages/stateless/components/HorizontalScroller.tsx
@@ -1,15 +1,7 @@
 import { CheckBoxOutlineBlankRounded } from '@mui/icons-material'
 import clsx from 'clsx'
-import {
-  ComponentType,
-  UIEventHandler,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react'
+import { ComponentType } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useInView } from 'react-intersection-observer'
 
 import { LoadingData } from '@dao-dao/types'
 
@@ -32,64 +24,6 @@ export const HorizontalScroller = <P extends {}>({
   shadowClassName,
 }: HorizontalScrollerProps<P>) => {
   const { t } = useTranslation()
-  const [clonesWidth, setClonesWidth] = useState(0)
-  const [autoscroll, setAutoscroll] = useState(true)
-
-  const scrollRef = useRef<HTMLDivElement | null>(null)
-  // Prevent autoscroll if user touching.
-  const userTouching = useRef(false)
-
-  // Don't scroll this element if it isn't visible as the scrolling is a
-  // reasonably heavy operation.
-  const { ref: viewRef, inView: componentIsVisible } = useInView()
-
-  const handleScroll: UIEventHandler<HTMLDivElement> = useCallback(
-    (e) => {
-      const container = e.currentTarget
-      const scrollPos = container.scrollLeft
-      const scrollWidth = container.scrollWidth
-      const divWidth = container.clientWidth
-
-      if (scrollPos >= scrollWidth - divWidth) {
-        // Scroll to the end of the non-clones. This will put us in a position
-        // where we have the last element followed by the clones. Subtracting
-        // div-width has the effect of placing the end of the scroll view at the
-        // end of the clones.
-        container.scrollLeft = clonesWidth - divWidth
-      } else if (scrollPos <= 0) {
-        // Scroll to the beginning of the clones. This will put us in a position
-        // where we have the first element with the non-clones behind us.
-        container.scrollLeft = scrollWidth - clonesWidth
-      }
-    },
-    [clonesWidth]
-  )
-
-  // Set the width of the clones once this component mounts.
-  useEffect(() => {
-    const clones = document.getElementsByClassName('is-clone')
-    const clonesArray = Array.from(clones)
-    const width = clonesArray.reduce(
-      (accum, { clientWidth }) => accum + clientWidth,
-      0
-    )
-    // We use 16 pixels of padding between each element so we need to add that
-    // information when considering the width.
-    setClonesWidth(width + 16 * clonesArray.length)
-  }, [])
-
-  useEffect(() => {
-    if (!autoscroll || !componentIsVisible) {
-      return
-    }
-
-    const interval = setInterval(() => {
-      if (scrollRef.current && !userTouching.current) {
-        scrollRef.current.scrollBy(1, 0)
-      }
-    }, 50)
-    return () => clearInterval(interval)
-  }, [autoscroll, componentIsVisible])
 
   return (
     <div className={clsx('relative', containerClassName)}>
@@ -112,33 +46,13 @@ export const HorizontalScroller = <P extends {}>({
       ) : items.data.length === 0 ? (
         <NoContent
           Icon={CheckBoxOutlineBlankRounded}
-          body={t('info.noFeaturedDaosYet')}
+          body={t('info.nothingFound')}
         />
       ) : (
-        <div
-          className="no-scrollbar w-full overflow-scroll"
-          onMouseEnter={() => setAutoscroll(false)}
-          onMouseLeave={() => setAutoscroll(true)}
-          onScroll={handleScroll}
-          onTouchEnd={() => {
-            userTouching.current = false
-          }}
-          onTouchStart={() => {
-            userTouching.current = true
-          }}
-          ref={(r) => {
-            scrollRef.current = r
-            viewRef(r)
-          }}
-        >
+        <div className="no-scrollbar w-full overflow-scroll">
           <div className="flex w-max flex-row gap-2 py-1 sm:gap-3 lg:gap-4">
             {items.data.map((item, index) => (
               <div key={index} className={itemClassName}>
-                <Component {...item} />
-              </div>
-            ))}
-            {items.data.map((item, index) => (
-              <div key={index} className={clsx('is-clone', itemClassName)}>
                 <Component {...item} />
               </div>
             ))}
