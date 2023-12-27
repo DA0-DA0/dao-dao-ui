02497f9b367a4e497fe32ebb26ade9b52fe63de0
Noah Saso <noahsaso@gmail.com>
2023-12-27 15:58:40 -0800

    Improved unknown error handler.

--- packages/stateless/components/inputs/InputErrorMessage.tsx
+++ packages/stateless/components/inputs/InputErrorMessage.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import { FieldError } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
 
 export interface InputErrorMessageProps {
   error?: FieldError | string | Error | unknown
@@ -10,6 +11,7 @@ export const InputErrorMessage = ({
   error,
   className,
 }: InputErrorMessageProps) => {
+  const { t } = useTranslation()
   const message =
     error &&
     (typeof error === 'string'
@@ -19,9 +21,9 @@ export const InputErrorMessage = ({
           'message' in error &&
           typeof (error as { message: string }).message === 'string')
       ? (error as { message: string }).message
-      : `${error}`)
+      : t('error.unknownError'))
 
-  return message && message !== '[object Object]' ? (
+  return message ? (
     <span
       className={clsx(
         'mt-1 ml-1 inline-block max-w-prose break-words text-xs text-text-interactive-error',
