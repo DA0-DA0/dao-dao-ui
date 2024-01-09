9b6929f82e14c19570e2ea72dc1beeddc3153201
Noah Saso <noahsaso@gmail.com>
2024-01-09 10:01:06 -0800

    Use Osmosis' nodes again.

--- packages/utils/constants/chains.ts
+++ packages/utils/constants/chains.ts
@@ -858,8 +858,8 @@ export const CHAIN_ENDPOINTS: Partial<
     rest: 'https://juno-testnet-api.polkachu.com',
   },
   [ChainId.OsmosisMainnet]: {
-    rpc: 'https://osmosis-rpc.polkachu.com',
-    rest: 'https://osmosis-api.polkachu.com',
+    rpc: 'https://rpc.osmosis.zone',
+    rest: 'https://lcd.osmosis.zone',
   },
   [ChainId.OsmosisTestnet]: {
     rpc: 'https://osmosis-testnet-rpc.polkachu.com',
