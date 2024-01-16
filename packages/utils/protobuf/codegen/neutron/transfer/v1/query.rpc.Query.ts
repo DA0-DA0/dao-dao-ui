235915284ceb879b314ce4af8ea486118fed838f
Noah Saso <noahsaso@gmail.com>
2024-01-16 12:08:00 -0800

    Fixed build.

--- packages/utils/protobuf/codegen/neutron/transfer/v1/query.rpc.Query.ts
+++ packages/utils/protobuf/codegen/neutron/transfer/v1/query.rpc.Query.ts
@@ -5,13 +5,13 @@ import { QueryDenomTraceRequest, QueryDenomTraceResponse, QueryDenomTracesReques
 /** Query provides defines the gRPC querier service. */
 export interface Query {
   /** DenomTrace queries a denomination trace information. */
-  denomTrace(request: ibc.applications.transfer.v1.QueryDenomTraceRequest): Promise<QueryDenomTraceResponse>;
+  denomTrace(request: QueryDenomTraceRequest): Promise<QueryDenomTraceResponse>;
   /** DenomTraces queries all denomination traces. */
-  denomTraces(request?: ibc.applications.transfer.v1.QueryDenomTracesRequest): Promise<QueryDenomTracesResponse>;
+  denomTraces(request?: QueryDenomTracesRequest): Promise<QueryDenomTracesResponse>;
   /** Params queries all parameters of the ibc-transfer module. */
-  params(request?: ibc.applications.transfer.v1.QueryParamsRequest): Promise<QueryParamsResponse>;
+  params(request?: QueryParamsRequest): Promise<QueryParamsResponse>;
   /** DenomHash queries a denomination hash information. */
-  denomHash(request: ibc.applications.transfer.v1.QueryDenomHashRequest): Promise<QueryDenomHashResponse>;
+  denomHash(request: QueryDenomHashRequest): Promise<QueryDenomHashResponse>;
 }
 export class QueryClientImpl implements Query {
   private readonly rpc: Rpc;
@@ -22,25 +22,25 @@ export class QueryClientImpl implements Query {
     this.params = this.params.bind(this);
     this.denomHash = this.denomHash.bind(this);
   }
-  denomTrace(request: ibc.applications.transfer.v1.QueryDenomTraceRequest, useInterfaces: boolean = true): Promise<QueryDenomTraceResponse> {
-    const data = ibc.applications.transfer.v1.QueryDenomTraceRequest.encode(request).finish();
+  denomTrace(request: QueryDenomTraceRequest, useInterfaces: boolean = true): Promise<QueryDenomTraceResponse> {
+    const data = QueryDenomTraceRequest.encode(request).finish();
     const promise = this.rpc.request("neutron.transfer.Query", "DenomTrace", data);
     return promise.then(data => QueryDenomTraceResponse.decode(new BinaryReader(data), undefined, useInterfaces));
   }
-  denomTraces(request: ibc.applications.transfer.v1.QueryDenomTracesRequest = {
+  denomTraces(request: QueryDenomTracesRequest = {
     pagination: undefined
   }, useInterfaces: boolean = true): Promise<QueryDenomTracesResponse> {
-    const data = ibc.applications.transfer.v1.QueryDenomTracesRequest.encode(request).finish();
+    const data = QueryDenomTracesRequest.encode(request).finish();
     const promise = this.rpc.request("neutron.transfer.Query", "DenomTraces", data);
     return promise.then(data => QueryDenomTracesResponse.decode(new BinaryReader(data), undefined, useInterfaces));
   }
-  params(request: ibc.applications.transfer.v1.QueryParamsRequest = {}, useInterfaces: boolean = true): Promise<QueryParamsResponse> {
-    const data = ibc.applications.transfer.v1.QueryParamsRequest.encode(request).finish();
+  params(request: QueryParamsRequest = {}, useInterfaces: boolean = true): Promise<QueryParamsResponse> {
+    const data = QueryParamsRequest.encode(request).finish();
     const promise = this.rpc.request("neutron.transfer.Query", "Params", data);
     return promise.then(data => QueryParamsResponse.decode(new BinaryReader(data), undefined, useInterfaces));
   }
-  denomHash(request: ibc.applications.transfer.v1.QueryDenomHashRequest, useInterfaces: boolean = true): Promise<QueryDenomHashResponse> {
-    const data = ibc.applications.transfer.v1.QueryDenomHashRequest.encode(request).finish();
+  denomHash(request: QueryDenomHashRequest, useInterfaces: boolean = true): Promise<QueryDenomHashResponse> {
+    const data = QueryDenomHashRequest.encode(request).finish();
     const promise = this.rpc.request("neutron.transfer.Query", "DenomHash", data);
     return promise.then(data => QueryDenomHashResponse.decode(new BinaryReader(data), undefined, useInterfaces));
   }
@@ -49,16 +49,16 @@ export const createRpcQueryExtension = (base: QueryClient) => {
   const rpc = createProtobufRpcClient(base);
   const queryService = new QueryClientImpl(rpc);
   return {
-    denomTrace(request: ibc.applications.transfer.v1.QueryDenomTraceRequest, useInterfaces: boolean = true): Promise<ibc.applications.transfer.v1.QueryDenomTraceResponse> {
+    denomTrace(request: QueryDenomTraceRequest, useInterfaces: boolean = true): Promise<QueryDenomTraceResponse> {
       return queryService.denomTrace(request, useInterfaces);
     },
-    denomTraces(request?: ibc.applications.transfer.v1.QueryDenomTracesRequest, useInterfaces: boolean = true): Promise<ibc.applications.transfer.v1.QueryDenomTracesResponse> {
+    denomTraces(request?: QueryDenomTracesRequest, useInterfaces: boolean = true): Promise<QueryDenomTracesResponse> {
       return queryService.denomTraces(request, useInterfaces);
     },
-    params(request?: ibc.applications.transfer.v1.QueryParamsRequest, useInterfaces: boolean = true): Promise<ibc.applications.transfer.v1.QueryParamsResponse> {
+    params(request?: QueryParamsRequest, useInterfaces: boolean = true): Promise<QueryParamsResponse> {
       return queryService.params(request, useInterfaces);
     },
-    denomHash(request: ibc.applications.transfer.v1.QueryDenomHashRequest, useInterfaces: boolean = true): Promise<ibc.applications.transfer.v1.QueryDenomHashResponse> {
+    denomHash(request: QueryDenomHashRequest, useInterfaces: boolean = true): Promise<QueryDenomHashResponse> {
       return queryService.denomHash(request, useInterfaces);
     }
   };
