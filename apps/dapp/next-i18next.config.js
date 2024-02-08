3327828c6f1c8c0d2752ea42f5b5a793fb6f0969
Noah Saso <noahsaso@gmail.com>
2024-02-08 15:21:26 -0800

    baddest*

--- apps/dapp/next-i18next.config.js
+++ apps/dapp/next-i18next.config.js
@@ -1,4 +1,21 @@
 // GNU AFFERO GENERAL PUBLIC LICENSE Version 3. Copyright (C) 2022 DAO DAO Contributors.
 // See the "LICENSE" file in the root directory of this package for more copyright information.
 
-module.exports = require('@dao-dao/i18n/next-i18next.config')
+const config = require('@dao-dao/i18n/next-i18next.config')
+
+module.exports = {
+  ...config,
+  i18n: {
+    ...config.i18n,
+    domains: [
+      {
+        domain: 'daodao.zone',
+        defaultLocale: 'en',
+      },
+      {
+        domain: 'badbad.zone',
+        defaultLocale: 'bad',
+      },
+    ],
+  },
+}
