b2955b04e33b41a8c6a29c1e3be9f015248046a9
Noah Saso <noahsaso@gmail.com>
2024-02-16 21:44:13 -0800

    Use Snapper many value history query.

--- packages/stateful/recoil/selectors/treasury.ts
+++ packages/stateful/recoil/selectors/treasury.ts
@@ -1,4 +1,3 @@
-import { Asset as SkipAsset } from '@skip-router/core'
 import uniq from 'lodash.uniq'
 import { noWait, selectorFamily, waitForAll, waitForNone } from 'recoil'
 
@@ -6,7 +5,6 @@ import {
   accountsSelector,
   genericTokenBalancesSelector,
   genericTokenSelector,
-  genericTokenSourceSelector,
   nativeDelegatedBalanceSelector,
   querySnapperSelector,
 } from '@dao-dao/state'
@@ -24,12 +22,8 @@ import {
 import {
   COMMUNITY_POOL_ADDRESS_PLACEHOLDER,
   convertMicroDenomToDenomWithDecimals,
-  deserializeTokenSource,
-  findValueAtTimestamp,
   getNativeTokenForChainId,
   loadableToLoadingData,
-  serializeTokenSource,
-  tokensEqual,
 } from '@dao-dao/utils'
 
 import { tokenCardLazyInfoSelector } from './token'
@@ -259,148 +253,57 @@ export const treasuryValueHistorySelector = selectorFamily<
       }
 
       // Value history for all accounts.
-      const historicalBalances = get(
+      const { timestamps, assets, totals } = get(
+        querySnapperSelector({
+          query: 'daodao-many-value-history',
+          parameters: {
+            accounts: allAccounts.map(
+              ({ chainId, address }) => `${chainId}:${address}`
+            ),
+            range,
+            tokenFilter: filter?.tokens
+              ?.map(
+                ({ chainId, denomOrAddress }) => `${chainId}:${denomOrAddress}`
+              )
+              .join(','),
+          },
+        })
+      ) as {
+        timestamps: number[]
+        assets: {
+          origin: {
+            chainId: string
+            denom: string
+          }
+          // Value at each timestamp.
+          values: (number | null)[]
+        }[]
+        // Total value at each timestamp.
+        totals: (number | null)[]
+      }
+
+      const tokens = get(
         waitForAll(
-          allAccounts.map(({ chainId, address }) =>
-            querySnapperSelector({
-              query: 'daodao-value-history',
-              parameters: {
-                chainId,
-                address,
-                range,
-              },
+          assets.map(({ origin: { chainId, denom } }) =>
+            genericTokenSelector({
+              type: denom.startsWith('cw20:')
+                ? TokenType.Cw20
+                : TokenType.Native,
+              chainId,
+              denomOrAddress: denom.startsWith('cw20:')
+                ? denom.slice(5)
+                : denom,
             })
           )
         )
-      ) as {
-        assets: SkipAsset[]
-        snapshots: {
-          timestamp: number
-          values: {
-            price?: number
-            balance?: string
-            value?: number
-          }[]
-          totalValue: number
-        }[]
-      }[]
-
-      const historicalBalanceTokenSources = get(
-        waitForAll(
-          historicalBalances.map(({ assets }) =>
-            waitForAll(
-              assets.map(({ chainID, denom, isCW20, tokenContract }) =>
-                genericTokenSourceSelector({
-                  chainId: chainID,
-                  type: isCW20 ? TokenType.Cw20 : TokenType.Native,
-                  denomOrAddress: (isCW20 && tokenContract) || denom,
-                })
-              )
-            )
-          )
-        )
       )
-
-      // All queries have similar timestamps since they use the same range
-      // (though they may have been cached at different times), so choose the
-      // one with the most timestamps available.
-      const oldestAccount = historicalBalances.reduce((acc, asset) =>
-        asset.snapshots.length > acc.snapshots.length ? acc : asset
-      )
-      let timestamps =
-        oldestAccount?.snapshots.map(({ timestamp }) => timestamp) || []
-
-      // Get unique tokens across all accounts.
-      const uniqueTokenSources = uniq(
-        historicalBalanceTokenSources.flatMap((sources) =>
-          sources.map((source) => serializeTokenSource(source))
-        )
-      ).map((tokenSource) => deserializeTokenSource(tokenSource))
-      // These are loaded/cached above, so this `get` should be instant.
-      const uniqueTokens = get(
-        waitForAll(
-          uniqueTokenSources.map((source) => genericTokenSelector(source))
-        )
-      )
-
-      const tokensWithValues = uniqueTokens.map((token) => {
-        // Get the snapshots of this token at each timestamp for each account.
-        const accountTokenSnapshots = historicalBalances.map(
-          ({ snapshots }, index) => {
-            // Get the index of this token in the snapshots for this account's
-            // historical balances.
-            const snapshotTokenIndex = historicalBalanceTokenSources[
-              index
-            ].findIndex((snapshotTokenSource) =>
-              tokensEqual(snapshotTokenSource, token)
-            )
-
-            if (snapshotTokenIndex === -1) {
-              return []
-            }
-
-            // Extract this token's value at each timestamp.
-            return snapshots.map(({ timestamp, values }) => ({
-              timestamp,
-              value: values[snapshotTokenIndex].value,
-            }))
-          }
-        )
-
-        const values = timestamps.map((timestamp) => {
-          // Get the account values at this timestamp for each account.
-          const accountValues = accountTokenSnapshots.map(
-            (snapshots) => findValueAtTimestamp(snapshots, timestamp)?.value
-          )
-
-          // Sum the values at this timestamp. If all are undefined, return null
-          // to indicate there's no data for this timestamp.
-          return accountValues.reduce(
-            (acc, value) =>
-              acc === null && value === undefined
-                ? null
-                : (acc || 0) + (value || 0),
-            null as number | null
-          )
-        })
-
-        return {
-          token,
-          values,
-        }
-      })
-
-      // Sum up the values at each timestamp, ignoring null values.
-      let totals = timestamps.map((_, index) =>
-        tokensWithValues.reduce(
-          (acc, { values }) => acc + (values[index] || 0),
-          0
-        )
-      )
-
-      // Remove timestamps at the front that have no data for all tokens.
-
-      // Get first timestamp with a value.
-      let firstNonNullTimestamp = timestamps.findIndex((_, index) =>
-        tokensWithValues.some(({ values }) => values[index] !== null)
-      )
-
-      // If no non-null timestamps, remove all.
-      if (firstNonNullTimestamp === -1) {
-        firstNonNullTimestamp = totals.length
-      }
-
-      if (firstNonNullTimestamp > 0) {
-        timestamps = timestamps.slice(firstNonNullTimestamp)
-        tokensWithValues.forEach(
-          (data) => (data.values = data.values.slice(firstNonNullTimestamp))
-        )
-        totals = totals.slice(firstNonNullTimestamp)
-      }
 
       return {
         timestamps: timestamps.map((timestamp) => new Date(timestamp)),
-        tokens: tokensWithValues,
+        tokens: assets.map(({ values }, index) => ({
+          token: tokens[index],
+          values: values.map((value) => value ?? null),
+        })),
         totals,
       }
     },
