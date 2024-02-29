9eea1f9442935986be0157f80402fb4e438e6f59
Noah Saso <noahsaso@gmail.com>
2024-02-28 18:52:06 -0800

    fixed valence actions

--- packages/utils/constants/chains.ts
+++ packages/utils/constants/chains.ts
@@ -1501,6 +1501,6 @@ chains
     })
   })
 
-export const VALENCE_SUPPORTED_CHAINS = Object.entries(SUPPORTED_CHAINS)
-  .filter(([, { valence }]) => valence)
-  .map(([chainId]) => chainId as ChainId)
+export const VALENCE_SUPPORTED_CHAINS = SUPPORTED_CHAINS.filter(
+  ({ valence }) => valence
+).map(({ chainId }) => chainId as ChainId)
