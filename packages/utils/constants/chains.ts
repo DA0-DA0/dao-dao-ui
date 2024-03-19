5585aed1862a784c051a635a031f0335c0f009a3
Noah Saso <noahsaso@gmail.com>
2024-03-19 16:22:46 +0100

    use different migaloo RPC

--- packages/utils/constants/chains.ts
+++ packages/utils/constants/chains.ts
@@ -1315,8 +1315,8 @@ export const CHAIN_ENDPOINTS: Partial<
     rest: 'https://terra-api.polkachu.com',
   },
   [ChainId.MigalooMainnet]: {
-    rpc: 'https://migaloo-rpc.polkachu.com',
-    rest: 'https://migaloo-api.polkachu.com',
+    rpc: 'https://rpc.cosmos.directory/migaloo',
+    rest: 'https://rest.cosmos.directory/migaloo',
   },
   [ChainId.MigalooTestnet]: {
     rpc: 'https://migaloo-testnet-rpc.polkachu.com',
