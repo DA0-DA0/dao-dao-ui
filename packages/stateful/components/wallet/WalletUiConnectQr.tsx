52a52c37903817bd6a6d5cf720692af8b51083e7
Noah Saso <noahsaso@gmail.com>
2024-04-01 09:32:27 -0700

    added copy to clipboard to walletconnect QR modal

--- packages/stateful/components/wallet/WalletUiConnectQr.tsx
+++ packages/stateful/components/wallet/WalletUiConnectQr.tsx
@@ -1,18 +1,31 @@
 import { WalletModalProps } from '@cosmos-kit/core'
 import QRCode from 'qrcode.react'
+import { useTranslation } from 'react-i18next'
+
+import { CopyToClipboard } from '@dao-dao/stateless'
 
 export const WalletUiConnectQr = ({
   walletRepo,
 }: Pick<WalletModalProps, 'walletRepo'>) => {
+  const { t } = useTranslation()
+
   if (!walletRepo?.current?.qrUrl?.data) {
     return null
   }
 
   return (
-    <QRCode
-      size={500}
-      style={{ width: '100%', height: '100%' }}
-      value={walletRepo.current.qrUrl.data}
-    />
+    <div className="flex flex-col gap-4 -mb-2 items-center">
+      <QRCode
+        size={500}
+        style={{ width: '100%', height: '100%' }}
+        value={walletRepo.current.qrUrl.data}
+      />
+
+      <CopyToClipboard
+        className="text-sm"
+        label={t('button.copyToClipboard')}
+        value={walletRepo.current.qrUrl.data}
+      />
+    </div>
   )
 }
