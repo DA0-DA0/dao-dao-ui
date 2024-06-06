cc02a074873834153f06adffcb3e8fa8a0a40f5f
Noah Saso <noahsaso@gmail.com>
2024-06-06 12:19:27 -0400

    add playwright test for chain DAO page

---
+++ apps/dapp/tests/chain.spec.ts
@@ -0,0 +1,54 @@
+// GNU AFFERO GENERAL PUBLIC LICENSE Version 3. Copyright (C) 2022 DAO DAO Contributors.
+// See the "LICENSE" file in the root directory of this package for more copyright information.
+
+import { expect, test } from '@playwright/test'
+
+import './setup'
+
+test('chain home/proposals tab renders', async ({ page }) => {
+  await page.goto('/dao/juno')
+
+  // Expect no 404 error.
+  await expect(page.getByText('404: Not Found')).not.toBeVisible({
+    timeout: 5000,
+  })
+
+  // Expect description to exist.
+  await expect(
+    page.getByText('Native chain governance for Juno Testnet.')
+  ).toBeVisible()
+
+  // Expect "New proposal" button to exist.
+  await expect(page.getByText('New proposal', { exact: true })).toBeVisible()
+})
+
+test('chain treasury tab renders', async ({ page }) => {
+  await page.goto('/dao/juno/treasury')
+
+  // Expect no 404 error.
+  await expect(page.getByText('404: Not Found')).not.toBeVisible({
+    timeout: 5000,
+  })
+
+  // Expect description to exist.
+  await expect(
+    page.getByText('Native chain governance for Juno Testnet.')
+  ).toBeVisible()
+
+  // Expect "Token" title to exist.
+  await expect(page.getByText('Token', { exact: true })).toBeVisible()
+})
+
+test('chain subDAOs tab renders', async ({ page }) => {
+  await page.goto('/dao/juno/subdaos')
+
+  // Expect no 404 error.
+  await expect(page.getByText('404: Not Found')).not.toBeVisible({
+    timeout: 5000,
+  })
+
+  // Expect description to exist.
+  await expect(
+    page.getByText('Native chain governance for Juno Testnet.')
+  ).toBeVisible()
+})
