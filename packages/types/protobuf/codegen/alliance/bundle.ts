DA0-DA0/secret-dao-dao-ui

added secret network protobufs

packages/types/protobuf/codegen/alliance/bundle.ts
@@ -8,12 +8,12 @@ import * as _6 from "./alliance/query";
 import * as _7 from "./alliance/redelegations";
 import * as _8 from "./alliance/tx";
 import * as _9 from "./alliance/unbonding";
-import * as _348 from "./alliance/tx.amino";
-import * as _349 from "./alliance/tx.registry";
-import * as _350 from "./alliance/query.rpc.Query";
-import * as _351 from "./alliance/tx.rpc.msg";
-import * as _583 from "./rpc.query";
-import * as _584 from "./rpc.tx";
+import * as _362 from "./alliance/tx.amino";
+import * as _363 from "./alliance/tx.registry";
+import * as _364 from "./alliance/query.rpc.Query";
+import * as _365 from "./alliance/tx.rpc.msg";
+import * as _610 from "./rpc.query";
+import * as _611 from "./rpc.tx";
 export namespace alliance {
   export const alliance = {
     ..._0,
@@ -26,13 +26,13 @@ export namespace alliance {
     ..._7,
     ..._8,
     ..._9,
-    ..._348,
-    ..._349,
-    ..._350,
-    ..._351
+    ..._362,
+    ..._363,
+    ..._364,
+    ..._365
   };
   export const ClientFactory = {
-    ..._583,
-    ..._584
+    ..._610,
+    ..._611
   };
 }

packages/types/protobuf/codegen/gaia/bundle.ts
@@ -1,26 +1,26 @@
 import * as _105 from "./globalfee/v1beta1/genesis";
 import * as _106 from "./globalfee/v1beta1/query";
 import * as _107 from "./globalfee/v1beta1/tx";
-import * as _415 from "./globalfee/v1beta1/tx.amino";
-import * as _416 from "./globalfee/v1beta1/tx.registry";
-import * as _417 from "./globalfee/v1beta1/query.rpc.Query";
-import * as _418 from "./globalfee/v1beta1/tx.rpc.msg";
-import * as _591 from "./rpc.query";
-import * as _592 from "./rpc.tx";
+import * as _429 from "./globalfee/v1beta1/tx.amino";
+import * as _430 from "./globalfee/v1beta1/tx.registry";
+import * as _431 from "./globalfee/v1beta1/query.rpc.Query";
+import * as _432 from "./globalfee/v1beta1/tx.rpc.msg";
+import * as _618 from "./rpc.query";
+import * as _619 from "./rpc.tx";
 export namespace gaia {
   export namespace globalfee {
     export const v1beta1 = {
       ..._105,
       ..._106,
       ..._107,
-      ..._415,
-      ..._416,
-      ..._417,
-      ..._418
+      ..._429,
+      ..._430,
+      ..._431,
+      ..._432
     };
   }
   export const ClientFactory = {
-    ..._591,
-    ..._592
+    ..._618,
+    ..._619
   };
 }

packages/types/protobuf/codegen/juno/bundle.ts
@@ -6,16 +6,16 @@ import * as _142 from "./mint/genesis";
 import * as _143 from "./mint/mint";
 import * as _144 from "./mint/query";
 import * as _145 from "./mint/tx";
-import * as _439 from "./feeshare/v1/tx.amino";
-import * as _440 from "./mint/tx.amino";
-import * as _441 from "./feeshare/v1/tx.registry";
-import * as _442 from "./mint/tx.registry";
-import * as _443 from "./feeshare/v1/query.rpc.Query";
-import * as _444 from "./mint/query.rpc.Query";
-import * as _445 from "./feeshare/v1/tx.rpc.msg";
-import * as _446 from "./mint/tx.rpc.msg";
-import * as _595 from "./rpc.query";
-import * as _596 from "./rpc.tx";
+import * as _453 from "./feeshare/v1/tx.amino";
+import * as _454 from "./mint/tx.amino";
+import * as _455 from "./feeshare/v1/tx.registry";
+import * as _456 from "./mint/tx.registry";
+import * as _457 from "./feeshare/v1/query.rpc.Query";
+import * as _458 from "./mint/query.rpc.Query";
+import * as _459 from "./feeshare/v1/tx.rpc.msg";
+import * as _460 from "./mint/tx.rpc.msg";
+import * as _622 from "./rpc.query";
+import * as _623 from "./rpc.tx";
 export namespace juno {
   export namespace feeshare {
     export const v1 = {
@@ -23,10 +23,10 @@ export namespace juno {
       ..._139,
       ..._140,
       ..._141,
-      ..._439,
-      ..._441,
-      ..._443,
-      ..._445
+      ..._453,
+      ..._455,
+      ..._457,
+      ..._459
     };
   }
   export const mint = {
@@ -34,13 +34,13 @@ export namespace juno {
     ..._143,
     ..._144,
     ..._145,
-    ..._440,
-    ..._442,
-    ..._444,
-    ..._446
+    ..._454,
+    ..._456,
+    ..._458,
+    ..._460
   };
   export const ClientFactory = {
-    ..._595,
-    ..._596
+    ..._622,
+    ..._623
   };
 }

packages/types/protobuf/codegen/neutron/bundle.ts
@@ -36,32 +36,32 @@ import * as _194 from "./interchaintxs/v1/genesis";
 import * as _195 from "./interchaintxs/v1/params";
 import * as _196 from "./interchaintxs/v1/query";
 import * as _197 from "./interchaintxs/v1/tx";
-import * as _456 from "./cron/tx.amino";
-import * as _457 from "./dex/tx.amino";
-import * as _458 from "./feeburner/tx.amino";
-import * as _459 from "./feerefunder/tx.amino";
-import * as _460 from "./interchainqueries/tx.amino";
-import * as _461 from "./interchaintxs/v1/tx.amino";
-import * as _462 from "./cron/tx.registry";
-import * as _463 from "./dex/tx.registry";
-import * as _464 from "./feeburner/tx.registry";
-import * as _465 from "./feerefunder/tx.registry";
-import * as _466 from "./interchainqueries/tx.registry";
-import * as _467 from "./interchaintxs/v1/tx.registry";
-import * as _468 from "./cron/query.rpc.Query";
-import * as _469 from "./dex/query.rpc.Query";
-import * as _470 from "./feeburner/query.rpc.Query";
-import * as _471 from "./feerefunder/query.rpc.Query";
-import * as _472 from "./interchainqueries/query.rpc.Query";
-import * as _473 from "./interchaintxs/v1/query.rpc.Query";
-import * as _474 from "./cron/tx.rpc.msg";
-import * as _475 from "./dex/tx.rpc.msg";
-import * as _476 from "./feeburner/tx.rpc.msg";
-import * as _477 from "./feerefunder/tx.rpc.msg";
-import * as _478 from "./interchainqueries/tx.rpc.msg";
-import * as _479 from "./interchaintxs/v1/tx.rpc.msg";
-import * as _599 from "./rpc.query";
-import * as _600 from "./rpc.tx";
+import * as _470 from "./cron/tx.amino";
+import * as _471 from "./dex/tx.amino";
+import * as _472 from "./feeburner/tx.amino";
+import * as _473 from "./feerefunder/tx.amino";
+import * as _474 from "./interchainqueries/tx.amino";
+import * as _475 from "./interchaintxs/v1/tx.amino";
+import * as _476 from "./cron/tx.registry";
+import * as _477 from "./dex/tx.registry";
+import * as _478 from "./feeburner/tx.registry";
+import * as _479 from "./feerefunder/tx.registry";
+import * as _480 from "./interchainqueries/tx.registry";
+import * as _481 from "./interchaintxs/v1/tx.registry";
+import * as _482 from "./cron/query.rpc.Query";
+import * as _483 from "./dex/query.rpc.Query";
+import * as _484 from "./feeburner/query.rpc.Query";
+import * as _485 from "./feerefunder/query.rpc.Query";
+import * as _486 from "./interchainqueries/query.rpc.Query";
+import * as _487 from "./interchaintxs/v1/query.rpc.Query";
+import * as _488 from "./cron/tx.rpc.msg";
+import * as _489 from "./dex/tx.rpc.msg";
+import * as _490 from "./feeburner/tx.rpc.msg";
+import * as _491 from "./feerefunder/tx.rpc.msg";
+import * as _492 from "./interchainqueries/tx.rpc.msg";
+import * as _493 from "./interchaintxs/v1/tx.rpc.msg";
+import * as _626 from "./rpc.query";
+import * as _627 from "./rpc.tx";
 export namespace neutron {
   export namespace contractmanager {
     export const v1 = {
@@ -74,10 +74,10 @@ export namespace neutron {
     ..._163,
     ..._164,
     ..._165,
-    ..._456,
-    ..._462,
-    ..._468,
-    ..._474
+    ..._470,
+    ..._476,
+    ..._482,
+    ..._488
   };
   export const dex = {
     ..._166,
@@ -94,10 +94,10 @@ export namespace neutron {
     ..._177,
     ..._178,
     ..._179,
-    ..._457,
-    ..._463,
-    ..._469,
-    ..._475
+    ..._471,
+    ..._477,
+    ..._483,
+    ..._489
   };
   export const feeburner = {
     ..._180,
@@ -105,10 +105,10 @@ export namespace neutron {
     ..._182,
     ..._183,
     ..._184,
-    ..._458,
-    ..._464,
-    ..._470,
-    ..._476
+    ..._472,
+    ..._478,
+    ..._484,
+    ..._490
   };
   export const feerefunder = {
     ..._185,
@@ -116,20 +116,20 @@ export namespace neutron {
     ..._187,
     ..._188,
     ..._189,
-    ..._459,
-    ..._465,
-    ..._471,
-    ..._477
+    ..._473,
+    ..._479,
+    ..._485,
+    ..._491
   };
   export const interchainqueries = {
     ..._190,
     ..._191,
     ..._192,
     ..._193,
-    ..._460,
-    ..._466,
-    ..._472,
-    ..._478
+    ..._474,
+    ..._480,
+    ..._486,
+    ..._492
   };
   export namespace interchaintxs {
     export const v1 = {
@@ -137,14 +137,14 @@ export namespace neutron {
       ..._195,
       ..._196,
       ..._197,
-      ..._461,
-      ..._467,
-      ..._473,
-      ..._479
+      ..._475,
+      ..._481,
+      ..._487,
+      ..._493
     };
   }
   export const ClientFactory = {
-    ..._599,
-    ..._600
+    ..._626,
+    ..._627
   };
 }

packages/types/protobuf/codegen/noble/bundle.ts
@@ -1,16 +1,16 @@
-import * as _335 from "../tariff/genesis";
-import * as _336 from "../tariff/params";
-import * as _337 from "../tariff/query";
-import * as _582 from "../tariff/query.rpc.Query";
-import * as _609 from "./rpc.query";
+import * as _349 from "../tariff/genesis";
+import * as _350 from "../tariff/params";
+import * as _351 from "../tariff/query";
+import * as _609 from "../tariff/query.rpc.Query";
+import * as _638 from "./rpc.query";
 export namespace noble {
   export const tariff = {
-    ..._335,
-    ..._336,
-    ..._337,
-    ..._582
+    ..._349,
+    ..._350,
+    ..._351,
+    ..._609
   };
   export const ClientFactory = {
-    ..._609
+    ..._638
   };
 }

packages/types/protobuf/codegen/publicawesome/bundle.ts
@@ -16,21 +16,21 @@ import * as _304 from "./stargaze/mint/v1beta1/genesis";
 import * as _305 from "./stargaze/mint/v1beta1/mint";
 import * as _306 from "./stargaze/mint/v1beta1/query";
 import * as _307 from "./stargaze/mint/v1beta1/tx";
-import * as _547 from "./stargaze/alloc/v1beta1/tx.amino";
-import * as _548 from "./stargaze/cron/v1/tx.amino";
-import * as _549 from "./stargaze/globalfee/v1/tx.amino";
-import * as _550 from "./stargaze/alloc/v1beta1/tx.registry";
-import * as _551 from "./stargaze/cron/v1/tx.registry";
-import * as _552 from "./stargaze/globalfee/v1/tx.registry";
-import * as _553 from "./stargaze/alloc/v1beta1/query.rpc.Query";
-import * as _554 from "./stargaze/cron/v1/query.rpc.Query";
-import * as _555 from "./stargaze/globalfee/v1/query.rpc.Query";
-import * as _556 from "./stargaze/mint/v1beta1/query.rpc.Query";
-import * as _557 from "./stargaze/alloc/v1beta1/tx.rpc.msg";
-import * as _558 from "./stargaze/cron/v1/tx.rpc.msg";
-import * as _559 from "./stargaze/globalfee/v1/tx.rpc.msg";
-import * as _605 from "./rpc.query";
-import * as _606 from "./rpc.tx";
+import * as _561 from "./stargaze/alloc/v1beta1/tx.amino";
+import * as _562 from "./stargaze/cron/v1/tx.amino";
+import * as _563 from "./stargaze/globalfee/v1/tx.amino";
+import * as _564 from "./stargaze/alloc/v1beta1/tx.registry";
+import * as _565 from "./stargaze/cron/v1/tx.registry";
+import * as _566 from "./stargaze/globalfee/v1/tx.registry";
+import * as _567 from "./stargaze/alloc/v1beta1/query.rpc.Query";
+import * as _568 from "./stargaze/cron/v1/query.rpc.Query";
+import * as _569 from "./stargaze/globalfee/v1/query.rpc.Query";
+import * as _570 from "./stargaze/mint/v1beta1/query.rpc.Query";
+import * as _571 from "./stargaze/alloc/v1beta1/tx.rpc.msg";
+import * as _572 from "./stargaze/cron/v1/tx.rpc.msg";
+import * as _573 from "./stargaze/globalfee/v1/tx.rpc.msg";
+import * as _632 from "./rpc.query";
+import * as _633 from "./rpc.tx";
 export namespace publicawesome {
   export namespace stargaze {
     export namespace alloc {
@@ -39,10 +39,10 @@ export namespace publicawesome {
         ..._291,
         ..._292,
         ..._293,
-        ..._547,
-        ..._550,
-        ..._553,
-        ..._557
+        ..._561,
+        ..._564,
+        ..._567,
+        ..._571
       };
     }
     export namespace cron {
@@ -52,10 +52,10 @@ export namespace publicawesome {
         ..._296,
         ..._297,
         ..._298,
-        ..._548,
-        ..._551,
-        ..._554,
-        ..._558
+        ..._562,
+        ..._565,
+        ..._568,
+        ..._572
       };
     }
     export namespace globalfee {
@@ -65,10 +65,10 @@ export namespace publicawesome {
         ..._301,
         ..._302,
         ..._303,
-        ..._549,
-        ..._552,
-        ..._555,
-        ..._559
+        ..._563,
+        ..._566,
+        ..._569,
+        ..._573
       };
     }
     export namespace mint {
@@ -77,12 +77,12 @@ export namespace publicawesome {
         ..._305,
         ..._306,
         ..._307,
-        ..._556
+        ..._570
       };
     }
   }
   export const ClientFactory = {
-    ..._605,
-    ..._606
+    ..._632,
+    ..._633
   };
 }

DA0-DA0/dao-dao-ui

packages/types/protobuf/codegen/alliance/bundle.ts
@@ -8,12 +8,12 @@ import * as _6 from "./alliance/query";
 import * as _7 from "./alliance/redelegations";
 import * as _8 from "./alliance/tx";
 import * as _9 from "./alliance/unbonding";
-import * as _351 from "./alliance/tx.amino";
-import * as _352 from "./alliance/tx.registry";
-import * as _353 from "./alliance/query.rpc.Query";
-import * as _354 from "./alliance/tx.rpc.msg";
-import * as _589 from "./rpc.query";
-import * as _590 from "./rpc.tx";
+import * as _365 from "./alliance/tx.amino";
+import * as _366 from "./alliance/tx.registry";
+import * as _367 from "./alliance/query.rpc.Query";
+import * as _368 from "./alliance/tx.rpc.msg";
+import * as _616 from "./rpc.query";
+import * as _617 from "./rpc.tx";
 export namespace alliance {
   export const alliance = {
     ..._0,
@@ -26,13 +26,13 @@ export namespace alliance {
     ..._7,
     ..._8,
     ..._9,
-    ..._351,
-    ..._352,
-    ..._353,
-    ..._354
+    ..._365,
+    ..._366,
+    ..._367,
+    ..._368
   };
   export const ClientFactory = {
-    ..._589,
-    ..._590
+    ..._616,
+    ..._617
   };
 }

packages/types/protobuf/codegen/gaia/bundle.ts
@@ -1,26 +1,26 @@
 import * as _103 from "./globalfee/v1beta1/genesis";
 import * as _104 from "./globalfee/v1beta1/query";
 import * as _105 from "./globalfee/v1beta1/tx";
-import * as _417 from "./globalfee/v1beta1/tx.amino";
-import * as _418 from "./globalfee/v1beta1/tx.registry";
-import * as _419 from "./globalfee/v1beta1/query.rpc.Query";
-import * as _420 from "./globalfee/v1beta1/tx.rpc.msg";
-import * as _598 from "./rpc.query";
-import * as _599 from "./rpc.tx";
+import * as _431 from "./globalfee/v1beta1/tx.amino";
+import * as _432 from "./globalfee/v1beta1/tx.registry";
+import * as _433 from "./globalfee/v1beta1/query.rpc.Query";
+import * as _434 from "./globalfee/v1beta1/tx.rpc.msg";
+import * as _625 from "./rpc.query";
+import * as _626 from "./rpc.tx";
 export namespace gaia {
   export namespace globalfee {
     export const v1beta1 = {
       ..._103,
       ..._104,
       ..._105,
-      ..._417,
-      ..._418,
-      ..._419,
-      ..._420
+      ..._431,
+      ..._432,
+      ..._433,
+      ..._434
     };
   }
   export const ClientFactory = {
-    ..._598,
-    ..._599
+    ..._625,
+    ..._626
   };
 }

packages/types/protobuf/codegen/juno/bundle.ts
@@ -6,16 +6,16 @@ import * as _140 from "./mint/genesis";
 import * as _141 from "./mint/mint";
 import * as _142 from "./mint/query";
 import * as _143 from "./mint/tx";
-import * as _441 from "./feeshare/v1/tx.amino";
-import * as _442 from "./mint/tx.amino";
-import * as _443 from "./feeshare/v1/tx.registry";
-import * as _444 from "./mint/tx.registry";
-import * as _445 from "./feeshare/v1/query.rpc.Query";
-import * as _446 from "./mint/query.rpc.Query";
-import * as _447 from "./feeshare/v1/tx.rpc.msg";
-import * as _448 from "./mint/tx.rpc.msg";
-import * as _602 from "./rpc.query";
-import * as _603 from "./rpc.tx";
+import * as _455 from "./feeshare/v1/tx.amino";
+import * as _456 from "./mint/tx.amino";
+import * as _457 from "./feeshare/v1/tx.registry";
+import * as _458 from "./mint/tx.registry";
+import * as _459 from "./feeshare/v1/query.rpc.Query";
+import * as _460 from "./mint/query.rpc.Query";
+import * as _461 from "./feeshare/v1/tx.rpc.msg";
+import * as _462 from "./mint/tx.rpc.msg";
+import * as _629 from "./rpc.query";
+import * as _630 from "./rpc.tx";
 export namespace juno {
   export namespace feeshare {
     export const v1 = {
@@ -23,10 +23,10 @@ export namespace juno {
       ..._137,
       ..._138,
       ..._139,
-      ..._441,
-      ..._443,
-      ..._445,
-      ..._447
+      ..._455,
+      ..._457,
+      ..._459,
+      ..._461
     };
   }
   export const mint = {
@@ -34,13 +34,13 @@ export namespace juno {
     ..._141,
     ..._142,
     ..._143,
-    ..._442,
-    ..._444,
-    ..._446,
-    ..._448
+    ..._456,
+    ..._458,
+    ..._460,
+    ..._462
   };
   export const ClientFactory = {
-    ..._602,
-    ..._603
+    ..._629,
+    ..._630
   };
 }

packages/types/protobuf/codegen/neutron/bundle.ts
@@ -36,32 +36,32 @@ import * as _192 from "./interchaintxs/v1/genesis";
 import * as _193 from "./interchaintxs/v1/params";
 import * as _194 from "./interchaintxs/v1/query";
 import * as _195 from "./interchaintxs/v1/tx";
-import * as _458 from "./cron/tx.amino";
-import * as _459 from "./dex/tx.amino";
-import * as _460 from "./feeburner/tx.amino";
-import * as _461 from "./feerefunder/tx.amino";
-import * as _462 from "./interchainqueries/tx.amino";
-import * as _463 from "./interchaintxs/v1/tx.amino";
-import * as _464 from "./cron/tx.registry";
-import * as _465 from "./dex/tx.registry";
-import * as _466 from "./feeburner/tx.registry";
-import * as _467 from "./feerefunder/tx.registry";
-import * as _468 from "./interchainqueries/tx.registry";
-import * as _469 from "./interchaintxs/v1/tx.registry";
-import * as _470 from "./cron/query.rpc.Query";
-import * as _471 from "./dex/query.rpc.Query";
-import * as _472 from "./feeburner/query.rpc.Query";
-import * as _473 from "./feerefunder/query.rpc.Query";
-import * as _474 from "./interchainqueries/query.rpc.Query";
-import * as _475 from "./interchaintxs/v1/query.rpc.Query";
-import * as _476 from "./cron/tx.rpc.msg";
-import * as _477 from "./dex/tx.rpc.msg";
-import * as _478 from "./feeburner/tx.rpc.msg";
-import * as _479 from "./feerefunder/tx.rpc.msg";
-import * as _480 from "./interchainqueries/tx.rpc.msg";
-import * as _481 from "./interchaintxs/v1/tx.rpc.msg";
-import * as _606 from "./rpc.query";
-import * as _607 from "./rpc.tx";
+import * as _472 from "./cron/tx.amino";
+import * as _473 from "./dex/tx.amino";
+import * as _474 from "./feeburner/tx.amino";
+import * as _475 from "./feerefunder/tx.amino";
+import * as _476 from "./interchainqueries/tx.amino";
+import * as _477 from "./interchaintxs/v1/tx.amino";
+import * as _478 from "./cron/tx.registry";
+import * as _479 from "./dex/tx.registry";
+import * as _480 from "./feeburner/tx.registry";
+import * as _481 from "./feerefunder/tx.registry";
+import * as _482 from "./interchainqueries/tx.registry";
+import * as _483 from "./interchaintxs/v1/tx.registry";
+import * as _484 from "./cron/query.rpc.Query";
+import * as _485 from "./dex/query.rpc.Query";
+import * as _486 from "./feeburner/query.rpc.Query";
+import * as _487 from "./feerefunder/query.rpc.Query";
+import * as _488 from "./interchainqueries/query.rpc.Query";
+import * as _489 from "./interchaintxs/v1/query.rpc.Query";
+import * as _490 from "./cron/tx.rpc.msg";
+import * as _491 from "./dex/tx.rpc.msg";
+import * as _492 from "./feeburner/tx.rpc.msg";
+import * as _493 from "./feerefunder/tx.rpc.msg";
+import * as _494 from "./interchainqueries/tx.rpc.msg";
+import * as _495 from "./interchaintxs/v1/tx.rpc.msg";
+import * as _633 from "./rpc.query";
+import * as _634 from "./rpc.tx";
 export namespace neutron {
   export namespace contractmanager {
     export const v1 = {
@@ -74,10 +74,10 @@ export namespace neutron {
     ..._161,
     ..._162,
     ..._163,
-    ..._458,
-    ..._464,
-    ..._470,
-    ..._476
+    ..._472,
+    ..._478,
+    ..._484,
+    ..._490
   };
   export const dex = {
     ..._164,
@@ -94,10 +94,10 @@ export namespace neutron {
     ..._175,
     ..._176,
     ..._177,
-    ..._459,
-    ..._465,
-    ..._471,
-    ..._477
+    ..._473,
+    ..._479,
+    ..._485,
+    ..._491
   };
   export const feeburner = {
     ..._178,
@@ -105,10 +105,10 @@ export namespace neutron {
     ..._180,
     ..._181,
     ..._182,
-    ..._460,
-    ..._466,
-    ..._472,
-    ..._478
+    ..._474,
+    ..._480,
+    ..._486,
+    ..._492
   };
   export const feerefunder = {
     ..._183,
@@ -116,20 +116,20 @@ export namespace neutron {
     ..._185,
     ..._186,
     ..._187,
-    ..._461,
-    ..._467,
-    ..._473,
-    ..._479
+    ..._475,
+    ..._481,
+    ..._487,
+    ..._493
   };
   export const interchainqueries = {
     ..._188,
     ..._189,
     ..._190,
     ..._191,
-    ..._462,
-    ..._468,
-    ..._474,
-    ..._480
+    ..._476,
+    ..._482,
+    ..._488,
+    ..._494
   };
   export namespace interchaintxs {
     export const v1 = {
@@ -137,14 +137,14 @@ export namespace neutron {
       ..._193,
       ..._194,
       ..._195,
-      ..._463,
-      ..._469,
-      ..._475,
-      ..._481
+      ..._477,
+      ..._483,
+      ..._489,
+      ..._495
     };
   }
   export const ClientFactory = {
-    ..._606,
-    ..._607
+    ..._633,
+    ..._634
   };
 }

packages/types/protobuf/codegen/noble/bundle.ts
@@ -1,16 +1,16 @@
-import * as _338 from "../tariff/genesis";
-import * as _339 from "../tariff/params";
-import * as _340 from "../tariff/query";
-import * as _588 from "../tariff/query.rpc.Query";
-import * as _616 from "./rpc.query";
+import * as _352 from "../tariff/genesis";
+import * as _353 from "../tariff/params";
+import * as _354 from "../tariff/query";
+import * as _615 from "../tariff/query.rpc.Query";
+import * as _645 from "./rpc.query";
 export namespace noble {
   export const tariff = {
-    ..._338,
-    ..._339,
-    ..._340,
-    ..._588
+    ..._352,
+    ..._353,
+    ..._354,
+    ..._615
   };
   export const ClientFactory = {
-    ..._616
+    ..._645
   };
 }

packages/types/protobuf/codegen/publicawesome/bundle.ts
@@ -16,21 +16,21 @@ import * as _307 from "./stargaze/mint/v1beta1/genesis";
 import * as _308 from "./stargaze/mint/v1beta1/mint";
 import * as _309 from "./stargaze/mint/v1beta1/query";
 import * as _310 from "./stargaze/mint/v1beta1/tx";
-import * as _553 from "./stargaze/alloc/v1beta1/tx.amino";
-import * as _554 from "./stargaze/cron/v1/tx.amino";
-import * as _555 from "./stargaze/globalfee/v1/tx.amino";
-import * as _556 from "./stargaze/alloc/v1beta1/tx.registry";
-import * as _557 from "./stargaze/cron/v1/tx.registry";
-import * as _558 from "./stargaze/globalfee/v1/tx.registry";
-import * as _559 from "./stargaze/alloc/v1beta1/query.rpc.Query";
-import * as _560 from "./stargaze/cron/v1/query.rpc.Query";
-import * as _561 from "./stargaze/globalfee/v1/query.rpc.Query";
-import * as _562 from "./stargaze/mint/v1beta1/query.rpc.Query";
-import * as _563 from "./stargaze/alloc/v1beta1/tx.rpc.msg";
-import * as _564 from "./stargaze/cron/v1/tx.rpc.msg";
-import * as _565 from "./stargaze/globalfee/v1/tx.rpc.msg";
-import * as _612 from "./rpc.query";
-import * as _613 from "./rpc.tx";
+import * as _567 from "./stargaze/alloc/v1beta1/tx.amino";
+import * as _568 from "./stargaze/cron/v1/tx.amino";
+import * as _569 from "./stargaze/globalfee/v1/tx.amino";
+import * as _570 from "./stargaze/alloc/v1beta1/tx.registry";
+import * as _571 from "./stargaze/cron/v1/tx.registry";
+import * as _572 from "./stargaze/globalfee/v1/tx.registry";
+import * as _573 from "./stargaze/alloc/v1beta1/query.rpc.Query";
+import * as _574 from "./stargaze/cron/v1/query.rpc.Query";
+import * as _575 from "./stargaze/globalfee/v1/query.rpc.Query";
+import * as _576 from "./stargaze/mint/v1beta1/query.rpc.Query";
+import * as _577 from "./stargaze/alloc/v1beta1/tx.rpc.msg";
+import * as _578 from "./stargaze/cron/v1/tx.rpc.msg";
+import * as _579 from "./stargaze/globalfee/v1/tx.rpc.msg";
+import * as _639 from "./rpc.query";
+import * as _640 from "./rpc.tx";
 export namespace publicawesome {
   export namespace stargaze {
     export namespace alloc {
@@ -39,10 +39,10 @@ export namespace publicawesome {
         ..._294,
         ..._295,
         ..._296,
-        ..._553,
-        ..._556,
-        ..._559,
-        ..._563
+        ..._567,
+        ..._570,
+        ..._573,
+        ..._577
       };
     }
     export namespace cron {
@@ -52,10 +52,10 @@ export namespace publicawesome {
         ..._299,
         ..._300,
         ..._301,
-        ..._554,
-        ..._557,
-        ..._560,
-        ..._564
+        ..._568,
+        ..._571,
+        ..._574,
+        ..._578
       };
     }
     export namespace globalfee {
@@ -65,10 +65,10 @@ export namespace publicawesome {
         ..._304,
         ..._305,
         ..._306,
-        ..._555,
-        ..._558,
-        ..._561,
-        ..._565
+        ..._569,
+        ..._572,
+        ..._575,
+        ..._579
       };
     }
     export namespace mint {
@@ -77,12 +77,12 @@ export namespace publicawesome {
         ..._308,
         ..._309,
         ..._310,
-        ..._562
+        ..._576
       };
     }
   }
   export const ClientFactory = {
-    ..._612,
-    ..._613
+    ..._639,
+    ..._640
   };
 }