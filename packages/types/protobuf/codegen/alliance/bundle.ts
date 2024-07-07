DA0-DA0/secret-dao-dao-ui

added secret network protobufs

packages/types/protobuf/codegen/alliance/bundle.ts
@@ -8,12 +8,12 @@ import * as _6 from "./alliance/query";
 import * as _7 from "./alliance/redelegations";
 import * as _8 from "./alliance/tx";
 import * as _9 from "./alliance/unbonding";
-import * as _348 from "./alliance/tx.amino";
-import * as _349 from "./alliance/tx.registry";
-import * as _350 from "./alliance/query.rpc.Query";
-import * as _351 from "./alliance/tx.rpc.msg";
-import * as _583 from "./rpc.query";
-import * as _584 from "./rpc.tx";
+import * as _362 from "./alliance/tx.amino";
+import * as _363 from "./alliance/tx.registry";
+import * as _364 from "./alliance/query.rpc.Query";
+import * as _365 from "./alliance/tx.rpc.msg";
+import * as _610 from "./rpc.query";
+import * as _611 from "./rpc.tx";
 export namespace alliance {
   export const alliance = {
     ..._0,
@@ -26,13 +26,13 @@ export namespace alliance {
     ..._7,
     ..._8,
     ..._9,
-    ..._348,
-    ..._349,
-    ..._350,
-    ..._351
+    ..._362,
+    ..._363,
+    ..._364,
+    ..._365
   };
   export const ClientFactory = {
-    ..._583,
-    ..._584
+    ..._610,
+    ..._611
   };
 }

packages/types/protobuf/codegen/circle/bundle.ts
@@ -14,12 +14,12 @@ import * as _23 from "./cctp/v1/sending_and_receiving_messages_paused";
 import * as _24 from "./cctp/v1/signature_threshold";
 import * as _25 from "./cctp/v1/token_pair";
 import * as _26 from "./cctp/v1/tx";
-import * as _352 from "./cctp/v1/tx.amino";
-import * as _353 from "./cctp/v1/tx.registry";
-import * as _354 from "./cctp/v1/query.rpc.Query";
-import * as _355 from "./cctp/v1/tx.rpc.msg";
-import * as _585 from "./rpc.query";
-import * as _586 from "./rpc.tx";
+import * as _366 from "./cctp/v1/tx.amino";
+import * as _367 from "./cctp/v1/tx.registry";
+import * as _368 from "./cctp/v1/query.rpc.Query";
+import * as _369 from "./cctp/v1/tx.rpc.msg";
+import * as _612 from "./rpc.query";
+import * as _613 from "./rpc.tx";
 export namespace circle {
   export namespace cctp {
     export const v1 = {
@@ -39,14 +39,14 @@ export namespace circle {
       ..._24,
       ..._25,
       ..._26,
-      ..._352,
-      ..._353,
-      ..._354,
-      ..._355
+      ..._366,
+      ..._367,
+      ..._368,
+      ..._369
     };
   }
   export const ClientFactory = {
-    ..._585,
-    ..._586
+    ..._612,
+    ..._613
   };
 }

packages/types/protobuf/codegen/cosmwasm/bundle.ts
@@ -10,16 +10,16 @@ import * as _101 from "./wasm/v1/proposal";
 import * as _102 from "./wasm/v1/query";
 import * as _103 from "./wasm/v1/tx";
 import * as _104 from "./wasm/v1/types";
-import * as _407 from "./tokenfactory/v1beta1/tx.amino";
-import * as _408 from "./wasm/v1/tx.amino";
-import * as _409 from "./tokenfactory/v1beta1/tx.registry";
-import * as _410 from "./wasm/v1/tx.registry";
-import * as _411 from "./tokenfactory/v1beta1/query.rpc.Query";
-import * as _412 from "./wasm/v1/query.rpc.Query";
-import * as _413 from "./tokenfactory/v1beta1/tx.rpc.msg";
-import * as _414 from "./wasm/v1/tx.rpc.msg";
-import * as _589 from "./rpc.query";
-import * as _590 from "./rpc.tx";
+import * as _421 from "./tokenfactory/v1beta1/tx.amino";
+import * as _422 from "./wasm/v1/tx.amino";
+import * as _423 from "./tokenfactory/v1beta1/tx.registry";
+import * as _424 from "./wasm/v1/tx.registry";
+import * as _425 from "./tokenfactory/v1beta1/query.rpc.Query";
+import * as _426 from "./wasm/v1/query.rpc.Query";
+import * as _427 from "./tokenfactory/v1beta1/tx.rpc.msg";
+import * as _428 from "./wasm/v1/tx.rpc.msg";
+import * as _616 from "./rpc.query";
+import * as _617 from "./rpc.tx";
 export namespace cosmwasm {
   export namespace tokenfactory {
     export const v1beta1 = {
@@ -28,10 +28,10 @@ export namespace cosmwasm {
       ..._95,
       ..._96,
       ..._97,
-      ..._407,
-      ..._409,
-      ..._411,
-      ..._413
+      ..._421,
+      ..._423,
+      ..._425,
+      ..._427
     };
   }
   export namespace wasm {
@@ -43,14 +43,14 @@ export namespace cosmwasm {
       ..._102,
       ..._103,
       ..._104,
-      ..._408,
-      ..._410,
-      ..._412,
-      ..._414
+      ..._422,
+      ..._424,
+      ..._426,
+      ..._428
     };
   }
   export const ClientFactory = {
-    ..._589,
-    ..._590
+    ..._616,
+    ..._617
   };
 }

packages/types/protobuf/codegen/juno/bundle.ts
@@ -6,16 +6,16 @@ import * as _142 from "./mint/genesis";
 import * as _143 from "./mint/mint";
 import * as _144 from "./mint/query";
 import * as _145 from "./mint/tx";
-import * as _439 from "./feeshare/v1/tx.amino";
-import * as _440 from "./mint/tx.amino";
-import * as _441 from "./feeshare/v1/tx.registry";
-import * as _442 from "./mint/tx.registry";
-import * as _443 from "./feeshare/v1/query.rpc.Query";
-import * as _444 from "./mint/query.rpc.Query";
-import * as _445 from "./feeshare/v1/tx.rpc.msg";
-import * as _446 from "./mint/tx.rpc.msg";
-import * as _595 from "./rpc.query";
-import * as _596 from "./rpc.tx";
+import * as _453 from "./feeshare/v1/tx.amino";
+import * as _454 from "./mint/tx.amino";
+import * as _455 from "./feeshare/v1/tx.registry";
+import * as _456 from "./mint/tx.registry";
+import * as _457 from "./feeshare/v1/query.rpc.Query";
+import * as _458 from "./mint/query.rpc.Query";
+import * as _459 from "./feeshare/v1/tx.rpc.msg";
+import * as _460 from "./mint/tx.rpc.msg";
+import * as _622 from "./rpc.query";
+import * as _623 from "./rpc.tx";
 export namespace juno {
   export namespace feeshare {
     export const v1 = {
@@ -23,10 +23,10 @@ export namespace juno {
       ..._139,
       ..._140,
       ..._141,
-      ..._439,
-      ..._441,
-      ..._443,
-      ..._445
+      ..._453,
+      ..._455,
+      ..._457,
+      ..._459
     };
   }
   export const mint = {
@@ -34,13 +34,13 @@ export namespace juno {
     ..._143,
     ..._144,
     ..._145,
-    ..._440,
-    ..._442,
-    ..._444,
-    ..._446
+    ..._454,
+    ..._456,
+    ..._458,
+    ..._460
   };
   export const ClientFactory = {
-    ..._595,
-    ..._596
+    ..._622,
+    ..._623
   };
 }

packages/types/protobuf/codegen/kujira/bundle.ts
@@ -12,17 +12,17 @@ import * as _156 from "./scheduler/hook";
 import * as _157 from "./scheduler/params";
 import * as _158 from "./scheduler/proposal";
 import * as _159 from "./scheduler/query";
-import * as _447 from "./denom/tx.amino";
-import * as _448 from "./oracle/tx.amino";
-import * as _449 from "./denom/tx.registry";
-import * as _450 from "./oracle/tx.registry";
-import * as _451 from "./denom/query.rpc.Query";
-import * as _452 from "./oracle/query.rpc.Query";
-import * as _453 from "./scheduler/query.rpc.Query";
-import * as _454 from "./denom/tx.rpc.msg";
-import * as _455 from "./oracle/tx.rpc.msg";
-import * as _597 from "./rpc.query";
-import * as _598 from "./rpc.tx";
+import * as _461 from "./denom/tx.amino";
+import * as _462 from "./oracle/tx.amino";
+import * as _463 from "./denom/tx.registry";
+import * as _464 from "./oracle/tx.registry";
+import * as _465 from "./denom/query.rpc.Query";
+import * as _466 from "./oracle/query.rpc.Query";
+import * as _467 from "./scheduler/query.rpc.Query";
+import * as _468 from "./denom/tx.rpc.msg";
+import * as _469 from "./oracle/tx.rpc.msg";
+import * as _624 from "./rpc.query";
+import * as _625 from "./rpc.tx";
 export namespace kujira {
   export const denom = {
     ..._146,
@@ -30,20 +30,20 @@ export namespace kujira {
     ..._148,
     ..._149,
     ..._150,
-    ..._447,
-    ..._449,
-    ..._451,
-    ..._454
+    ..._461,
+    ..._463,
+    ..._465,
+    ..._468
   };
   export const oracle = {
     ..._151,
     ..._152,
     ..._153,
     ..._154,
-    ..._448,
-    ..._450,
-    ..._452,
-    ..._455
+    ..._462,
+    ..._464,
+    ..._466,
+    ..._469
   };
   export const scheduler = {
     ..._155,
@@ -51,10 +51,10 @@ export namespace kujira {
     ..._157,
     ..._158,
     ..._159,
-    ..._453
+    ..._467
   };
   export const ClientFactory = {
-    ..._597,
-    ..._598
+    ..._624,
+    ..._625
   };
 }

packages/types/protobuf/codegen/noble/bundle.ts
@@ -1,16 +1,16 @@
-import * as _335 from "../tariff/genesis";
-import * as _336 from "../tariff/params";
-import * as _337 from "../tariff/query";
-import * as _582 from "../tariff/query.rpc.Query";
-import * as _609 from "./rpc.query";
+import * as _349 from "../tariff/genesis";
+import * as _350 from "../tariff/params";
+import * as _351 from "../tariff/query";
+import * as _609 from "../tariff/query.rpc.Query";
+import * as _638 from "./rpc.query";
 export namespace noble {
   export const tariff = {
-    ..._335,
-    ..._336,
-    ..._337,
-    ..._582
+    ..._349,
+    ..._350,
+    ..._351,
+    ..._609
   };
   export const ClientFactory = {
-    ..._609
+    ..._638
   };
 }

DA0-DA0/dao-dao-ui

packages/types/protobuf/codegen/alliance/bundle.ts
@@ -8,12 +8,12 @@ import * as _6 from "./alliance/query";
 import * as _7 from "./alliance/redelegations";
 import * as _8 from "./alliance/tx";
 import * as _9 from "./alliance/unbonding";
-import * as _355 from "./alliance/tx.amino";
-import * as _356 from "./alliance/tx.registry";
-import * as _357 from "./alliance/query.rpc.Query";
-import * as _358 from "./alliance/tx.rpc.msg";
-import * as _597 from "./rpc.query";
-import * as _598 from "./rpc.tx";
+import * as _369 from "./alliance/tx.amino";
+import * as _370 from "./alliance/tx.registry";
+import * as _371 from "./alliance/query.rpc.Query";
+import * as _372 from "./alliance/tx.rpc.msg";
+import * as _624 from "./rpc.query";
+import * as _625 from "./rpc.tx";
 export namespace alliance {
   export const alliance = {
     ..._0,
@@ -26,13 +26,13 @@ export namespace alliance {
     ..._7,
     ..._8,
     ..._9,
-    ..._355,
-    ..._356,
-    ..._357,
-    ..._358
+    ..._369,
+    ..._370,
+    ..._371,
+    ..._372
   };
   export const ClientFactory = {
-    ..._597,
-    ..._598
+    ..._624,
+    ..._625
   };
 }

packages/types/protobuf/codegen/circle/bundle.ts
@@ -14,12 +14,12 @@ import * as _24 from "./cctp/v1/sending_and_receiving_messages_paused";
 import * as _25 from "./cctp/v1/signature_threshold";
 import * as _26 from "./cctp/v1/token_pair";
 import * as _27 from "./cctp/v1/tx";
-import * as _362 from "./cctp/v1/tx.amino";
-import * as _363 from "./cctp/v1/tx.registry";
-import * as _364 from "./cctp/v1/query.rpc.Query";
-import * as _365 from "./cctp/v1/tx.rpc.msg";
-import * as _600 from "./rpc.query";
-import * as _601 from "./rpc.tx";
+import * as _376 from "./cctp/v1/tx.amino";
+import * as _377 from "./cctp/v1/tx.registry";
+import * as _378 from "./cctp/v1/query.rpc.Query";
+import * as _379 from "./cctp/v1/tx.rpc.msg";
+import * as _627 from "./rpc.query";
+import * as _628 from "./rpc.tx";
 export namespace circle {
   export namespace cctp {
     export const v1 = {
@@ -39,14 +39,14 @@ export namespace circle {
       ..._25,
       ..._26,
       ..._27,
-      ..._362,
-      ..._363,
-      ..._364,
-      ..._365
+      ..._376,
+      ..._377,
+      ..._378,
+      ..._379
     };
   }
   export const ClientFactory = {
-    ..._600,
-    ..._601
+    ..._627,
+    ..._628
   };
 }

packages/types/protobuf/codegen/cosmwasm/bundle.ts
@@ -10,16 +10,16 @@ import * as _99 from "./wasm/v1/proposal";
 import * as _100 from "./wasm/v1/query";
 import * as _101 from "./wasm/v1/tx";
 import * as _102 from "./wasm/v1/types";
-import * as _413 from "./tokenfactory/v1beta1/tx.amino";
-import * as _414 from "./wasm/v1/tx.amino";
-import * as _415 from "./tokenfactory/v1beta1/tx.registry";
-import * as _416 from "./wasm/v1/tx.registry";
-import * as _417 from "./tokenfactory/v1beta1/query.rpc.Query";
-import * as _418 from "./wasm/v1/query.rpc.Query";
-import * as _419 from "./tokenfactory/v1beta1/tx.rpc.msg";
-import * as _420 from "./wasm/v1/tx.rpc.msg";
-import * as _604 from "./rpc.query";
-import * as _605 from "./rpc.tx";
+import * as _427 from "./tokenfactory/v1beta1/tx.amino";
+import * as _428 from "./wasm/v1/tx.amino";
+import * as _429 from "./tokenfactory/v1beta1/tx.registry";
+import * as _430 from "./wasm/v1/tx.registry";
+import * as _431 from "./tokenfactory/v1beta1/query.rpc.Query";
+import * as _432 from "./wasm/v1/query.rpc.Query";
+import * as _433 from "./tokenfactory/v1beta1/tx.rpc.msg";
+import * as _434 from "./wasm/v1/tx.rpc.msg";
+import * as _631 from "./rpc.query";
+import * as _632 from "./rpc.tx";
 export namespace cosmwasm {
   export namespace tokenfactory {
     export const v1beta1 = {
@@ -28,10 +28,10 @@ export namespace cosmwasm {
       ..._93,
       ..._94,
       ..._95,
-      ..._413,
-      ..._415,
-      ..._417,
-      ..._419
+      ..._427,
+      ..._429,
+      ..._431,
+      ..._433
     };
   }
   export namespace wasm {
@@ -43,14 +43,14 @@ export namespace cosmwasm {
       ..._100,
       ..._101,
       ..._102,
-      ..._414,
-      ..._416,
-      ..._418,
-      ..._420
+      ..._428,
+      ..._430,
+      ..._432,
+      ..._434
     };
   }
   export const ClientFactory = {
-    ..._604,
-    ..._605
+    ..._631,
+    ..._632
   };
 }

packages/types/protobuf/codegen/juno/bundle.ts
@@ -6,16 +6,16 @@ import * as _144 from "./mint/genesis";
 import * as _145 from "./mint/mint";
 import * as _146 from "./mint/query";
 import * as _147 from "./mint/tx";
-import * as _449 from "./feeshare/v1/tx.amino";
-import * as _450 from "./mint/tx.amino";
-import * as _451 from "./feeshare/v1/tx.registry";
-import * as _452 from "./mint/tx.registry";
-import * as _453 from "./feeshare/v1/query.rpc.Query";
-import * as _454 from "./mint/query.rpc.Query";
-import * as _455 from "./feeshare/v1/tx.rpc.msg";
-import * as _456 from "./mint/tx.rpc.msg";
-import * as _612 from "./rpc.query";
-import * as _613 from "./rpc.tx";
+import * as _463 from "./feeshare/v1/tx.amino";
+import * as _464 from "./mint/tx.amino";
+import * as _465 from "./feeshare/v1/tx.registry";
+import * as _466 from "./mint/tx.registry";
+import * as _467 from "./feeshare/v1/query.rpc.Query";
+import * as _468 from "./mint/query.rpc.Query";
+import * as _469 from "./feeshare/v1/tx.rpc.msg";
+import * as _470 from "./mint/tx.rpc.msg";
+import * as _639 from "./rpc.query";
+import * as _640 from "./rpc.tx";
 export namespace juno {
   export namespace feeshare {
     export const v1 = {
@@ -23,10 +23,10 @@ export namespace juno {
       ..._141,
       ..._142,
       ..._143,
-      ..._449,
-      ..._451,
-      ..._453,
-      ..._455
+      ..._463,
+      ..._465,
+      ..._467,
+      ..._469
     };
   }
   export const mint = {
@@ -34,13 +34,13 @@ export namespace juno {
     ..._145,
     ..._146,
     ..._147,
-    ..._450,
-    ..._452,
-    ..._454,
-    ..._456
+    ..._464,
+    ..._466,
+    ..._468,
+    ..._470
   };
   export const ClientFactory = {
-    ..._612,
-    ..._613
+    ..._639,
+    ..._640
   };
 }

packages/types/protobuf/codegen/kujira/bundle.ts
@@ -12,17 +12,17 @@ import * as _158 from "./scheduler/hook";
 import * as _159 from "./scheduler/params";
 import * as _160 from "./scheduler/proposal";
 import * as _161 from "./scheduler/query";
-import * as _457 from "./denom/tx.amino";
-import * as _458 from "./oracle/tx.amino";
-import * as _459 from "./denom/tx.registry";
-import * as _460 from "./oracle/tx.registry";
-import * as _461 from "./denom/query.rpc.Query";
-import * as _462 from "./oracle/query.rpc.Query";
-import * as _463 from "./scheduler/query.rpc.Query";
-import * as _464 from "./denom/tx.rpc.msg";
-import * as _465 from "./oracle/tx.rpc.msg";
-import * as _614 from "./rpc.query";
-import * as _615 from "./rpc.tx";
+import * as _471 from "./denom/tx.amino";
+import * as _472 from "./oracle/tx.amino";
+import * as _473 from "./denom/tx.registry";
+import * as _474 from "./oracle/tx.registry";
+import * as _475 from "./denom/query.rpc.Query";
+import * as _476 from "./oracle/query.rpc.Query";
+import * as _477 from "./scheduler/query.rpc.Query";
+import * as _478 from "./denom/tx.rpc.msg";
+import * as _479 from "./oracle/tx.rpc.msg";
+import * as _641 from "./rpc.query";
+import * as _642 from "./rpc.tx";
 export namespace kujira {
   export const denom = {
     ..._148,
@@ -30,20 +30,20 @@ export namespace kujira {
     ..._150,
     ..._151,
     ..._152,
-    ..._457,
-    ..._459,
-    ..._461,
-    ..._464
+    ..._471,
+    ..._473,
+    ..._475,
+    ..._478
   };
   export const oracle = {
     ..._153,
     ..._154,
     ..._155,
     ..._156,
-    ..._458,
-    ..._460,
-    ..._462,
-    ..._465
+    ..._472,
+    ..._474,
+    ..._476,
+    ..._479
   };
   export const scheduler = {
     ..._157,
@@ -51,10 +51,10 @@ export namespace kujira {
     ..._159,
     ..._160,
     ..._161,
-    ..._463
+    ..._477
   };
   export const ClientFactory = {
-    ..._614,
-    ..._615
+    ..._641,
+    ..._642
   };
 }

packages/types/protobuf/codegen/noble/bundle.ts
@@ -1,16 +1,16 @@
-import * as _342 from "../tariff/genesis";
-import * as _343 from "../tariff/params";
-import * as _344 from "../tariff/query";
-import * as _596 from "../tariff/query.rpc.Query";
-import * as _626 from "./rpc.query";
+import * as _356 from "../tariff/genesis";
+import * as _357 from "../tariff/params";
+import * as _358 from "../tariff/query";
+import * as _623 from "../tariff/query.rpc.Query";
+import * as _655 from "./rpc.query";
 export namespace noble {
   export const tariff = {
-    ..._342,
-    ..._343,
-    ..._344,
-    ..._596
+    ..._356,
+    ..._357,
+    ..._358,
+    ..._623
   };
   export const ClientFactory = {
-    ..._626
+    ..._655
   };
 }