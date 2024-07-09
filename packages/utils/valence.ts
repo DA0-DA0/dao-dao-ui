7039c3a37e9d4fc4d6440bec986ec98e7bb41ca0
Noah Saso <noahsaso@gmail.com>
2024-07-09 14:39:59 -0400

    add valence allowlist

--- packages/utils/valence.ts
+++ packages/utils/valence.ts
@@ -1,5 +1,11 @@
-import { ActionContextType, ActionOptions, ChainId } from '@dao-dao/types'
+import {
+  AccountType,
+  ActionContextType,
+  ActionOptions,
+  ChainId,
+} from '@dao-dao/types'
 
+import { VALENCE_ALLOWLIST } from './constants'
 import { VALENCE_SUPPORTED_CHAINS } from './constants/chains'
 
 // Whether or not the current action context supports Valence accounts.
@@ -7,8 +13,14 @@ export const actionContextSupportsValence = ({
   context,
   chain: { chain_id: chainId },
 }: ActionOptions): boolean =>
-  VALENCE_SUPPORTED_CHAINS.includes(chainId as ChainId) ||
-  (context.type === ActionContextType.Dao &&
-    VALENCE_SUPPORTED_CHAINS.some(
-      (chainId) => chainId in context.dao.info.polytoneProxies
-    ))
+  // Must either be on the valence allowlist or already have a Valence account.
+  context.accounts.some(
+    (a) =>
+      VALENCE_ALLOWLIST.includes(a.address) || a.type === AccountType.Valence
+  ) &&
+  // Must be on a supported Valence chain.
+  (VALENCE_SUPPORTED_CHAINS.includes(chainId as ChainId) ||
+    (context.type === ActionContextType.Dao &&
+      VALENCE_SUPPORTED_CHAINS.some(
+        (chainId) => chainId in context.dao.info.polytoneProxies
+      )))
