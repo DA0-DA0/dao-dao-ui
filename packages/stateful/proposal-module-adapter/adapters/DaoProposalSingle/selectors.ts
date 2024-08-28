25de4687350bbcdeb133627f64fde9009983ad79
Noah Saso <noahsaso@gmail.com>
2024-08-28 17:38:31 +0300

    fixed type

--- packages/stateful/proposal-module-adapter/adapters/DaoProposalSingle/selectors.ts
+++ packages/stateful/proposal-module-adapter/adapters/DaoProposalSingle/selectors.ts
@@ -83,7 +83,7 @@ export const approverIdForPreProposeApprovalIdSelector = selectorFamily<
                 },
               },
             ],
-          }) as RecoilValueReadOnly<number>,
+          }) as RecoilValueReadOnly<number | null>,
         ])
       )
 
