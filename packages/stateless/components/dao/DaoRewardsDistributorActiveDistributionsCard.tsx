a911be8c7437d6782a35687236b7008fb778e3cc
Noah Saso <noahsaso@gmail.com>
2024-10-23 14:42:03 -0400

    hide paused reward distributions on home page

--- packages/stateless/components/dao/DaoRewardsDistributorActiveDistributionsCard.tsx
+++ packages/stateless/components/dao/DaoRewardsDistributorActiveDistributionsCard.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { DaoRewardsDistributorActiveDistributionsCardProps } from '@dao-dao/types'
@@ -12,6 +13,7 @@ import {
 import { useQuerySyncedState } from '../../hooks'
 import { Button } from '../buttons'
 import { ErrorPage } from '../error'
+import { SwitchCard } from '../inputs'
 import { LineLoaders } from '../LineLoader'
 
 export const DaoRewardsDistributorActiveDistributionsCard = ({
@@ -35,6 +37,20 @@ export const DaoRewardsDistributorActiveDistributionsCard = ({
         )
       : undefined
 
+  const [showPaused, setShowPaused] = useState(false)
+  const distributionsToShow =
+    distributions.loading || distributions.errored
+      ? []
+      : showPaused
+      ? distributions.data
+      : distributions.data.filter(
+          (d) => !('paused' in d.active_epoch.emission_rate)
+        )
+  const pausedDistributionsExist =
+    !distributions.loading &&
+    !distributions.errored &&
+    distributions.data.some((d) => 'paused' in d.active_epoch.emission_rate)
+
   return (
     <>
       <div
@@ -46,8 +62,12 @@ export const DaoRewardsDistributorActiveDistributionsCard = ({
         <p className="caption-text">
           {distributions.loading || distributions.errored
             ? t('title.activeDistributions')
-            : t('title.numActiveDistributions', {
+            : showPaused && pausedDistributionsExist
+            ? t('title.numDistributions', {
                 count: distributions.data.length,
+              })
+            : t('title.numActiveDistributions', {
+                count: distributionsToShow.length,
               })}
         </p>
 
@@ -57,7 +77,7 @@ export const DaoRewardsDistributorActiveDistributionsCard = ({
           <ErrorPage error={distributions.error} />
         ) : (
           <div className="styled-scrollbar flex flex-col gap-1 max-h-64 overflow-y-auto -mb-4 pb-4">
-            {distributions.data.map((distribution) => (
+            {distributionsToShow.map((distribution) => (
               <Button
                 key={distribution.id}
                 onClick={() =>
@@ -81,6 +101,16 @@ export const DaoRewardsDistributorActiveDistributionsCard = ({
               </Button>
             ))}
           </div>
+        )}
+
+        {pausedDistributionsExist && (
+          <SwitchCard
+            containerClassName="self-start mt-2"
+            enabled={showPaused}
+            label={t('title.showPaused')}
+            onClick={() => setShowPaused((s) => !s)}
+            sizing="sm"
+          />
         )}
       </div>
 
