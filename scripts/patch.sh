6e4e677af74b825bbdeb6e686280c0769b2b9143
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-04-09 19:23:24 -0400

    try patch-package 3 times before failing

---
+++ scripts/patch.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+for i in {1..3}; do
+  patch-package
+  status=$?
+  if [ $status -eq 0 ]; then
+    break
+  else
+    echo "patch-package attempt $i failed"
+    [ $i -lt 3 ] && echo "retrying..." && sleep 1
+  fi
+done
+
+if [ $status -ne 0 ]; then
+  if [ "$CI" = "true" ]; then
+    echo "patch-package status = $status, exiting with 0 due to CI=true"
+    exit 0
+  else
+    exit $status
+  fi
+fi
