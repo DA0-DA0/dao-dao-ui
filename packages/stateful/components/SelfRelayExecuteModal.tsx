89c3c4b2325090559f6810adede2c16cf4098a12
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-08-06 23:18:31 -0400

    increased juno and osmosis self relay fees

--- packages/stateful/components/SelfRelayExecuteModal.tsx
+++ packages/stateful/components/SelfRelayExecuteModal.tsx
@@ -89,8 +89,8 @@ enum RelayStatus {
 
 const RELAYER_FUNDS_NEEDED: Partial<Record<ChainId | string, number>> = {
   [ChainId.CosmosHubMainnet]: 0.06 * 10 ** 6,
-  [ChainId.JunoMainnet]: 1 * 10 ** 6,
-  [ChainId.OsmosisMainnet]: 1 * 10 ** 6,
+  [ChainId.JunoMainnet]: 3 * 10 ** 6,
+  [ChainId.OsmosisMainnet]: 2 * 10 ** 6,
   [ChainId.StargazeMainnet]: 10 * 10 ** 6,
   [ChainId.NeutronMainnet]: 5 * 10 ** 6,
   [ChainId.TerraMainnet]: 0.1 * 10 ** 6,
